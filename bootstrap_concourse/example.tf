4d584029de976f7aa65c90f7ff69ca1539ae284a
M. Adam Kendall <michael.kendall@gsa.gov>
2016-05-11 17:26:00 -0400

    master bosh bits

--- bootstrap_concourse/example.tf
+++ bootstrap_concourse/example.tf
@@ -1,3 +1,4 @@
+variable "SECRETS_PASSPHRASE" {}
 variable "PUBLIC_KEY_PATH" {}
 variable "ACCESS_KEY_ID" {}
 variable "SECRET_ACCESS_KEY" {}
@@ -51,7 +52,40 @@ resource "aws_security_group" "allow_all_tcp" {
 
 }
 
+resource "aws_s3_bucket" "secrets_bucket" {
+    bucket = "cloud-gov-varz"
+    acl = "private"
+    versioning {
+      enabled = true
+    }
+}
+
+resource "aws_s3_bucket_object" "master_bosh_secrets" {
+    depends_on = [ "aws_s3_bucket.secrets_bucket" ]
+    bucket = "${aws_s3_bucket.secrets_bucket.id}"
+    key = "master-bosh.yml"
+    source = "./master-bosh.enc.yml"
+}
+
+resource "aws_s3_bucket_object" "master_bosh_cert" {
+    depends_on = [ "aws_s3_bucket.secrets_bucket" ]
+    bucket = "${aws_s3_bucket.secrets_bucket.id}"
+    key = "master-bosh.pem"
+    source = "./master-bosh.enc.pem"
+}
+
+resource "aws_s3_bucket_object" "master_bosh_init_state" {
+    depends_on = [ "aws_s3_bucket.secrets_bucket" ]
+    bucket = "${aws_s3_bucket.secrets_bucket.id}"
+    key = "master-bosh-state.json"
+    content = "{}"
+}
+
 resource "aws_instance" "example" {
+  depends_on = [ "aws_s3_bucket_object.master_bosh_secrets",
+    "aws_s3_bucket_object.master_bosh_cert",
+    "aws_s3_bucket_object.master_bosh_init_state"
+  ]
   ami = "${var.AMI}"
   instance_type = "m1.small"
   key_name = "${aws_key_pair.auth.id}"
@@ -74,7 +108,7 @@ resource "aws_instance" "example" {
       "test -f concourse_linux_amd64 || curl -LO https://github.com/concourse/concourse/releases/download/v1.2.0/concourse_linux_amd64",
       "echo \"sudo iptables -t nat -F && sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080\" > start_concourse.sh",
       "echo \"sleep 10 && screen -dmS web ./concourse_linux_amd64 web --basic-auth-username ${var.CI_USER} --basic-auth-password ${var.CI_PASS}  --session-signing-key session_signing_key --tsa-host-key host_key --tsa-authorized-keys authorized_worker_keys --postgres-data-source='postgres://ubuntu:ci@127.0.0.1:5432/atc?sslmode=disable' --external-url http://${aws_instance.example.public_ip}\" >> start_concourse.sh",
-      "echo \"sudo mkdir -p /opt/worker && screen -dmS worker sudo ./concourse_linux_amd64 worker --work-dir /opt/worker --tsa-host 127.0.0.1 --tsa-public-key host_key.pub --tsa-worker-private-key worker_key\" >> start_concourse.sh",
+      "echo \"sudo mkdir -p /mnt/worker && screen -dmS worker sudo ./concourse_linux_amd64 worker --work-dir /mnt/worker --tsa-host 127.0.0.1 --tsa-public-key host_key.pub --tsa-worker-private-key worker_key\" >> start_concourse.sh",
       "chmod 755 concourse_linux_amd64 start_concourse.sh",
       "echo \"cd /home/ubuntu && ./start_concourse.sh\" > /tmp/rc.local && chmod 755 /tmp/rc.local && sudo mv /tmp/rc.local /etc/rc.local",
       "sudo shutdown -r now"
@@ -85,9 +119,10 @@ resource "aws_instance" "example" {
 until nc -z ${aws_instance.example.public_ip} 80; do sleep 1; done && \
 fly -t boot login -c http://${aws_instance.example.public_ip} -u ${var.CI_USER} -p ${var.CI_PASS} && \
 until fly -t boot workers 2>&1| grep linux; do sleep 1; done && \
-fly -t boot set-pipeline -n -p bootstrap -c pipeline.yml -v aws_access_keyid=${var.ACCESS_KEY_ID} \
+fly -t boot set-pipeline -n -p bootstrap -c pipeline.yml -v aws_access_key_id=${var.ACCESS_KEY_ID} \
   -v aws_secret_access_key=${var.SECRET_ACCESS_KEY} -v aws_default_region=${var.DEFAULT_REGION} \
-  -v aws_s3_bucket=${var.CF_S3_BUCKET} && \
+  -v aws_s3_bucket=${var.CF_S3_BUCKET} -v passphrase=${var.SECRETS_PASSPHRASE} \
+  -v aws_secrets_bucket="${aws_s3_bucket.secrets_bucket.id}" && \
 fly -t boot unpause-pipeline -p bootstrap && \
 fly -t boot trigger-job -j bootstrap/bootstrap
 EOC
