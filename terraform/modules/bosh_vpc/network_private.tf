583ed9a4595aa9d388bf1b7deb28bab5d1aca1a8
M. Adam Kendall <michael.kendall@gsa.gov>
2016-05-23 13:56:30 -0400

    Use aws_route instead of inline routes in the route table. Currently, terraform allows one or the other, not both. Because we want potentially want to add routes for a peering connection, use the aws_route.

--- terraform/modules/bosh_vpc/network_private.tf
+++ terraform/modules/bosh_vpc/network_private.tf
@@ -37,10 +37,6 @@ resource "aws_subnet" "az2_private" {
 
 resource "aws_route_table" "az1_private_route_table" {
   vpc_id = "${aws_vpc.main_vpc.id}"
-  route {
-    cidr_block = "0.0.0.0/0"
-    instance_id = "${aws_instance.az1_private_nat.id}"
-  }
 
   tags {
     Name = "${var.stack_description} (Private Route Table)"
@@ -49,14 +45,22 @@ resource "aws_route_table" "az1_private_route_table" {
 
 resource "aws_route_table" "az2_private_route_table" {
   vpc_id = "${aws_vpc.main_vpc.id}"
-  route {
-    cidr_block = "0.0.0.0/0"
-    instance_id = "${aws_instance.az2_private_nat.id}"
-  }
 
   tags {
     Name = "${var.stack_description} (Private Route Table)"
   }
+}
+
+resource "aws_route" "az1_nat_route" {
+    route_table_id = "${aws_route_table.az1_private_route_table.id}"
+    destination_cidr_block = "0.0.0.0/0"
+    instance_id = "${aws_instance.az1_private_nat.id}"
+}
+
+resource "aws_route" "az2_nat_route" {
+    route_table_id = "${aws_route_table.az2_private_route_table.id}"
+    destination_cidr_block = "0.0.0.0/0"
+    instance_id = "${aws_instance.az2_private_nat.id}"
 }
 
 resource "aws_route_table_association" "az1_private_rta" {
