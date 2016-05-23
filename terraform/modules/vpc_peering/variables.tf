b7e88b3430e2f8ce5937d45b721285b53b6d6e9c
M. Adam Kendall <michael.kendall@gsa.gov>
2016-05-23 15:21:43 -0400

    Add vpc_peering module.

---
+++ terraform/modules/vpc_peering/variables.tf
@@ -0,0 +1,17 @@
+variable "peer_owner_id" {}
+
+variable "source_az1_route_table" {}
+
+variable "source_az2_route_table" {}
+
+variable "target_az1_route_table" {}
+
+variable "target_az2_route_table" {}
+
+variable "target_vpc_id" {}
+
+variable "target_vpc_cidr" {}
+
+variable "source_vpc_id" {}
+
+variable "source_vpc_cidr" {}
