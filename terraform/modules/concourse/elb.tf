d31da87207f6578c3746e3d6683fe0d240db18d7
M. Adam Kendall <michael.kendall@gsa.gov>
2016-05-25 11:27:50 -0400

    For now, we'll disable 443 on the concourse elb instance until we can figure out process for cert

--- terraform/modules/concourse/elb.tf
+++ terraform/modules/concourse/elb.tf
@@ -11,13 +11,14 @@ resource "aws_elb" "concourse_elb" {
     lb_protocol = "tcp"
   }
 
-  listener {
-    instance_port = 8080
-    instance_protocol = "tcp"
-    lb_port = 443
-    lb_protocol = "ssl"
-    ssl_certificate_id = "arn:aws:iam::${var.account_id}:server-certificate/${var.elb_cert_name}"
-  }
+  /* TODO: Make sure we can upload certs for use */
+  # listener {
+  #   instance_port = 8080
+  #   instance_protocol = "tcp"
+  #   lb_port = 443
+  #   lb_protocol = "ssl"
+  #   ssl_certificate_id = "arn:aws:iam::${var.account_id}:server-certificate/${var.elb_cert_name}"
+  # }
 
   health_check {
     healthy_threshold = 2
