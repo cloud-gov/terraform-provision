18bcaf1437567216822f7c311a6c397c543cad87
M. Adam Kendall <michael.kendall@gsa.gov>
2016-06-06 17:12:55 -0400

    Open up permissions for bosh user for now

--- terraform/modules/iam_user/bosh_user/user.tf
+++ terraform/modules/iam_user/bosh_user/user.tf
@@ -20,12 +20,7 @@ module "bosh_user" {
         "Sid": "S3Access",
         "Effect": "Allow",
         "Action": [
-            "s3:PutObject",
-            "s3:RestoreObject",
-            "s3:GetObject",
-            "s3:GetObjectVersion",
-            "s3:DeleteObject",
-            "s3:DeleteObjectVersion"
+            "s3:*"
         ],
         "Resource": [
             "*"
@@ -35,35 +30,8 @@ module "bosh_user" {
         "Sid": "BoshDeployments",
         "Effect": "Allow",
         "Action": [
-            "ec2:DescribeAccountAttributes",
-            "ec2:DescribeAvailabilityZones",
-            "ec2:DescribeKeypairs",
-            "ec2:DescribeVpcs",
-            "ec2:DescribeSecurityGroups",
-            "ec2:DescribeSubnets",
-            "ec2:DescribeImages",
-            "ec2:RegisterImage",
-            "ec2:DeregisterImage",
-            "ec2:RunInstances",
-            "ec2:DescribeInstances",
-            "ec2:TerminateInstances",
-            "ec2:RebootInstances",
-            "elasticloadbalancing:DescribeLoadBalancers",
-            "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
-            "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
-            "ec2:DescribeAddresses",
-            "ec2:DisassociateAddress",
-            "ec2:AssociateAddress",
-            "ec2:CreateTags",
-            "ec2:DescribeVolumes",
-            "ec2:CreateVolume",
-            "ec2:AttachVolume",
-            "ec2:DeleteVolume",
-            "ec2:DetachVolume",
-            "ec2:CreateSnapshot",
-            "ec2:DeleteSnapshot",
-            "ec2:DescribeSnapshots",
-            "ec2:DescribeRegions"
+            "ec2:*",
+            "elasticloadbalancing:*"
         ],
         "Resource": [
             "*"
