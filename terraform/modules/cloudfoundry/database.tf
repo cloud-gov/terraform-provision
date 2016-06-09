00d6b0f475f247393d937c83ab8b3d0a8c191759
M. Adam Kendall <michael.kendall@gsa.gov>
2016-06-08 21:43:39 -0400

    Use rds db name for cf database

--- terraform/modules/cloudfoundry/database.tf
+++ terraform/modules/cloudfoundry/database.tf
@@ -6,6 +6,7 @@ module "cf_database" {
     rds_db_size = "${var.rds_db_size}"
     rds_db_engine = "${var.rds_db_engine}"
     rds_db_engine_version = "${var.rds_db_engine_version}"
+    rds_db_name = "${var.rds_db_name}"
     rds_username = "${var.rds_username}"
     rds_password = "${var.rds_password}"
     rds_subnet_group = "${var.rds_subnet_group}"
