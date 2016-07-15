3b1eafa9525ec44d3cbb8cb5906f2d4bb915c431
s-matyukevich <s.matyukevich@gmail.com>
2016-07-15 11:47:36 +0300

    bug with default values in concourse_user vars fixed

--- terraform/modules/iam_user/concourse_user/variables.tf
+++ terraform/modules/iam_user/concourse_user/variables.tf
@@ -2,17 +2,17 @@ variable "username" {}
 variable "aws_partition" {}
 
 variable "staging_private_bucket" {
-  default: "cloud-gov-varz-staging" 
+  default = "cloud-gov-varz-staging" 
 }
 
 variable "prod_private_bucket" {
-  default: "cloud-gov-varz" 
+  default= "cloud-gov-varz" 
 }
 
 variable "bosh_releases_bucket" {
-  default: "cloud-gov-bosh-release" 
+  default = "cloud-gov-bosh-release" 
 }
 
 variable "bosh_stemcells_bucket" {
-  default: "cg-stemcell-images" 
+  default = "cg-stemcell-images" 
 }
