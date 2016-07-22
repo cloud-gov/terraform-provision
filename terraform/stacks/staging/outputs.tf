d88c4463a617e1d53fff7b83f1087afc1d4bf762
Fawad Siraj <fawad.siraj@coresphere.com>
2016-07-22 11:54:29 -0400

    fixed output for subnets

--- terraform/stacks/staging/outputs.tf
+++ terraform/stacks/staging/outputs.tf
@@ -137,10 +137,10 @@ output "diego_elb_dns_name" {
 
 /* Diego subnets */
 output "diego_services_subnet_az1" {
-  value = "${module.diego.diego_cidr_1}"
+  value = "${module.diego.diego_services_subnet_az1}"
 }
 output "diego_services_subnet_az2" {
-  value = "${module.diego.diego_cidr_2}"
+  value = "${module.diego.diego_services_subnet_az2}"
 }
 
 
