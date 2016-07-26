e98434db149282efbfcabaf6ab1b654c8f5f1186
Joshua Carp <jm.carp@gmail.com>
2016-07-26 10:34:03 -0400

    Add logsearch ELB variables to outputs.

--- terraform/modules/cloudfoundry/outputs.tf
+++ terraform/modules/cloudfoundry/outputs.tf
@@ -47,3 +47,16 @@ output "monitoring_elb_name" {
 output "monitoring_elb_security_group" {
   value = "${aws_security_group.monitoring.id}"
 }
+
+/* Logsearch */
+output "logsearch_elb_dns_name" {
+  value = "${aws_elb.logsearch_elb.dns_name}"
+}
+
+output "logsearch_elb_name" {
+  value = "${aws_elb.logsearch_elb.name}"
+}
+
+output "logsearch_elb_security_group" {
+  value = "${aws_security_group.logsearch.id}"
+}
--- terraform/stacks/staging/outputs.tf
+++ terraform/stacks/staging/outputs.tf
@@ -107,6 +107,16 @@ output "monitoring_elb_security_group" {
   value = "${module.cf.monitoring_elb_security_group}"
 }
 
+output "logsearch_elb_dns_name" {
+  value = "${module.cf.logsearch_elb_dns_name}"
+}
+output "logsearch_elb_name" {
+  value = "${module.cf.logsearch_elb_name}"
+}
+output "logsearch_elb_security_group" {
+  value = "${module.cf.logsearch_elb_security_group}"
+}
+
 /* CloudFoundry RDS */
 output "cf_rds_url" {
     value = "${module.cf.cf_rds_url}"
