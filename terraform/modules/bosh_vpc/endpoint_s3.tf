bcbcd146d14c034b3d4c0c37026f616c5f8dbb67
M. Adam Kendall <michael.kendall@gsa.gov>
2016-08-02 10:34:30 -0400

    Correctly use az2 route table

--- terraform/modules/bosh_vpc/endpoint_s3.tf
+++ terraform/modules/bosh_vpc/endpoint_s3.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc_endpoint" "private-s3" {
     vpc_id = "${aws_vpc.main_vpc.id}"
     service_name = "com.amazonaws.${var.aws_default_region}.s3"
-    route_table_ids = ["${aws_route_table.az1_private_route_table.id}", "${aws_route_table.az1_private_route_table.id}"]
+    route_table_ids = ["${aws_route_table.az1_private_route_table.id}", "${aws_route_table.az2_private_route_table.id}"]
     policy = <<EOF
 {
     "Version": "2012-10-17",
