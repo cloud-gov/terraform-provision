ce48ddebfc5cc4fb9e4d22428edaaba8fdfc65b6
Joshua Carp <jm.carp@gmail.com>
2016-08-22 12:00:57 -0400

    Replace state resource with data source.
    See:
    * https://www.terraform.io/docs/configuration/data-sources.html
    * https://www.terraform.io/docs/providers/terraform/d/remote_state.html
    
    Goes with
    https://github.com/18F/cg-deploy-concourse-docker-image/pull/7.

--- terraform/stacks/bootstrap/peering/tooling_peer.tf
+++ terraform/stacks/bootstrap/peering/tooling_peer.tf
@@ -1,4 +1,4 @@
-resource "terraform_remote_state" "tooling_vpc" {
+data "terraform_remote_state" "tooling_vpc" {
     backend = "s3"
     config {
         bucket = "${var.remote_state_bucket}"
@@ -9,25 +9,25 @@ resource "terraform_remote_state" "tooling_vpc" {
 # Create peering connection for default_vpc -> tooling
 resource "aws_vpc_peering_connection" "peering" {
     peer_owner_id = "${var.account_id}"
-    peer_vpc_id = "${terraform_remote_state.tooling_vpc.output.vpc_id}"
+    peer_vpc_id = "${data.terraform_remote_state.tooling_vpc.vpc_id}"
     auto_accept = true
     vpc_id = "${var.default_vpc_id}"
 
     tags = {
-        Name = "${var.default_vpc_id} to ${terraform_remote_state.tooling_vpc.output.vpc_id}"
+        Name = "${var.default_vpc_id} to ${data.terraform_remote_state.tooling_vpc.vpc_id}"
     }
 }
 
 # Add peering connection to tooling private_route_table_az1 with default_vpc_cidr
 resource "aws_route" "target_az1_to_source_cidr" {
-    route_table_id = "${terraform_remote_state.tooling_vpc.output.private_route_table_az1}"
+    route_table_id = "${data.terraform_remote_state.tooling_vpc.private_route_table_az1}"
     destination_cidr_block = "${var.default_vpc_cidr}"
     vpc_peering_connection_id = "${aws_vpc_peering_connection.peering.id}"
 }
 
 # Add peering connection to tooling private_route_table_az2 with default_vpc_cidr
 resource "aws_route" "target_az2_to_source_cidr" {
-    route_table_id = "${terraform_remote_state.tooling_vpc.output.private_route_table_az2}"
+    route_table_id = "${data.terraform_remote_state.tooling_vpc.private_route_table_az2}"
     destination_cidr_block = "${var.default_vpc_cidr}"
     vpc_peering_connection_id = "${aws_vpc_peering_connection.peering.id}"
 }
@@ -35,7 +35,7 @@ resource "aws_route" "target_az2_to_source_cidr" {
 # add peering connection to default_vpc_route_table with tooling_vpc_cidr
 resource "aws_route" "source_az1_to_target_cidr" {
     route_table_id = "${var.default_vpc_route_table}"
-    destination_cidr_block = "${terraform_remote_state.tooling_vpc.output.vpc_cidr}"
+    destination_cidr_block = "${data.terraform_remote_state.tooling_vpc.vpc_cidr}"
     vpc_peering_connection_id = "${aws_vpc_peering_connection.peering.id}"
 }
 
@@ -46,5 +46,5 @@ resource "aws_security_group_rule" "allow_all_default" {
     to_port = 0
     protocol = "-1"
     cidr_blocks = ["${var.default_vpc_cidr}"]
-    security_group_id = "${terraform_remote_state.tooling_vpc.output.bosh_security_group}"
+    security_group_id = "${data.terraform_remote_state.tooling_vpc.bosh_security_group}"
 }
