b9cf0c3cc44640c4e68171b5646bc3c87f9ef825
Joshua Carp <jm.carp@gmail.com>
2016-09-15 21:13:53 -0400

    Run validate from project root.

--- validate.sh
+++ validate.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+set -e
+
 which terraform > /dev/null 2>&1 || {
   echo "Aborted. Please install terraform by following https://www.terraform.io/intro/getting-started/install.html" 1>&2
   exit 1
 }
 
-find ./terraform -name "*.tf" -print0 \
+path="$(dirname $0)"
+
+find "${path}/terraform" -name "*.tf" -print0 \
   | xargs -0 -n1 dirname \
   | sort --unique \
   | xargs -I {} terraform validate {}
