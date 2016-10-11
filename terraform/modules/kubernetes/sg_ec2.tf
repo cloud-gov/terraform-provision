11d37c7156807515834384a0b50dc87c51fb1ae8
Chris Nelson <cnelson@cnelson.org>
2016-10-11 12:01:10 -0700

    Update sg_ec2.tf

--- terraform/modules/kubernetes/sg_ec2.tf
+++ terraform/modules/kubernetes/sg_ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "kubernetes_ec2" {
+sresource "aws_security_group" "kubernetes_ec2" {
   description = "Allow access to incoming kubernetes traffic"
   vpc_id = "${var.vpc_id}"
 
@@ -25,11 +25,11 @@ resource "aws_security_group_rule" "kubernetes_elb" {
   security_group_id = "${aws_security_group.kubernetes_ec2.id}"
 }
 
-resource "aws_security_group_rule" "bosh" {
+resource "aws_security_group_rule" "consul_dns" {
   type = "ingress"
-  from_port = 0
-  to_port = 0
-  protocol = -1
+  from_port = 53
+  to_port = 53
+  protocol = "udp"
   source_security_group_id = "${var.target_bosh_security_group}"
   security_group_id = "${aws_security_group.kubernetes_ec2.id}"
 }
