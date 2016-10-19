3ba30378ab6fce6b315223796a65a98f56fc1bd1
Roger Steve Ruiz <roger.ruiz@gsa.gov>
2016-10-19 15:52:49 -0400

    Add zoneid output to shibboleth module and stacks

--- terraform/modules/shibboleth/outputs.tf
+++ terraform/modules/shibboleth/outputs.tf
@@ -8,3 +8,7 @@ output "shibboleth_elb_name" {
 output "shibboleth_elb_dns_name" {
   value = "${aws_elb.shibboleth_elb_main.dns_name}"
 }
+
+output "shibboleth_elb_zone_id" {
+  value = "${aws_elb.shibboleth_elb_main.zone_id}"
+}
