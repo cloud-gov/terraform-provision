2133fd1de615207bc6607bffe6d85ae1df311e16
Joshua Carp <jm.carp@gmail.com>
2016-11-28 13:50:34 -0500

    Block outbound traffic on rds security groups. (#141)

--- terraform/modules/rds_network/sg_mysql.tf
+++ terraform/modules/rds_network/sg_mysql.tf
@@ -4,7 +4,6 @@
  *   vpc_id
  */
 
-
 resource "aws_security_group" "rds_mysql" {
   description = "Allow access to incoming mysql traffic"
   vpc_id = "${var.vpc_id}"
@@ -13,18 +12,17 @@ resource "aws_security_group" "rds_mysql" {
     from_port = 3306
     to_port = 3306
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    security_groups = ["${var.bosh_security_group}"]
   }
 
   egress {
     from_port = 0
     to_port = 0
     protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    security_groups = ["${var.bosh_security_group}"]
   }
 
-  tags =  {
+  tags = {
     Name = "${var.stack_description} - Incoming MySQL Traffic"
   }
-
 }
