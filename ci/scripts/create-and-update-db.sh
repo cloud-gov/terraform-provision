2aaae05a992b407105a65d85b9df009d98bfbfa8
Roger Steve Ruiz <roger.ruiz@gsa.gov>
2017-01-03 21:12:05 -0500

    Make changes to totp_seed in a single transaction

--- ci/scripts/create-and-update-db.sh
+++ ci/scripts/create-and-update-db.sh
@@ -27,19 +27,21 @@ for db in ${DATABASES}; do
   # Special case for Shibboelth, create FK between totp_seed and users and CASCADE on delete
   if [ "${db}" = "uaadb" ]; then
     psql_adm -d "${db}" <<-EOT
-    CREATE TABLE IF NOT EXISTS totp_seed
-      (
-        username varchar(255) PRIMARY KEY,
-        seed varchar(36),
-        backup_code varchar(36)
-      )
-EOT
-    psql_adm -d "${db}" <<-EOT
+    BEGIN;
+      CREATE TABLE IF NOT EXISTS totp_seed
+        (
+          username varchar(255) PRIMARY KEY,
+          seed varchar(36),
+          backup_code varchar(36)
+        );
+      ALTER TABLE IF EXISTS totp_seed
+        DROP CONSTRAINT username_record_keeper;
       ALTER TABLE IF EXISTS totp_seed
         ADD CONSTRAINT username_record_keeper
           FOREIGN KEY (username)
           REFERENCES users (username)
-          ON DELETE CASCADE
+          ON DELETE CASCADE;
+    COMMIT;
 EOT
 
     psql_adm -d "${db}" <<-EOT
