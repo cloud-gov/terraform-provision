dc7b802e83ccad79e291f35270b4e46905844aae
Chris Nelson <christopher.nelson@gsa.gov>
2017-01-12 11:48:03 -0800

    move policy to it's own file

--- terraform/modules/iam_role/riemann_monitoring/role.tf
+++ terraform/modules/iam_role/riemann_monitoring/role.tf
@@ -1,44 +1,19 @@
+data "template_file" "policy" {
+  template = "${file("${path.module}/policy.json")}"
+
+  vars {
+    aws_partition = "${var.aws_partition}"
+    aws_default_region = "${var.aws_default_region}"
+    account_id = "${var.account_id}"
+  }
+}
+
 module "riemann_monitoring" {
   source = ".."
 
   role_name = "${var.role_name}"
-  iam_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "logs:DescribeLogGroups",
-        "logs:DescribeLogStreams"
-      ],
-      "Resource": [
-        "arn:${var.aws_partition}:logs:${var.aws_default_region}:${var.account_id}:*"
-      ]
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "ec2:DescribeInstances",
-        "ec2:CreateSnapshot",
-        "ec2:CreateTags"
-      ],
-      "Resource": [
-        "*"
-      ]
-    },
-    {
-        "Effect": "Allow",
-        "Action": [
-            "ec2:StopInstances"
-        ],
-        "Resource": [
-          "arn:${var.aws_partition}:ec2:${var.aws_default_region}:${var.account_id}:*"
-        ]
-    }
-  ]
-}
-EOF
+
+  iam_policy = "${data.template_file.policy.rendered}"
 }
 
 
