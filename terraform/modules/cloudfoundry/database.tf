760c10ae2a19a13989c0052b81eb1a9e692b4620
Joshua Carp <jm.carp@gmail.com>
2017-02-27 10:25:22 -0500

    Add postgres 9.6 instances for cloud foundry.

--- terraform/modules/cloudfoundry/database.tf
+++ terraform/modules/cloudfoundry/database.tf
@@ -13,3 +13,19 @@ module "cf_database" {
     rds_security_groups = "${var.rds_security_groups}"
     rds_encrypted = true
 }
+
+module "cf_database_96" {
+    source = "../rds"
+
+    stack_description = "${var.stack_description}"
+    rds_instance_type = "${var.rds_instance_type}"
+    rds_db_size = "${var.rds_db_size}"
+    rds_db_engine = "${var.rds_db_engine}"
+    rds_db_engine_version = "${var.rds_96_db_engine_version}"
+    rds_db_name = "${var.rds_db_name}"
+    rds_username = "${var.rds_username}"
+    rds_password = "${var.rds_password}"
+    rds_subnet_group = "${var.rds_subnet_group}"
+    rds_security_groups = "${var.rds_security_groups}"
+    rds_encrypted = true
+}
