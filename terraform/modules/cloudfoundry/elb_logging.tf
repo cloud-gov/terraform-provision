847db5fd8232d13afcd0fa8de1d385f1e53d4cf9
Joshua Carp <jm.carp@gmail.com>
2017-04-28 14:18:22 -0400

    Check gorouter health from load balancer. (#234)

--- terraform/modules/cloudfoundry/elb_logging.tf
+++ terraform/modules/cloudfoundry/elb_logging.tf
@@ -15,12 +15,12 @@ resource "aws_elb" "cloudfoundry_elb_logging" {
   health_check {
     healthy_threshold = 2
     interval = 5
-    target = "TCP:80"
+    target = "HTTP:81/"
     timeout = 4
     unhealthy_threshold = 3
   }
 
-  tags = {
+  tags {
     Name = "${var.stack_description}-CloudFoundry-Logging"
   }
 }
