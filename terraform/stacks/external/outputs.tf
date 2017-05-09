4fdee6bc9806b21ba83827a93d835c1759ca8ff3
Roger Steve Ruiz <roger.ruiz@gsa.gov>
2017-05-09 13:03:37 -0400

    Remove externals

--- terraform/stacks/external/outputs.tf
+++ terraform/stacks/external/outputs.tf
@@ -3,10 +3,10 @@ output "cdn_broker_username" {
   value = "${module.cdn_broker.username}"
 }
 output "cdn_broker_access_key_id_prev" {
-  value = "${module.cdn_broker.access_key_id_prev}"
+  value = ""
 }
 output "cdn_broker_secret_access_key_prev" {
-  value = "${module.cdn_broker.secret_access_key_prev}"
+  value = ""
 }
 output "cdn_broker_access_key_id_curr" {
   value = "${module.cdn_broker.access_key_id_curr}"
@@ -20,10 +20,10 @@ output "limit_check_username" {
   value = "${module.limit_check_user.username}"
 }
 output "limit_check_access_key_id_prev" {
-  value = "${module.limit_check_user.access_key_id_prev}"
+  value = ""
 }
 output "limit_check_secret_access_key_prev" {
-  value = "${module.limit_check_user.secret_access_key_prev}"
+  value = ""
 }
 output "limit_check_access_key_id_curr" {
   value = "${module.limit_check_user.access_key_id_curr}"
