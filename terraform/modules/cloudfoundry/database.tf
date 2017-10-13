a572f7147484b08e66ee4c31581041014e20f68e
James C. Scott <jamesc.scott@gsa.gov>
2017-10-13 15:45:42 -0400

    Bump prod postgres to 9.6.3

--- terraform/modules/cloudfoundry/database.tf
+++ terraform/modules/cloudfoundry/database.tf
@@ -5,7 +5,7 @@ module "cf_database_96" {
     rds_instance_type = "${var.rds_instance_type}"
     rds_db_size = "${var.rds_db_size}"
     rds_db_engine = "${var.rds_db_engine}"
-    rds_db_engine_version = "${var.stack_prefix == "cf-production" ? "9.6.2" : var.rds_db_engine_version}"
+    rds_db_engine_version = "${var.rds_db_engine_version}"
     rds_db_name = "${var.rds_db_name}"
     rds_username = "${var.rds_username}"
     rds_password = "${var.rds_password}"
