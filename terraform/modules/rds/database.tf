9ea99f60d46fbb13d853169f3fa444f0ba10b593
Will Woodson <william.woodson@gsa.gov>
2018-02-09 13:06:04 -0700

    Restore prevent_destroy on RDS instances

--- terraform/modules/rds/database.tf
+++ terraform/modules/rds/database.tf
@@ -21,7 +21,7 @@ resource "aws_db_instance" "rds_database" {
 
   lifecycle {
     ignore_changes = ["identifier"]
-    prevent_destroy = false
+    prevent_destroy = true
   }
   identifier = "${var.stack_description}-${element(split("-", uuid()),4)}"
 
