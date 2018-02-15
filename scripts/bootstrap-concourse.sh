85da8ca145379a5a2c73aaea2f699a6b431c39db
Joshua Carp <jm.carp@gmail.com>
2018-02-14 19:15:31 -0500

    Simplify bootstrap terraform and script variable collection.

--- scripts/bootstrap-concourse.sh
+++ scripts/bootstrap-concourse.sh
@@ -1,7 +1,36 @@
+#!/bin/bash
+
+set -eux
+
+export TF_VAR_az1="us-gov-west-1"
+
+workspace=$(mktemp)
+
+aws ec2 create-key-pair --key-name bootstrap > ${workspace}/aws-keypair.json
+
+# Provision terraform infrastructure
+terraform init
+terraform apply ./terraform/stacks/bootstrap
+terraform output -json > ${workspace}/terraform-outputs.json
+
+# Collect AWS variables
+bosh interpolate \
+  ./bosh/varsfiles/collect-aws-variables.yml \
+  -l ${workspace}/terraform-outputs.json \
+  -l ${workspace}/aws-keypair.json \
+  > ${workspace}/aws-variables.yml
+
+# Deploy bootstrap concourse
 bosh create-env ../concourse-deployment/lite/concourse.yml \
   --state bootstrap-concourse-state.json \
-  --vars-store bootstrap-concourse-creds.yml \
+  --vars-store ${workspace}/bootstrap-concourse-creds.yml \
   -o ../concourse-deployment/lite/infrastructures/aws.yml \
   -o ./bosh/opsfiles/basic-auth.yml \
   -o ./bosh/opsfiles/self-signed-tls.yml \
   -l ../concourse-deployment/versions.yml \
+  -l ${workspace}/aws-variables.yml \
+  -v region=${AWS_DEFAULT_REGION} \
+  -v default_key_name=bootstrap \
+  -v az=${TF_VAR_az1} \
+  -v access_key_id=${AWS_ACCESS_KEY_ID} \
+  -v secret_access_key=${AWS_SECRET_ACCESS_KEY}
---
+++ terraform/stacks/bootstrap/bootstrap.tf
@@ -0,0 +1,61 @@
+variable "az1" {}
+
+variable "ingress_cidrs" {
+  type = "list"
+  default = ["159.142.0.0/16"]
+}
+
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "${var.az1}"
+}
+
+resource "aws_security_group" "bootstrap" {
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["${var.ingress_cidrs}"]
+  }
+
+  ingress {
+    from_port = 4443
+    to_port = 4443
+    protocol = "tcp"
+    cidr_blocks = ["${var.ingress_cidrs}"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_eip" "bootstrap" {
+  vpc = true
+}
+
+output "public_subnet_id" {
+  value = "${aws_default_subnet.default_az1.id}"
+}
+
+output "public_subnet_cidr" {
+  value = "${aws_default_subnet.default_az1.cidr_block}"
+}
+
+output "public_subnet_gateway" {
+  value = "${cidrhost("${aws_default_subnet.default_az1.cidr_block}", 1)}"
+}
+
+output "private_ip" {
+  value = "${cidrhost("${aws_default_subnet.default_az1.cidr_block}", 100)}"
+}
+
+output "public_ip" {
+  value = "${aws_eip.bootstrap.public_ip}"
+}
+
+output "security_group_id" {
+  value = "${aws_security_group.bootstrap.id}"
+}
