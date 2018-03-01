1fcc77d45da11aa0a28baf9463fa2866dfd8b841
Will Woodson <william.woodson@gsa.gov>
2018-02-28 17:05:53 -0700

    complete minimal main concourse deploy in 07-deploy-concourse.sh

--- scripts/bootstrap/07-deploy-concourse.sh
+++ scripts/bootstrap/07-deploy-concourse.sh
@@ -4,10 +4,16 @@ set -eux
 
 fly --target bootstrap set-pipeline \
   --pipeline deploy-concourse \
-  --config ../cg-deploy-concourse/ci/pipeline.yml \
+  --config ../cg-deploy-concourse/ci/pipeline-development.yml \
   --load-vars-from ${WORKSPACE_DIR}/concourse-environment.yml \
-  --load-vars-from ../cg-deploy-concourse/ci/concourse-defaults.yml
+  --load-vars-from ../cg-deploy-concourse/ci/concourse-defaults.yml \
+  --var concourse-production-private-bucket=${VARZ_BUCKET} \
+  --var concourse-production-deployment-bosh-target=$(bosh interpolate ${WORKSPACE_DIR}/tooling-state.yml --path /terraform_outputs/tooling_bosh_static_ip) \
+  --var concourse-production-private-passphrase=${CONCOURSE_SECRETS_PASSPHRASE} \
+  --var tf-state-bucket=${TF_STATE_BUCKET} \
+  --var slack-webhook-url=${SLACK_WEBHOOK_URL} \
+  --var concourse-config-git-branch=concourse-defaults
 fly --target bootstrap unpause-pipeline --pipeline deploy-concourse
 
-fly --target bootstrap trigger-job --job deploy-concourse/deploy-concourse-staging --watch
 fly --target bootstrap trigger-job --job deploy-concourse/deploy-concourse-production --watch
+
