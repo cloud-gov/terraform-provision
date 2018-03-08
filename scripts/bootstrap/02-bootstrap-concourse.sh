c605932becfa1d1d55d9436bcfa4c414adc52654
timothy-spencer <timothy.spencer@gsa.gov>
2018-03-07 16:23:52 -0800

    try to increase the speed of all the work by bumping instance size up

--- scripts/bootstrap/02-bootstrap-concourse.sh
+++ scripts/bootstrap/02-bootstrap-concourse.sh
@@ -17,6 +17,7 @@ bosh create-env ../concourse-deployment/lite/concourse.yml \
   -o ./bosh/opsfiles/basic-auth.yml \
   -o ./bosh/opsfiles/self-signed-tls.yml \
   -o ./bosh/opsfiles/iam-instance-profile.yml \
+  -o ./bosh/opsfiles/bootstrap-instance-size.yml \
   -l ../concourse-deployment/versions.yml \
   -o ./bosh/opsfiles/ssh-tunnel.yml \
   -o ./bosh/opsfiles/vip-network.yml \
