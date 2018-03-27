ba15d1e8b88f1ebf88b515901257c8753c24ea92
Joshua Carp <jm.carp@gmail.com>
2018-03-27 15:25:18 -0400

    Fix references for rds alert user. (#386)

--- terraform/modules/iam_user/rds_storage_alert/outputs.tf
+++ terraform/modules/iam_user/rds_storage_alert/outputs.tf
@@ -1,9 +1,9 @@
 output "username" {
   value = "${var.username}"
 }
-output "access_key_id_curr" {
-  value = "${aws_iam_access_key.iam_access_key_v3.id}"
+output "access_key_id" {
+  value = "${aws_iam_access_key.iam_access_key.id}"
 }
-output "secret_access_key_curr" {
-  value = "${aws_iam_access_key.iam_access_key_v3.secret}"
+output "secret_access_key" {
+  value = "${aws_iam_access_key.iam_access_key.secret}"
 }
