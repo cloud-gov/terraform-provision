6558f47db8b6ae982e41dfe5574d1c8ea9d337d0
Steven Harms <steven.harms@gsa.gov>
2018-03-27 14:47:40 -0400

    Add rds storage user to iam

---
+++ terraform/modules/iam_user/rds_storage_alert/outputs.tf
@@ -0,0 +1,9 @@
+output "username" {
+  value = "${var.username}"
+}
+output "access_key_id_curr" {
+  value = "${aws_iam_access_key.iam_access_key_v3.id}"
+}
+output "secret_access_key_curr" {
+  value = "${aws_iam_access_key.iam_access_key_v3.secret}"
+}
