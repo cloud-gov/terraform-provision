422e288ac8f98b334e785ae1ca38b2543ddbe77f
timothy-spencer <timothy.spencer@gsa.gov>
2018-03-29 15:48:19 -0700

    got an opsfile to edit the terraform pipeline properly

--- scripts/bootstrap/03-main-terraform.sh
+++ scripts/bootstrap/03-main-terraform.sh
@@ -15,14 +15,16 @@ fly --target bootstrap login \
 fly --target bootstrap sync
 
 # TODO: Fix worker tagging
-bosh int ${TERRAFORM_PIPELINE_FILE} --ops-file bosh/opsfiles/development.yml | sed 's/\[iaas\]//g' > ${WORKSPACE_DIR}/cg-provision-pipeline.yml
+bosh int ${TERRAFORM_PIPELINE_FILE} --ops-file bosh/opsfiles/${DEPLOY_ENV}.yml | sed 's/\[iaas\]//g' > ${WORKSPACE_DIR}/cg-provision-pipeline.yml
 
 # Set terraform-provision pipeline
 fly --target bootstrap set-pipeline \
   --pipeline terraform-provision \
   --config ${WORKSPACE_DIR}/cg-provision-pipeline.yml \
   --load-vars-from ci/concourse-defaults.yml \
-  --load-vars-from ${TERRAFORM_PROVISION_CREDENTIALS_FILE}
+  --load-vars-from ${TERRAFORM_PROVISION_CREDENTIALS_FILE} \
+  --load-vars-from provision-meta-${DEPLOY_ENV}.yml
+
 fly --target bootstrap unpause-pipeline --pipeline terraform-provision
 
 # Ensure tf has a bucket for state
