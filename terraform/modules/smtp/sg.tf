85fa8dbb02bcfc4f60ee2ae1dc7010588e51b662
timothy-spencer <timothy.spencer@gsa.gov>
2018-04-05 13:30:21 -0700

    allow smtp to get out to 587

--- terraform/modules/smtp/sg.tf
+++ terraform/modules/smtp/sg.tf
@@ -9,12 +9,12 @@ resource "aws_security_group" "smtp_inbound" {
     protocol = "tcp"
   }
 
-  # egress {
-  #   from_port = 0
-  #   to_port = 0
-  #   protocol = "-1"
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
+  egress {
+    from_port = 0
+    to_port = 587
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags {
     Name = "${var.stack_description} - smtp inbound"
