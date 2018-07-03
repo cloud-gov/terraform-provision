05911473532ed4837d01350572ba5839465d1790
Jon Carmack <jonathan.carmack@gsa.gov>
2018-07-03 14:58:20 -0400

    More copy and paste errors

--- terraform/modules/credhub/outputs.tf
+++ terraform/modules/credhub/outputs.tf
@@ -1,28 +1,28 @@
 /* RDS credhub Instance */
 output "credhub_rds_identifier" {
-  value = "${module.rds_96.rds_identifier}"
+  value = "${module.credhub.rds_identifier}"
 }
 
 output "credhub_rds_name" {
-  value = "${module.rds_96.rds_name}"
+  value = "${module.credhub.rds_name}"
 }
 
 output "credhub_rds_host" {
-  value = "${module.rds_96.rds_host}"
+  value = "${module.credhub.rds_host}"
 }
 
 output "credhub_rds_port" {
-  value = "${module.rds_96.rds_port}"
+  value = "${module.credhub.rds_port}"
 }
 
 output "credhub_rds_url" {
-  value = "${module.rds_96.rds_url}"
+  value = "${module.credhub.rds_url}"
 }
 
 output "credhub_rds_username" {
-  value = "${module.rds_96.rds_username}"
+  value = "${module.credhub.rds_username}"
 }
 
 output "credhub_rds_password" {
-  value = "${module.rds_96.rds_password}"
+  value = "${module.credhub.rds_password}"
 }
