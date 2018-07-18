540689325140df4ded94466d99aea638875387fd
Joshua Carp <jm.carp@gmail.com>
2018-07-18 14:15:45 -0400

    Add a vpc isolation segment to development.

--- terraform/stacks/isolation-segment/stack.tf
+++ terraform/stacks/isolation-segment/stack.tf
@@ -14,10 +14,30 @@ data "terraform_remote_state" "target_vpc" {
   }
 }
 
+data "aws_caller_identity" "current" {}
+
 module "vpc" {
   source = "../../modules/bosh_vpc"
+
+  stack_description = "${var.stack_description}"
+  vpc_cidr = "${var.vpc_cidr}"
+  public_cidr_1 = "${cidrsubnet(var.vpc_cidr, 8, 100)}"
+  public_cidr_2 = "${cidrsubnet(var.vpc_cidr, 8, 101)}"
+  private_cidr_1 = "${cidrsubnet(var.vpc_cidr, 8, 1)}"
+  private_cidr_2 = "${cidrsubnet(var.vpc_cidr, 8, 2)}"
+  restricted_ingress_web_cidrs = []
 }
 
 module "vpc_peering" {
   source = "../../modules/vpc_peering"
+
+  peer_owner_id = "${data.aws_caller_identity.current.account_id}"
+  target_vpc_id = "${data.terraform_remote_state.target_vpc.vpc_id}"
+  target_vpc_cidr = "${data.terraform_remote_state.target_vpc.vpc_cidr}"
+  target_az1_route_table = "${data.terraform_remote_state.target_vpc.private_route_table_az1}"
+  target_az2_route_table = "${data.terraform_remote_state.target_vpc.private_route_table_az2}"
+  source_vpc_id = "${module.vpc.vpc_id}"
+  source_vpc_cidr = "${module.vpc.vpc_cidr}"
+  source_az1_route_table = "${module.vpc.private_route_table_az1}"
+  source_az2_route_table = "${module.vpc.private_route_table_az2}"
 }
