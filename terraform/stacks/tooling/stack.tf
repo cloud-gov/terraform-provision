0a582856b054caba096a6dc1aace6a0222fc6659
Steven Harms <steven.harms@gsa.gov>
2018-07-23 22:18:23 -0400

    Switch tooling to nat gateway service

--- terraform/stacks/tooling/stack.tf
+++ terraform/stacks/tooling/stack.tf
@@ -73,6 +73,7 @@ module "stack" {
   rds_security_groups = ["${module.stack.bosh_security_group}"]
   rds_security_groups_count = "1"
   use_nat_gateway_eip = "${var.use_nat_gateway_eip}"
+  use_nat_gateway_service = "${var.use_nat_gateway_service}"
 }
 
 module "concourse_production" {
