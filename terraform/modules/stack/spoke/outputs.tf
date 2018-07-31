faf9406285253d4a25c4a9ab2a91cff2a3986c60
Roger Steve Ruiz <roger.ruiz@gsa.gov>
2018-07-31 16:59:15 -0400

    Output from the base module not from other module;
    These a few layers at work here that I don't quite understand.

--- terraform/modules/stack/spoke/outputs.tf
+++ terraform/modules/stack/spoke/outputs.tf
@@ -123,21 +123,21 @@ output "bosh_rds_password" {
  */
 
 output "credhub_rds_host" {
-  value = "${module.credhub_rds.rds_host}"
+  value = "${module.base.credhub_rds_host}"
 }
 
 output "credhub_rds_port" {
-  value = "${module.credhub_rds.rds_port}"
+  value = "${module.base.credhub_rds_port}"
 }
 
 output "credhub_rds_url" {
-  value = "${module.credhub_rds.rds_url}"
+  value = "${module.base.credhub_rds_url}"
 }
 
 output "credhub_rds_username" {
-  value = "${module.credhub_rds.rds_username}"
+  value = "${module.base.credhub_rds_username}"
 }
 
 output "credhub_rds_password" {
-  value = "${module.credhub_rds.rds_password}"
+  value = "${module.base.credhub_rds_password}"
 }
