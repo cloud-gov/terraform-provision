2d70b9d6be57e8cc56a028f4f82bd1cc6be568be
M. Adam Kendall <michael.kendall@gsa.gov>
2018-09-12 16:48:27 -0400

    Point admin-ui to the lb

--- terraform/stacks/dns/stack.tf
+++ terraform/stacks/dns/stack.tf
@@ -290,6 +290,50 @@ resource "aws_route53_record" "cloud_gov_star_app_cloud_gov_aaaa" {
   }
 }
 
+resource "aws_route53_record" "cloud_gov_admin_fr_cloud_gov_a" {
+  zone_id = "${aws_route53_zone.cloud_gov_zone.zone_id}"
+  name = "admin.fr.cloud.gov."
+  type = "A"
+  alias {
+    name = "dualstack.${data.terraform_remote_state.production.admin_lb_dns_name}"
+    zone_id = "${var.cloudfront_zone_id}"
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "cloud_gov_admin_fr_cloud_gov_aaaa" {
+  zone_id = "${aws_route53_zone.cloud_gov_zone.zone_id}"
+  name = "admin.fr.cloud.gov."
+  type = "AAAA"
+  alias {
+    name = "dualstack.${data.terraform_remote_state.production.admin_lb_dns_name}"
+    zone_id = "${var.cloudfront_zone_id}"
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "cloud_gov_admin_fr-stage_cloud_gov_a" {
+  zone_id = "${aws_route53_zone.cloud_gov_zone.zone_id}"
+  name = "admin.fr-stage.cloud.gov."
+  type = "A"
+  alias {
+    name = "dualstack.${data.terraform_remote_state.staging.admin_lb_dns_name}"
+    zone_id = "${var.cloudfront_zone_id}"
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "cloud_gov_admin_fr-stage_cloud_gov_aaaa" {
+  zone_id = "${aws_route53_zone.cloud_gov_zone.zone_id}"
+  name = "admin.fr-stage.cloud.gov."
+  type = "AAAA"
+  alias {
+    name = "dualstack.${data.terraform_remote_state.staging.admin_lb_dns_name}"
+    zone_id = "${var.cloudfront_zone_id}"
+    evaluate_target_health = false
+  }
+}
+
 resource "aws_route53_record" "cloud_gov_ci-stage_fr_cloud_gov_a" {
   zone_id = "${aws_route53_zone.cloud_gov_zone.zone_id}"
   name = "ci.fr-stage.cloud.gov."
