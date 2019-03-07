d282d9209ba75892099e86447bf07c5834cdd003
Jon Carmack <jonathan.carmack@gsa.gov>
2019-03-07 13:47:19 -0500

    Move to tooling stack

--- terraform/stacks/tooling/stack.tf
+++ terraform/stacks/tooling/stack.tf
@@ -26,6 +26,12 @@ resource "aws_lb" "main" {
   security_groups = ["${module.stack.restricted_web_traffic_security_group}"]
   ip_address_type = "dualstack"
   idle_timeout = 3600
+
+  access_logs {
+    bucket        = "cloud-gov-elb-logs"
+    bucket_prefix = "{var.stack_description}"
+    interval      = 5
+  }
 }
 
 resource "aws_lb_listener" "main" {
