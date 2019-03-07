fd633e5d9583691670fe849d6cec2efd2f4e27d6
Jon Carmack <jonathan.carmack@gsa.gov>
2019-03-07 15:52:49 -0500

    Try a map...

--- terraform/stacks/main/stack.tf
+++ terraform/stacks/main/stack.tf
@@ -37,11 +37,12 @@ resource "aws_lb" "main" {
   ip_address_type = "dualstack"
   idle_timeout = 3600
   
-  access_logs {
-    bucket        = "cloud-gov-elb-logs-${var.stack_description}"
-    bucket_prefix = "${var.stack_description}"
-    interval      = 5
-  }
+  access_logs =  [
+    {
+      bucket        = "cloud-gov-elb-logs"
+      bucket_prefix = "${var.stack_description}"
+      interval      = 5
+    },
 }
 
 resource "aws_lb_listener" "main" {
