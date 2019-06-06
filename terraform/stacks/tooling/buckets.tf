b74bf143056f582cb4219431e8073180da29c415
Ben Berry <ben.berry@gsa.gov>
2019-06-06 15:44:37 -0700

    pass variables explicitly

--- terraform/stacks/tooling/buckets.tf
+++ terraform/stacks/tooling/buckets.tf
@@ -48,4 +48,5 @@ module "cg_binaries_bucket" {
 module "log_bucket" {
   source = "../../modules/log_bucket"
   aws_partition = "${data.aws_partition.current.partition}"
+  log_bucket_name = "${var.log_bucket_name}"
 }
