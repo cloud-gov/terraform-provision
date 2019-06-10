9808ea407a01cce5ee21bb14b6e987324891f247
Ben Berry <ben.berry@gsa.gov>
2019-06-10 12:09:17 -0700

    aws_region != aws_partion
    refer to region where we mean region

--- terraform/stacks/main/buckets.tf
+++ terraform/stacks/main/buckets.tf
@@ -9,4 +9,5 @@ module "log_bucket" {
   source = "../../modules/log_bucket"
   aws_partition = "${data.aws_partition.current.partition}"
   log_bucket_name = "${var.log_bucket_name}"
+  aws_region = "${data.aws_region.current}"
 }
