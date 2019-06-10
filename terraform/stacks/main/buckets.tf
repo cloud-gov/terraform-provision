315fb3a32608055d010e29a9aaf5cedb1b912ee7
Ben Berry <ben.berry@gsa.gov>
2019-06-10 12:14:23 -0700

    reference region variable name

--- terraform/stacks/main/buckets.tf
+++ terraform/stacks/main/buckets.tf
@@ -9,5 +9,5 @@ module "log_bucket" {
   source = "../../modules/log_bucket"
   aws_partition = "${data.aws_partition.current.partition}"
   log_bucket_name = "${var.log_bucket_name}"
-  aws_region = "${data.aws_region.current}"
+  aws_region = "${data.aws_region.current.name}"
 }
