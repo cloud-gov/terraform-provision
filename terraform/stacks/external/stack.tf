9b567e3d6f9b4b81eebd094f0098f4319e7d94a5
Mike Lloyd <mike.lloyd@gsa.gov>
2019-11-20 12:33:13 -0700

    updating credentials.
    * adding aws commercial user with route53 permissions to the cloud.gov
    hosted zone.
    * outputting the important aspects from above.
    * updating ELB broker permissions.
    
    Signed-off-by: Mike Lloyd <mike.lloyd@gsa.gov>

--- terraform/stacks/external/stack.tf
+++ terraform/stacks/external/stack.tf
@@ -31,3 +31,10 @@ module "lets_encrypt_user" {
   hosted_zone = "${var.lets_encrypt_hosted_zone}"
   username = "lets-encrypt-${var.stack_description}"
 }
+
+module "domain_broker_v2_user" {
+  source = "../../modules/iam_user/lets_encrypt"
+  aws_partition = "${data.aws_partition.current.partition}"
+  hosted_zone = "${var.lets_encrypt_hosted_zone}"
+  username = "domain-broker-v2-${var.stack_description}"
+}
