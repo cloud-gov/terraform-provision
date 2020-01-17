da1979951530d9cd3f12f9357eba60cbd1fe7a44
Mike Lloyd <mike.lloyd@gsa.gov>
2020-01-17 15:16:00 -0700

    upgrading to aws provider 2.10.0
    Signed-off-by: Mike Lloyd <mike.lloyd@gsa.gov>

--- terraform/stacks/dns/stack.tf
+++ terraform/stacks/dns/stack.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 provider "aws" {
-  version = "~> 2.0.0"
+  version = "~> 2.10.0"
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
   region = "${var.aws_region}"
