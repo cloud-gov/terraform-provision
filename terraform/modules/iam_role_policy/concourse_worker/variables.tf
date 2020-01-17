86d9ab5d80976d40a35b9d6b49635e25d20f0b70
Mike Lloyd <mike.lloyd@gsa.gov>
2020-01-17 11:44:46 -0700

    testing new concourse worker policy.
    Signed-off-by: Mike Lloyd <mike.lloyd@gsa.gov>

--- terraform/modules/iam_role_policy/concourse_worker/variables.tf
+++ terraform/modules/iam_role_policy/concourse_worker/variables.tf
@@ -1,6 +1,5 @@
 variable "policy_name" {}
 variable "aws_partition" {}
-
 variable "varz_bucket" {}
 variable "varz_staging_bucket" {}
 variable "bosh_release_bucket" {}
@@ -10,3 +9,4 @@ variable "buildpack_notify_bucket" {}
 variable "billing_bucket" {}
 variable "cg_binaries_bucket" {}
 variable "log_bucket" {}
+variable "concourse_varz_bucket" {}
