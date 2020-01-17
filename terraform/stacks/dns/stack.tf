1f0ec69fbf4c3eb517ed6fc9e15dd88bf2ce32ab
Mike Lloyd <mike.lloyd@gsa.gov>
2020-01-17 14:54:21 -0700

    updating stack version to 1.60.0
    Signed-off-by: Mike Lloyd <mike.lloyd@gsa.gov>

--- terraform/stacks/dns/stack.tf
+++ terraform/stacks/dns/stack.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 provider "aws" {
-  version = "~> 1.39.0"
+  version = "~> 1.60.0"
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
   region = "${var.aws_region}"
