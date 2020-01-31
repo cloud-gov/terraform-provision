5e33f4f7cc3db4d031f074a0d91322851bcc5535
Ben Berry <ben.berry@gsa.gov>
2020-01-31 15:35:10 -0800

    add general log to mysql parameter groups

--- terraform/modules/rds/parameter_group.tf
+++ terraform/modules/rds/parameter_group.tf
@@ -38,4 +38,12 @@ resource "aws_db_parameter_group" "parameter_group_mysql" {
     var.rds_parameter_group_name :
     "${replace("${var.stack_description}-${var.rds_db_name}", "/[^a-zA-Z-]+/", "-")}"}"
   family = "${var.rds_parameter_group_family}"
+  parameter {
+    name = "general_log"
+    value = 1
+  }
+  parameter {
+    name = "log_output"
+    value = "FILE"
+  }
 }
