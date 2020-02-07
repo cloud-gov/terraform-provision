4b19d5377c91c609df32c9a43c3c97e3bbe2baec
Eddie A Tejeda <eddie.tejeda@gmail.com>
2020-02-07 17:26:24 -0500

    fix commas and domains

--- terraform/stacks/dns/stack.tf
+++ terraform/stacks/dns/stack.tf
@@ -96,10 +96,11 @@ resource "aws_route53_record" "cloud_gov_google_gsuite_mx" {
   type    = "MX"
   ttl     = 100
   records = [
-    "5 ALT1.ASPMX.L.GOOGLE.COM.",
-    "5 ALT2.ASPMX.L.GOOGLE.COM."
-    "10 ALT1.ASPMX.L.GOOGLE.COM."
-    "10 ALT1.ASPMX.L.GOOGLE.COM."
+    "1         ASPMX.L.GOOGLE.COM.",
+    "5    ALT1.ASPMX.L.GOOGLE.COM.",
+    "5    ALT2.ASPMX.L.GOOGLE.COM.",
+    "10   ALT3.ASPMX.L.GOOGLE.COM.",
+    "10   ALT4.ASPMX.L.GOOGLE.COM."
   ]
 }
 
