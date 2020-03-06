6309a00cbc97c6abcf2647e0da6a2a742115ee2b
Ben Berry <ben.berry@gsa.gov>
2020-03-06 12:05:57 -0800

    pass var to template

--- terraform/modules/iam_role_policy/self_managed_mfa/policy.tf
+++ terraform/modules/iam_role_policy/self_managed_mfa/policy.tf
@@ -1,7 +1,9 @@
 data "template_file" "policy" {
   # from https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_examples_aws_my-sec-creds-self-manage-mfa-only.html
   template = "${file("${path.module}/policy.json")}"
-
+  vars {
+    aws_partition = "${var.aws_partition}"
+  }
 }
 
 resource "aws_iam_policy" "iam_policy" {
