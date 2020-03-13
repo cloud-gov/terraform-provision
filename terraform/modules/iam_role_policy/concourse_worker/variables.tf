62639e99d7c6dddcda8ea0ea7aeb02a5ddc93f4c
Tammer Saleh <tammer@superorbit.al>
2020-03-13 23:31:35 +0000

    Missing var

--- terraform/modules/iam_role_policy/concourse_worker/variables.tf
+++ terraform/modules/iam_role_policy/concourse_worker/variables.tf
@@ -9,4 +9,5 @@ variable "buildpack_notify_bucket" {}
 variable "billing_bucket" {}
 variable "cg_binaries_bucket" {}
 variable "log_bucket" {}
+variable "build_artifact_bucket" {}
 variable "concourse_varz_bucket" {}
