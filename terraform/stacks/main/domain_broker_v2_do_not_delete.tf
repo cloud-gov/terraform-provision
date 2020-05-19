4ca2c2d6ba35f1faba5e3e22c294653182518957
Tammer Saleh <tammer@superorbit.al>
2020-05-19 18:13:41 +0000

    Add big warning about removing the domain broker v2 resources

---
+++ terraform/stacks/main/domain_broker_v2_do_not_delete.tf
@@ -0,0 +1,244 @@
+/* new broker ALB */
+
+// =====================================
+// OK, so DO NOT DELETE THESE RESOURCES
+// =====================================
+// 
+// ...unless you're prepared to spend ~2 days doing terraform state surgery.
+//
+// https://docs.google.com/document/d/19LDEX8ac46JkPgoJUoSAJCf2B0K5D_QpX6O1IdO8w7Q/edit
+//
+// Basically, these resources are pointed at the same AWS resources (LB,
+// listeners, etc) as the other domains_broker resources in this directory.  
+// We attempted to delete these resources.  `terraform plan` looked good, but
+// `terraform apply` got real mad.  
+
+variable "domain_broker_v2_rds_username" {}
+variable "domain_broker_v2_rds_password" {}
+
+variable "domain_broker_v2_alb_count" {
+  default = 0
+}
+
+resource "aws_iam_user" "domain_broker_v2" {
+  name = "domain_broker_v2_${var.stack_description}"
+  path = "/domain-broker-v2/"
+}
+
+resource "aws_iam_access_key" "domain_broker_v2_access_key" {
+  user = "${aws_iam_user.domain_broker_v2.name}"
+}
+
+resource "aws_iam_user_policy" "domain_broker_v2_policy" {
+  name = "domain_broker_v2_policy"
+  user = "${aws_iam_user.domain_broker_v2.name}"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": [
+                "elasticloadbalancing:CreateLoadBalancer",
+                "elasticloadbalancing:DeleteLoadBalancer"
+            ],
+            "Effect": "Deny",
+            "Resource": "*"
+        },
+        {
+            "Action": [
+                "elasticloadbalancing:AddListenerCertificates",
+                "elasticloadbalancing:CreateListener",
+                "elasticloadbalancing:CreateRule",
+                "elasticloadbalancing:CreateLoadBalancerListeners",
+                "elasticloadbalancing:CreateTargetGroup",
+                "elasticloadbalancing:DeleteListener",
+                "elasticloadbalancing:DeleteRule",
+                "elasticloadbalancing:DeleteTargetGroup",
+                "elasticloadbalancing:DeregisterTargets",
+                "elasticloadbalancing:Describe*",
+                "elasticloadbalancing:Modify*",
+                "elasticloadbalancing:RegisterTargets",
+                "elasticloadbalancing:RemoveListenerCertificates"
+            ],
+            "Effect": "Allow",
+            "Resource": "*"
+        },
+        {
+            "Action": [
+                "iam:DeleteServerCertificate",
+                "iam:GetServerCertificate",
+                "iam:ListServerCertificates",
+                "iam:UpdateServerCertificate",
+                "iam:UploadServerCertificate"
+            ],
+            "Effect": "Allow",
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_lb" "domain_broker_v2" {
+  count = "${var.domain_broker_v2_alb_count}"
+
+  name            = "${var.stack_description}-domains-${count.index}"
+  subnets         = ["${module.stack.public_subnet_az1}", "${module.stack.public_subnet_az2}"]
+  security_groups = ["${module.stack.web_traffic_security_group}"]
+  ip_address_type = "dualstack"
+  idle_timeout    = 3600
+
+  access_logs = {
+    bucket = "${var.log_bucket_name}"
+    prefix = "${var.stack_description}"
+  }
+}
+
+resource "aws_lb_listener" "domain_broker_v2_http" {
+  count = "${var.domain_broker_v2_alb_count}"
+
+  load_balancer_arn = "${aws_lb.domain_broker_v2.*.arn[count.index]}"
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = "${aws_lb_target_group.domain_broker_v2_apps.*.arn[count.index]}"
+    type             = "forward"
+  }
+}
+
+resource "aws_lb_listener" "domain_broker_v2_https" {
+  count = "${var.domain_broker_v2_alb_count}"
+
+  load_balancer_arn = "${aws_lb.domain_broker_v2.*.arn[count.index]}"
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
+  certificate_arn   = "${data.aws_iam_server_certificate.wildcard.arn}"
+
+  default_action {
+    target_group_arn = "${aws_lb_target_group.domain_broker_v2_apps.*.arn[count.index]}"
+    type             = "forward"
+  }
+}
+
+resource "aws_lb_listener_rule" "domain_broker_v2static_http" {
+  count = "${var.domain_broker_v2_alb_count}"
+
+  listener_arn = "${aws_lb_listener.domain_broker_v2_http.*.arn[count.index]}"
+
+  action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.domain_broker_v2_challenge.*.arn[count.index]}"
+  }
+
+  condition {
+    path_pattern {
+      values = ["/.well-known/acme-challenge/*"]
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "domain_broker_v2_static_https" {
+  count = "${var.domain_broker_v2_alb_count}"
+
+  listener_arn = "${aws_lb_listener.domain_broker_v2_https.*.arn[count.index]}"
+
+  action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.domain_broker_v2_challenge.*.arn[count.index]}"
+  }
+
+  condition {
+    path_pattern {
+      values = ["/.well-known/acme-challenge/*"]
+    }
+  }
+}
+
+resource "aws_lb_target_group" "domain_broker_v2_apps" {
+  count = "${var.domain_broker_v2_alb_count}"
+
+  name     = "${var.stack_description}-domains-apps-${count.index}"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "${module.stack.vpc_id}"
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+    timeout             = 4
+    interval            = 5
+    port                = 81
+    matcher             = 200
+  }
+}
+
+resource "aws_lb_target_group" "domain_broker_v2_challenge" {
+  count = "${var.domain_broker_v2_alb_count}"
+
+  name     = "${var.stack_description}-domains-acme-${count.index}"
+  port     = 8081
+  protocol = "HTTP"
+  vpc_id   = "${module.stack.vpc_id}"
+
+  health_check {
+    path = "/health"
+  }
+}
+
+resource "aws_db_instance" "domain_broker_v2" {
+  name                   = "domain_broker_v2"
+  storage_type           = "gp2"
+  allocated_storage      = 10
+  instance_class         = "db.t2.micro"
+  username               = "${var.domain_broker_v2_rds_username}"
+  password               = "${var.domain_broker_v2_rds_password}"
+  engine                 = "postgres"
+  db_subnet_group_name   = "${module.stack.rds_subnet_group}"
+  vpc_security_group_ids = ["${module.stack.rds_postgres_security_group}"]
+}
+
+output "domain_broker_v2_rds_username" {
+  value = "${aws_db_instance.domain_broker_v2.username}"
+}
+
+output "domain_broker_v2_rds_password" {
+  value = "${aws_db_instance.domain_broker_v2.password}"
+}
+
+output "domain_broker_v2_rds_address" {
+  value = "${aws_db_instance.domain_broker_v2.address}"
+}
+
+output "domain_broker_v2_rds_port" {
+  value = "${aws_db_instance.domain_broker_v2.port}"
+}
+
+output "domain_broker_v2_alb_names" {
+  value = "${aws_lb.domain_broker_v2.*.name}"
+}
+
+output "domain_broker_v2_target_group_apps_names" {
+  value = "${aws_lb_target_group.domain_broker_v2_apps.*.name}"
+}
+
+output "domain_broker_v2_target_group_challenge_names" {
+  value = "${aws_lb_target_group.domain_broker_v2_challenge.*.name}"
+}
+
+output "domain_broker_v2_listener_arns" {
+  value = "${aws_lb_listener.domain_broker_v2_http.*.arn}"
+}
+
+output "domain_broker_v2_access_key_id" {
+  value = "${aws_iam_access_key.domain_broker_v2_access_key.id}"
+}
+
+output "domain_broker_v2_secret_access_key" {
+  value = "${aws_iam_access_key.domain_broker_v2_access_key.secret}"
+}
+
+/* end new broker alb config */
+
