9958574d6c82fcca740a98dc38308b28b2eb779c
Tammer Saleh <tammer@superorbit.al>
2020-05-29 19:40:02 +0000

    Simplify configuration for external-domain-broker
    - We should NOT be passing in TF_VAR_foo_staging or
      TF_VAR_foo_production variables into our pipeline tasks.  We already
      pass in `STACK_NAME`.  This should be enough.
    - Change values to match what's configured in the broker and its
      pipeline.
    
    [Closes cloud-gov/external-domain-broker#71]

--- terraform/modules/external_domain_broker/resources.tf
+++ terraform/modules/external_domain_broker/resources.tf
@@ -1,5 +1,5 @@
 resource "aws_route53_zone" "zone" {
-  name    = "${var.hosted_zone}"
+  name    = "external-domains-${var.stack_description}.cloud.gov"
   comment = "Hosts TXT and CNAME records for the external-domain-broker"
 }
 
@@ -25,15 +25,14 @@ data "template_file" "policy" {
   template = "${file("${path.module}/policy.json")}"
 
   vars {
-    aws_partition     = "${var.aws_partition}"
     account_id        = "${var.account_id}"
-    cloudfront_prefix = "${var.cloudfront_prefix}"
-    hosted_zone       = "${aws_route53_zone.zone.zone_id}"
+    hosted_zone_id    = "${aws_route53_zone.zone.zone_id}"
+    stack             = "${var.stack_description}"
   }
 }
 
 resource "aws_iam_user" "iam_user" {
-  name = "${var.username}"
+  name = "external-domain-broker-${var.stack_description}"
 }
 
 resource "aws_iam_access_key" "iam_access_key_v3" {
--- terraform/modules/external_domain_broker/variables.tf
+++ terraform/modules/external_domain_broker/variables.tf
@@ -1,5 +1,2 @@
-variable "username" {}
 variable "account_id" {}
-variable "aws_partition" {}
-variable "cloudfront_prefix" {}
-variable "hosted_zone" {}
+variable "stack_description" {}
