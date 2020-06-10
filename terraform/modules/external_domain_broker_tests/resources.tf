2ac88e7a6a02c290195f77872f6150172dc3441c
Ben Berry <ben.berry@gsa.gov>
2020-06-10 11:13:13 -0700

    add user and policy for external domain acceptance tests

---
+++ terraform/modules/external_domain_broker_tests/resources.tf
@@ -0,0 +1,27 @@
+resource "aws_route53_zone" "zone" {
+  name    = "${var.stack_description}.edb.cloud.gov"
+  comment = "Hosts TXT and CNAME records for the external-domain-broker tests"
+}
+
+data "template_file" "policy" {
+  template = "${file("${path.module}/policy.json")}"
+
+  vars {
+    aws_partition = "${var.aws_partition}"
+    hosted_zone = "${data.aws_route53_zone.zone.zone_id}"
+  }
+}
+
+resource "aws_iam_user" "iam_user" {
+  name = "external-domain-broker-tests-${var.stack_description}"
+}
+
+resource "aws_iam_access_key" "iam_access_key_v1" {
+  user = "${aws_iam_user.iam_user.name}"
+}
+
+resource "aws_iam_user_policy" "iam_policy" {
+  name = "${aws_iam_user.iam_user.name}-policy"
+  user = "${aws_iam_user.iam_user.name}"
+  policy = "${data.template_file.policy.rendered}"
+}
---
+++ terraform/modules/external_domain_broker_tests/variables.tf
@@ -0,0 +1,6 @@
+variable "username" {}
+variable "account_id" {}
+variable "aws_partition" {}
+variable "bucket" {}
+variable "cloudfront_prefix" {}
+variable "hosted_zone" {}
