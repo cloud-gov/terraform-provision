ca76661909b818773888ab47c439d95c6fd74ba3
Ben Berry <ben.berry@gsa.gov>
2020-06-10 12:00:33 -0700

    correct user info version

--- terraform/modules/external_domain_broker_tests/outputs.tf
+++ terraform/modules/external_domain_broker_tests/outputs.tf
@@ -8,8 +8,8 @@ output "secret_access_key_prev" {
   value = ""
 }
 output "access_key_id_curr" {
-  value = "${aws_iam_access_key.iam_access_key_v3.id}"
+  value = "${aws_iam_access_key.iam_access_key_v1.id}"
 }
 output "secret_access_key_curr" {
-  value = "${aws_iam_access_key.iam_access_key_v3.secret}"
+  value = "${aws_iam_access_key.iam_access_key_v1.secret}"
 }
