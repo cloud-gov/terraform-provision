c6d1c98e3f0fb52c51713ae84fcd2c4dd76809b2
Ben Berry <ben.berry@gsa.gov>
2020-06-10 13:28:56 -0700

    add ns records for new zone

--- terraform/modules/external_domain_broker_tests/resources.tf
+++ terraform/modules/external_domain_broker_tests/resources.tf
@@ -3,6 +3,19 @@ resource "aws_route53_zone" "zone" {
   comment = "Hosts TXT and CNAME records for the external-domain-broker tests"
 }
 
+resource "aws_route53_record" "record" {
+  name    = "${aws_route53_zone.zone.name}"
+  zone_id = "${data.aws_route53_zone.cloud_gov.zone_id}"
+  type    = "NS"
+  ttl     = "60"
+
+  records = [
+    "${aws_route53_zone.zone.name_servers.0}",
+    "${aws_route53_zone.zone.name_servers.1}",
+    "${aws_route53_zone.zone.name_servers.2}",
+    "${aws_route53_zone.zone.name_servers.3}",
+  ]
+}
 data "template_file" "policy" {
   template = "${file("${path.module}/policy.json")}"
 
