e213250acc8aea7c874ce0461adbd92082888b9e
Van Nguyen <vanh.nguyen@gsa.gov>
2020-11-03 14:38:44 -0500

    Loadbalancer for UAA

--- terraform/stacks/main/outputs.tf
+++ terraform/stacks/main/outputs.tf
@@ -167,10 +167,18 @@ output "cf_apps_lb_dns_name" {
   value = "${module.cf.apps_lb_dns_name}"
 }
 
+output "cf_uaa_lb_name" {
+  value = "${module.cf.uaa_lb_name}"
+}
+output "cf_uaa_lb_dns_name" {
+  value = "${module.cf.uaa_lb_dns_name}"
+}
+
 output "cf_router_target_groups" {
   value = "${concat(
     list("${module.cf.lb_target_group}"),
     list("${module.cf.apps_lb_target_group}"),
+    list("${module.cf.uaa_lb_target_group}"),
     aws_lb_target_group.domains_broker_apps.*.name,
     aws_lb_target_group.domains_broker_challenge.*.name,
     aws_lb_target_group.domain_broker_v2_apps.*.name,
@@ -183,6 +191,10 @@ output "cf_target_group" {
 }
 output "cf_apps_target_group" {
   value = "${module.cf.apps_lb_target_group}"
+}
+
+output "cf_uaa_target_group" {
+  value = "${module.cf.uaa_lb_target_group}"
 }
 
 /* Security Groups */
