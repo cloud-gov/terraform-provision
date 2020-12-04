47f7c9a2ee053d16cd7ba03ff3e7839ca6685d4f
Van Nguyen <vanh.nguyen@gsa.gov>
2020-12-04 13:07:33 -0500

    Added DNS for development uaa/login

--- terraform/stacks/dns/stack.tf
+++ terraform/stacks/dns/stack.tf
@@ -899,6 +899,54 @@ resource "aws_route53_record" "cloud_gov_star_dev_env_aaaa" {
   }
 }
 
+resource "aws_route53_record" "cloud_gov_uaa_dev_env_a" {
+  zone_id = "${aws_route53_zone.cloud_gov_zone.zone_id}"
+  name    = "uaa.dev.us-gov-west-1.aws-us-gov.cloud.gov."
+  type    = "A"
+
+  alias {
+    name                   = "dualstack.${data.terraform_remote_state.development.cf_uaa_lb_dns_name}"
+    zone_id                = "${var.cloudfront_zone_id}"
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "cloud_gov_uaa_dev_env_aaaa" {
+  zone_id = "${aws_route53_zone.cloud_gov_zone.zone_id}"
+  name    = "uaa.dev.us-gov-west-1.aws-us-gov.cloud.gov."
+  type    = "AAAA"
+
+  alias {
+    name                   = "dualstack.${data.terraform_remote_state.development.cf_uaa_lb_dns_name}"
+    zone_id                = "${var.cloudfront_zone_id}"
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "cloud_gov_login_dev_env_a" {
+  zone_id = "${aws_route53_zone.cloud_gov_zone.zone_id}"
+  name    = "login.dev.us-gov-west-1.aws-us-gov.cloud.gov."
+  type    = "A"
+
+  alias {
+    name                   = "dualstack.${data.terraform_remote_state.development.cf_uaa_lb_dns_name}"
+    zone_id                = "${var.cloudfront_zone_id}"
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "cloud_gov_uaa_login_env_aaaa" {
+  zone_id = "${aws_route53_zone.cloud_gov_zone.zone_id}"
+  name    = "login.dev.us-gov-west-1.aws-us-gov.cloud.gov."
+  type    = "AAAA"
+
+  alias {
+    name                   = "dualstack.${data.terraform_remote_state.development.cf_uaa_lb_dns_name}"
+    zone_id                = "${var.cloudfront_zone_id}"
+    evaluate_target_health = false
+  }
+}
+
 resource "aws_route53_record" "cloud_gov_www_cloud_gov_cname" {
   zone_id = "${aws_route53_zone.cloud_gov_zone.zone_id}"
   name    = "www.cloud.gov."
