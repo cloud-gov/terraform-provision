ede1f0c22091a9e20eef53e84e3943107129eece
Van Nguyen <vhnguyen326@gmail.com>
2021-02-09 11:24:19 -0500

    added defaults that terraform12 was complaining about (#805)
    Co-authored-by: Van Nguyen <vanh.nguyen@gsa.gov>

--- terraform/stacks/dns/stack.tf
+++ terraform/stacks/dns/stack.tf
@@ -13,7 +13,7 @@ terraform {
 }
 
 provider "aws" {
-  version    = "~> 2.40"
+  version    = "~> 3.26"
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
   region     = var.aws_region
