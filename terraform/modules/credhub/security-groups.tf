b816e658fb2896a469fe052e8817c837a736e583
Steve Greenberg <steve.greenberg@gsa.gov>
2021-02-25 13:07:07 -0700

    all ingress on health check port 8845

--- terraform/modules/credhub/security-groups.tf
+++ terraform/modules/credhub/security-groups.tf
@@ -25,7 +25,7 @@ resource "aws_security_group_rule" "self_reference" {
 resource "aws_security_group_rule" "credhub" {
   type              = "ingress"
   from_port         = 8844
-  to_port           = 8844
+  to_port           = 8845
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.credhub.id
