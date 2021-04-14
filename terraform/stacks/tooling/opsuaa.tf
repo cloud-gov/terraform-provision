d2a769768820b1aea28b35049509f7e71085246c
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2021-04-14 16:40:47 -0600

    upgrade opsuaa db to 12.4

--- terraform/stacks/tooling/opsuaa.tf
+++ terraform/stacks/tooling/opsuaa.tf
@@ -11,10 +11,10 @@ module "opsuaa_db" {
   rds_password                    = var.opsuaa_rds_password
   rds_subnet_group                = module.stack.rds_subnet_group
   rds_security_groups             = [module.stack.rds_postgres_security_group]
-  rds_db_engine_version           = "9.6.19" #var.rds_db_engine_version
-  rds_parameter_group_family      = "postgres9.6" #var.rds_parameter_group_family
-  rds_allow_major_version_upgrade = "false" #var.rds_allow_major_version_upgrade
-  rds_apply_immediately           = "false" #var.rds_apply_immediately
+  rds_db_engine_version           = var.rds_db_engine_version
+  rds_parameter_group_family      = var.rds_parameter_group_family
+  rds_allow_major_version_upgrade = var.rds_allow_major_version_upgrade
+  rds_apply_immediately           = var.rds_apply_immediately
 }
 
 output "opsuaa_rds_url" {
