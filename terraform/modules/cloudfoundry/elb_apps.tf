6b74637d699780cd4057884ccfcccdbdfac3932c
ChrisMcGowan <christopher.r.mcgowan@gsa.gov>
2021-10-05 15:22:05 -0400

    moving to 25% http 75% https

--- terraform/modules/cloudfoundry/elb_apps.tf
+++ terraform/modules/cloudfoundry/elb_apps.tf
@@ -59,11 +59,11 @@ resource "aws_lb_listener" "cf_apps" {
         }
       target_group {
         arn = aws_lb_target_group.cf_apps_target.arn
-        weight = 50
+        weight = 25
       }
       target_group {
         arn = aws_lb_target_group.cf_apps_target_https.arn
-        weight = 50
+        weight = 75
       }
     }
   }
