46a749e0ada520611f1fd2bf0642717474783442
ChrisMcGowan <christopher.r.mcgowan@gsa.gov>
2021-10-26 11:21:36 -0400

    fix syntax on logs

--- terraform/modules/cloudfoundry/elb_uaa.tf
+++ terraform/modules/cloudfoundry/elb_uaa.tf
@@ -4,10 +4,10 @@ resource "aws_lb" "cf_uaa" {
   security_groups = var.elb_security_groups
   ip_address_type = "dualstack"
   idle_timeout = 3600
-  access_logs = {
-      bucket        = var.log_bucket_name
-      prefix        = var.stack_description
-      enabled       = true
+  access_logs {
+    bucket  = var.log_bucket_name
+    prefix  = var.stack_description
+    enabled = true
   }
 }
 
