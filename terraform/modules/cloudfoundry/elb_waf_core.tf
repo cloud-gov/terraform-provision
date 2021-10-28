5246bd9a66a14db362841e22e4b0d1959b7ea003
ChrisMcGowan <christopher.r.mcgowan@gsa.gov>
2021-10-28 10:17:38 -0400

    1st pass waf acl test

---
+++ terraform/modules/cloudfoundry/elb_waf_core.tf
@@ -0,0 +1,40 @@
+resource "aws_wafv2_web_acl" "cf_waf_core" {
+  name        = "${var.stack_description}-cf_waf_core_acl"
+  description = "WebACL with managed core ruleset"
+  scope       = "REGIONAL"
+
+  default_action {
+    allow {}
+  }
+
+  rule {
+    name     = "AWSManagedRulesCommonRuleSet-rule-1"
+    priority = "1"
+
+    override_action = "none"
+
+    visibility_config = {
+      cloudwatch_metrics_enabled = false
+      metric_name                = "AWSManagedRulesCommonRuleSet-metric"
+      sampled_requests_enabled   = false
+      }
+
+      managed_rule_group_statement = {
+      name        = "AWSManagedRulesCommonRuleSet"
+      vendor_name = "AWS"
+        }
+      }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = false
+      metric_name                = "friendly-rule-metric-name"
+      sampled_requests_enabled   = false
+    }
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = false
+    metric_name                = "friendly-metric-name"
+    sampled_requests_enabled   = false
+  }
+}
