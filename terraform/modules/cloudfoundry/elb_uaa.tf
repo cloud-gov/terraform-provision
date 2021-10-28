0087bfd59ca6229f5072d258f5c697131ea5c490
Van Nguyen <vanh.nguyen@gsa.gov>
2021-10-28 11:35:26 -0400

    updated reference

--- terraform/modules/cloudfoundry/elb_uaa.tf
+++ terraform/modules/cloudfoundry/elb_uaa.tf
@@ -91,5 +91,5 @@ resource "aws_wafv2_web_acl" "cf_uaa_waf_core" {
 
 resource "aws_wafv2_web_acl_association" "cf_uaa_waf_core" {
   resource_arn = aws_lb.cf_uaa.arn
-  web_acl_arn  = aws_wafv2_web_acl.uaa-core.arn
+  web_acl_arn  = aws_wafv2_web_acl.cf_uaa_waf_core.arn
 }
