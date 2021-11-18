a5ef45711cf345c68edeb564bba69e14b609f436
Ben Berry <ben.berry@gsa.gov>
2021-11-18 10:59:03 -0800

    add staging route53 healthchecks

--- terraform/stacks/dns/stack.tf
+++ terraform/stacks/dns/stack.tf
@@ -282,6 +282,52 @@ resource "aws_route53_record" "cloud_gov_star_fr-stage_cloud_gov_aaaa" {
   }
 }
 
+
+####################################################
+# healthchecks zero-downtime deployment assertions #
+####################################################
+
+resource "aws_route53_health_check" "stage_uaa_healthcheck" {
+  fqdn              = "uaa.fr-stage.cloud.gov"
+  port              = 443
+  type              = "HTTPS"
+  resource_path     = "/"
+  failure_threshold = "3"
+  request_interval  = "30"
+}
+
+resource "aws_route53_health_check" "stage_api_healthcheck" {
+  fqdn              = "api.fr-stage.cloud.gov"
+  port              = 443
+  type              = "HTTPS"
+  resource_path     = "/"
+  failure_threshold = "3"
+  request_interval  = "30"
+}
+
+resource "aws_route53_health_check" "stage_app_healthcheck" {
+  fqdn              = "test-python-flask.fr-stage.cloud.gov"
+  port              = 443
+  type              = "HTTPS"
+  resource_path     = "/"
+  failure_threshold = "3"
+  request_interval  = "30"
+}
+
+resource "aws_route_53_health_check" "stage_aggregate_health" {
+  type                   = "CALCULATED"
+  child_health_threshold = 1
+  child_healthchecks     = [aws_route53_health_check.stage_api_healthcheck.id, aws_route53_health_check.stage_app_healthcheck.id, aws_route53_health_check.stage_uaa_healthcheck.id]
+}
+
+output "staging_aggregate_healthcheck_id" {
+  value = aws_route53_health_check.stage_aggregate_health.id
+}
+
+####################
+# end healthchecks #
+####################
+
 resource "aws_route53_record" "cloud_gov_uaa_fr-stage_cloud_gov_a" {
   zone_id = aws_route53_zone.cloud_gov_zone.zone_id
   name    = "uaa.fr-stage.cloud.gov."
