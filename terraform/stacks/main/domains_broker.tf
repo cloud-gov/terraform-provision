fb7548fe920446fc7ef1c6440df4e382c34e8369
Van Nguyen <vanh.nguyen@gsa.gov>
2021-12-14 11:41:02 -0500

    removed stack from web acl reference

--- terraform/stacks/main/domains_broker.tf
+++ terraform/stacks/main/domains_broker.tf
@@ -204,7 +204,7 @@ resource "aws_wafv2_web_acl_association" "domain_waf_core" {
   count = var.domains_broker_alb_count
 
   resource_arn = aws_lb.domains_broker[count.index].arn
-  web_acl_arn  = module.stack.cf.cf_uaa_waf_core_arn
+  web_acl_arn  = module.cf.cf_uaa_waf_core_arn
 }
 
 output "domains_broker_alb_names" {
