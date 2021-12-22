5ac40e914058d041e82231801a2654a2e8398704
Ben Berry <ben.berry@gsa.gov>
2021-12-22 13:44:06 -0800

    consistent module name

--- terraform/modules/logsearch/platform_archive.tf
+++ terraform/modules/logsearch/platform_archive.tf
@@ -1,4 +1,4 @@
-module "logsearch_platform_archive" {
+module "platform_archive" {
   source          = "../s3_bucket/encrypted_bucket"
   bucket          = "logsearch-platform-${var.stack_prefix}"
   aws_partition   = var.aws_partition
