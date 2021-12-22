67f810d06e9b1a1424cb2a94a9fb9ed5cd220cb4
Ben Berry <ben.berry@gsa.gov>
2021-12-22 13:49:37 -0800

    consistent variable name

--- terraform/modules/logsearch/platform_archive.tf
+++ terraform/modules/logsearch/platform_archive.tf
@@ -1,6 +1,6 @@
 module "platform_archive" {
   source          = "../s3_bucket/encrypted_bucket"
-  bucket          = "logsearch-platform-${var.stack_prefix}"
+  bucket          = "logsearch-platform-${var.stack_description}"
   aws_partition   = var.aws_partition
   expiration_days = 548
 }
