4807c8667237a568bfdd11cb8013c0289f285f8e
Ben Berry <ben.berry@gsa.gov>
2021-12-29 15:07:50 -0800

    add environment names, fix typos
    fix typos and errors in pipeline
    add environment names to roles and policies

--- scripts/add_environment/manage_environment.sh
+++ scripts/add_environment/manage_environment.sh
@@ -2,16 +2,16 @@
 
 set -eux
 
-if [[ -z "${TF_VAR_remote_role_arn}" ]]; then
+if [[ -z "${TF_VAR_cert_remote_role_arn}" ]]; then
     echo "you must set TF_VAR_remote_role_arn"
     echo "check the README for more info"
     exit 1
 fi
 
+mkdir -p ${WORKSPACE_DIR}
 TF="terraform -chdir=./terraform/stacks/managedaccount"
 # Provision terraform infrastructure
 ${TF} init
 ${TF} validate 
-${TF} plan
-# ${TF} apply
-# ${TF} output -json > ${WORKSPACE_DIR}/terraform-outputs.json
+${TF} apply
+${TF} output -json > ${WORKSPACE_DIR}/terraform-outputs.json
