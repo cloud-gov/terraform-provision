d08e21d5e9972990f0c6c3aaa4832027f3e2d2bf
Ben Berry <ben.berry@gsa.gov>
2021-12-29 17:02:54 -0800

    use assume role correctly

--- ci/check-certificates.sh
+++ ci/check-certificates.sh
@@ -6,7 +6,9 @@ export AWS_ACCESS_KEY_ID=$(spruce json terraform-yaml-tooling/state.yml | jq -r 
 export AWS_SECRET_ACCESS_KEY=$(spruce json terraform-yaml-tooling/state.yml | jq -r ".terraform_outputs.iam_cert_provision_secret_access_key_curr")
 
 if [[ -n "${ASSUME_ROLE_ARN}" ]]; then
-  aws sts assume-role --role-arn=${ASSUME_ROLE_ARN} --role-session-name=concourse_cert_check
+  read access_key_id secret_access_key < <(echo $(aws sts assume-role --role-arn=${ASSUME_ROLE_ARN} --role-session-name=concourse_cert_upload | jq -r '.Credentials.AccessKeyId .Credentials.SecretAccessKey'))
+  export AWS_ACCESS_KEY_ID=${access_key_id}
+  export AWS_SECRET_ACCESS_KEY=${secret_access_key}
 fi
 
 aws iam list-server-certificates \
