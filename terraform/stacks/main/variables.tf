67ff96d170f5de2c5bc35a60402addd526a1efff
Ben Berry <ben.berry@gsa.gov>
2021-12-29 17:29:00 -0800

    find security groups by category instead of stack

--- terraform/stacks/main/variables.tf
+++ terraform/stacks/main/variables.tf
@@ -95,3 +95,6 @@ variable "rds_apply_immediately" {
 variable "assume_arn" {
   # arn of a role to assume in the account being configured
 }
+variable "stack_class" {
+  # should be one of development, staging, production
+}
