ec06acafe1ad82c5f5dc42499528b856488a4fe5
Ben Berry <ben.berry@gsa.gov>
2021-12-29 17:11:53 -0800

    add a session token for good measure

--- ci/upload-certificate.sh
+++ ci/upload-certificate.sh
@@ -6,9 +6,10 @@ export AWS_ACCESS_KEY_ID=$(spruce json terraform-yaml-tooling/state.yml | jq -r 
 export AWS_SECRET_ACCESS_KEY=$(spruce json terraform-yaml-tooling/state.yml | jq -r ".terraform_outputs.iam_cert_provision_secret_access_key_curr")
 
 if [[ -n "${ASSUME_ROLE_ARN}" ]]; then
-  read access_key_id secret_access_key < <(echo $(aws sts assume-role --role-arn=${ASSUME_ROLE_ARN} --role-session-name=concourse_cert_upload | jq -r '.Credentials.AccessKeyId, .Credentials.SecretAccessKey'))
-  export AWS_ACCESS_KEY_ID=${access_key_id}
-  export AWS_SECRET_ACCESS_KEY=${secret_access_key}
+  read access_key_id secret_access_key session_token < <(echo $(aws sts assume-role --role-arn=${ASSUME_ROLE_ARN} --role-session-name=concourse_cert_upload | jq -r '.Credentials.AccessKeyId, .Credentials.SecretAccessKey, .Credentials.SessionToken'))
+  export AWS_ACCESS_KEY_ID="${access_key_id}"
+  export AWS_SECRET_ACCESS_KEY="${secret_access_key}"
+  export AWS_SESSION_TOKEN="${session_token}"
 fi
 
 # Upload new certificate if present
