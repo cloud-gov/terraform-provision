d734020c2e7cf45c59c19e8bbc7fcd6c329f1559
Ben Berry <ben.berry@gsa.gov>
2022-01-04 14:38:04 -0800

    add regionalbosh, fix region targeting

---
+++ terraform/stacks/regionalmasterbosh/buckets.tf
@@ -0,0 +1,21 @@
+module "bosh_blobstore_bucket" {
+  source        = "../../modules/s3_bucket/encrypted_bucket"
+  bucket        = var.blobstore_bucket_name
+  aws_partition = data.aws_partition.current.partition
+  force_destroy = "true"
+}
+
+module "bosh_release_bucket" {
+  source        = "../../modules/s3_bucket/encrypted_bucket"
+  bucket        = var.bosh_release_bucket
+  aws_partition = data.aws_partition.current.partition
+  versioning    = "true"
+  force_destroy = "true"
+}
+
+module "log_bucket" {
+  source          = "../../modules/log_bucket"
+  aws_partition   = data.aws_partition.current.partition
+  log_bucket_name = var.log_bucket_name
+  aws_region      = data.aws_region.current.name
+}
--- terraform/stacks/regionalmasterbosh/variables.tf
+++ terraform/stacks/regionalmasterbosh/variables.tf
@@ -38,54 +38,11 @@ variable "rds_parameter_group_family" {
 variable "remote_state_bucket" {
 }
 
-variable "concourse_prod_rds_password" {
-}
-
-variable "concourse_staging_rds_password" {
-}
-
-variable "concourse_varz_bucket" {
-}
-
-variable "credhub_prod_rds_password" {
-}
-
-variable "credhub_staging_rds_password" {
-}
-
 variable "wildcard_production_certificate_name_prefix" {
   default = ""
 }
 
-variable "wildcard_staging_certificate_name_prefix" {
-  default = ""
-}
-
-variable "concourse_production_hosts" {
-  type = list(string)
-}
-
-variable "concourse_staging_hosts" {
-  type = list(string)
-}
-
-variable "credhub_production_hosts" {
-  type = list(string)
-}
-
-variable "credhub_staging_hosts" {
-  type = list(string)
-}
-
 variable "monitoring_production_hosts" {
-  type = list(string)
-}
-
-variable "monitoring_staging_hosts" {
-  type = list(string)
-}
-
-variable "nessus_hosts" {
   type = list(string)
 }
 
@@ -108,44 +65,8 @@ variable "dns_eip_count_production" {
   default = 4
 }
 
-variable "dns_eip_count_staging" {
-  default = 2
-}
-
-variable "varz_bucket" {
-  default = "cloud-gov-varz"
-}
-
-variable "varz_bucket_stage" {
-  default = "cloud-gov-varz-stage"
-}
-
 variable "bosh_release_bucket" {
   default = "cloud-gov-bosh-releases"
-}
-
-variable "terraform_state_bucket" {
-  default = "terraform-state"
-}
-
-variable "semver_bucket" {
-  default = "cg-semver"
-}
-
-variable "build_artifacts_bucket" {
-  default = "cg-build-artifacts"
-}
-
-variable "buildpack_notify_bucket" {
-  default = "buildpack-notify-state-*"
-}
-
-variable "billing_bucket" {
-  default = "cg-billing-*"
-}
-
-variable "cg_binaries_bucket" {
-  default = "cg-binaries"
 }
 
 variable "smtp_ingress_cidr_blocks" {
@@ -166,4 +87,14 @@ variable "oidc_client" {
 variable "oidc_client_secret" {
 }
 
+variable "assume_arn" {
+  # arn of a role in the remote account to assume
+}
 
+variable "target_stack_name" {
+  # name of the stack that ci runs in to bootstrap this stack
+}
+
+variable "nessus_hosts" {
+
+}
