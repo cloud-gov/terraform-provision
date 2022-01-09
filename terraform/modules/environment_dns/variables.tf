0de3ed4c148154977a2969628c88d9c0d5d18ca2
Ben Berry <ben.berry@gsa.gov>
2022-01-09 09:32:07 -0800

    create DNS for new stacks

--- terraform/modules/environment_dns/variables.tf
+++ terraform/modules/environment_dns/variables.tf
@@ -2,7 +2,10 @@ variable "zone_id" {
 
 }
 
-variable "subdomain" {
+variable "app_subdomain" {
+
+}
+variable "admin_subdomain" {
 
 }
 
@@ -12,4 +15,11 @@ variable "stack_name" {
 
 variable "cloudfront_zone_id" {
   default = "Z33AYJ8TM3BH4J"
+}
+
+variable "remote_state_bucket" {
+
+}
+variable "remote_state_region" {
+
 }
