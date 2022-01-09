5aebb8596280e37f579bdc2ce3788cfc6db35877
Ben Berry <ben.berry@gsa.gov>
2022-01-08 23:32:01 -0800

    add dns modules

---
+++ terraform/modules/environment_dns/variables.tf
@@ -0,0 +1,15 @@
+variable "zone_id" {
+
+}
+
+variable "subdomain" {
+
+}
+
+variable "stack_name" {
+
+}
+
+variable "cloudfront_zone_id" {
+  default = "Z33AYJ8TM3BH4J"
+}
---
+++ terraform/modules/regionalmaster_dns/variables.tf
@@ -0,0 +1,15 @@
+variable "zone_id" {
+
+}
+
+variable "subdomain" {
+
+}
+
+variable "tooling_stack_name" {
+
+}
+
+variable "cloudfront_zone_id" {
+  default = "Z33AYJ8TM3BH4J"
+}
