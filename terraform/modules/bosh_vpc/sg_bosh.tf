5f2b645da2dc14036da44d49d6136c84a78cde80
Ben Berry <ben.berry@gsa.gov>
2022-01-12 18:26:46 -0800

    let connie talk to boshuaa

--- terraform/modules/bosh_vpc/sg_bosh.tf
+++ terraform/modules/bosh_vpc/sg_bosh.tf
@@ -126,6 +126,16 @@ resource "aws_security_group_rule" "bosh_director" {
   security_group_id = aws_security_group.bosh.id
 }
 
+
+resource "aws_security_group_rule" "bosh_uaa" {
+  type              = "ingress"
+  from_port         = 8443
+  to_port           = 8443
+  protocol          = "tcp"
+  cidr_blocks       = concat(var.concourse_security_group_cidrs, [aws_vpc.main_vpc.cidr_block])
+  security_group_id = aws_security_group.bosh.id
+}
+
 resource "aws_security_group_rule" "node_exporter" {
   type                     = "ingress"
   from_port                = 9100
