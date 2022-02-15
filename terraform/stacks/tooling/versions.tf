bea6b7e134c9b902747d55d74b7cabb25df802bb
Ben Berry <ben.berry@gsa.gov>
2022-02-15 10:04:14 -0800

    use single canonical versions file

--- terraform/stacks/tooling/versions.tf
+++ terraform/stacks/tooling/versions.tf
@@ -1,10 +1,1 @@
-
-terraform {
-  required_version = ">= 0.14"
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = ">=3.37.0, < 4.0.0"
-    }
-  }
-}
+../main/versions.tf
