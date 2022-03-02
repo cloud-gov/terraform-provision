d0c61ef180a92f6f8571aebe9c2268680c94598b
Ben Berry <ben.berry@gsa.gov>
2022-03-02 13:39:45 -0800

    remove staging from dns

--- terraform/stacks/dns/staging.tf
+++
@@ -1,9 +0,0 @@
-module "staging_dns" {
-  source              = "../../modules/environment_dns"
-  stack_name          = "staging"
-  domain              = "fr-stage.cloud.gov"
-  app_subdomain       = "app.fr-stage.cloud.gov"
-  admin_subdomain     = "fr-stage.cloud.gov"
-  remote_state_bucket = var.remote_state_bucket
-  remote_state_region = var.remote_state_region
-}
