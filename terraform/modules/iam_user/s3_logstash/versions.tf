b8ed325bde1c0216d74794ddb4c49a3baa487970
Ben Berry <ben.berry@gsa.gov>
2022-03-10 17:50:31 -0800

    update versions files

--- terraform/modules/iam_user/s3_logstash/versions.tf
+++ terraform/modules/iam_user/s3_logstash/versions.tf
@@ -1,4 +1,9 @@
-
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.15"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">=4.4.0"
+    }
+  }
 }
