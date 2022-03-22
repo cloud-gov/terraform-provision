5a919af1a7c3c067254a40e868cc99a7e6106344
Van Nguyen <vanh.nguyen@gsa.gov>
2022-03-22 14:09:34 -0400

    removed extra stuff

--- terraform/modules/ecr/ecr.tf
+++ terraform/modules/ecr/ecr.tf
@@ -1,8 +1,9 @@
 resource "aws_ecr_repository" "repository" {
   name                 = var.repository_name
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
 
   tags = {
     Name = var.stack_description
   }
 }
+
