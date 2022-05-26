a491d3b47f9d0e8c0a5f4cc3450e226f55b90cda
Ben Berry <ben.berry@gsa.gov>
2022-05-26 10:28:14 -0700

    create clamav sync user

---
+++ terraform/stacks/tooling/cvd_mirror.tf
@@ -0,0 +1,20 @@
+module "cvd_meta_bucket" {
+  source        = "../../modules/s3_bucket/encrypted_bucket"
+  bucket        = "cg-clamav-meta"
+  aws_partition = data.aws_partition.current.partition
+  versioning    = "true"
+}
+
+module "cvd_database_bucket" {
+  source        = "../../modules/cvd_mirror"
+  bucket        = "cg-clamav-mirror"
+  aws_partition = data.aws_partition.current.partition
+  versioning    = "false"
+}
+
+module cvd_sync_user {
+  source = "../../modules/iam_user/cvd_sync"
+  username = "clamav_mirror_sync"
+  cvd_database_bucket = "cg-clamav-mirror"
+  cvd_metadata_bucket = "cg-clamav-meta"
+}
