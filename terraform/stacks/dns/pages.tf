e981d125ea1e97eca087fa4cbc7905d6771b8331
Andrew Burnes <andrew.burnes@gsa.gov>
2022-09-30 13:05:03 -0700

    Add CNAMES to provision custom domains for pages templates

--- terraform/stacks/dns/pages.tf
+++ terraform/stacks/dns/pages.tf
@@ -56,6 +56,57 @@ resource "aws_route53_record" "cloud_gov_star_sites_pages_cloud_gov_aaaa" {
     evaluate_target_health = false
   }
 }
+
+## Templates ##
+
+resource "aws_route53_record" "cloud_gov_uswds-11ty_pages_cloud_gov" {
+    zone_id = aws_route53_zone.cloud_gov_zone.zone_id
+    name    = "uswds-11ty.pages.cloud.gov."
+    type    = "CNAME"
+    ttl     = 60
+    records = ["uswds-11ty.pages.cloud.gov.external-domains-production.cloud.gov."]
+}
+
+resource "aws_route53_record" "cloud_gov__acme-challenge_uswds-11ty_pages_cloud_gov" {
+    zone_id = aws_route53_zone.cloud_gov_zone.zone_id
+    name    = "_acme-challenge.uswds-11ty.pages.cloud.gov."
+    type    = "CNAME"
+    ttl     = 60
+    records = ["_acme-challenge.uswds-11ty.pages.cloud.gov.external-domains-production.cloud.gov."]
+}
+
+resource "aws_route53_record" "cloud_gov_uswds-gatsby_pages_cloud_gov" {
+    zone_id = aws_route53_zone.cloud_gov_zone.zone_id
+    name    = "uswds-gatsby.pages.cloud.gov."
+    type    = "CNAME"
+    ttl     = 60
+    records = ["uswds-gatsby.pages.cloud.gov.external-domains-production.cloud.gov."]
+}
+
+resource "aws_route53_record" "cloud_gov__acme-challenge_uswds-gatsby_pages_cloud_gov" {
+    zone_id = aws_route53_zone.cloud_gov_zone.zone_id
+    name    = "_acme-challenge.uswds-gatsby.pages.cloud.gov."
+    type    = "CNAME"
+    ttl     = 60
+    records = ["_acme-challenge.uswds-gatsby.pages.cloud.gov.external-domains-production.cloud.gov."]
+}
+
+resource "aws_route53_record" "cloud_gov_uswds-jekyll_pages_cloud_gov" {
+    zone_id = aws_route53_zone.cloud_gov_zone.zone_id
+    name    = "uswds-jekyll.pages.cloud.gov."
+    type    = "CNAME"
+    ttl     = 60
+    records = ["uswds-jekyll.pages.cloud.gov.external-domains-production.cloud.gov."]
+}
+
+resource "aws_route53_record" "cloud_gov__acme-challenge_uswds-jekyll_pages_cloud_gov" {
+    zone_id = aws_route53_zone.cloud_gov_zone.zone_id
+    name    = "_acme-challenge.uswds-jekyll.pages.cloud.gov."
+    type    = "CNAME"
+    ttl     = 60
+    records = ["_acme-challenge.uswds-jekyll.pages.cloud.gov.external-domains-production.cloud.gov."]
+}
+
 ## End Production ##
 
 #############
