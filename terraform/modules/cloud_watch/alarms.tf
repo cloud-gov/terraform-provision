9b8edc69da2ffaadf875345baf7569883352ed09
David Anderson <david.n.anderson@gsa.gov>
2022-10-27 15:38:24 -0400

    update alarm name for current stack

--- terraform/modules/cloud_watch/alarms.tf
+++ terraform/modules/cloud_watch/alarms.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_metric_alarm" "lb_404_anomaly_detection" {
-    alarm_name = "Load Balancer High 404 Response Rate"
+    alarm_name = "${var.stack_description} CF Load Balancer High 404 Response Rate"
     comparison_operator = "GreaterThanUpperThreshold"
     evaluation_periods = 2
     threshold_metric_id = "ad1"
