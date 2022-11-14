1dbf710c8bdbdb02374138616bcd39fbc9ef2c57
James Hochadel <james.hochadel@gsa.gov>
2022-11-14 11:57:40 -0500

    Configure DNS stack like external stack

--- terraform/stacks/dns/stack.tf
+++ terraform/stacks/dns/stack.tf
@@ -1,21 +1,9 @@
-variable "aws_access_key" {
-}
-
-variable "aws_secret_key" {
-}
-
-variable "aws_region" {
-}
-
 terraform {
   backend "s3" {
   }
 }
 
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region     = var.aws_region
 }
 
 variable "cloudfront_zone_id" {
