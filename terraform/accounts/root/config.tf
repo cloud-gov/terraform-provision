150be66dbb1f55af9d30429635840fa175ea5c3c
Mark Boyd <mark.boyd@gsa.gov>
2022-12-07 10:58:24 -0500

    remove deployment to gov root account

--- terraform/accounts/root/config.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_organizations_delegated_administrator" "config_delegated_administrator" {
-  account_id        = var.config_delegated_administrator_account_id
-  service_principal = "config.amazonaws.com"
-}
