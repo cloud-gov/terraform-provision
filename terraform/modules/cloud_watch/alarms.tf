47163f770528acb5a1973d209968b5b03f200842
David Anderson <david.n.anderson@gsa.gov>
2022-12-09 11:40:58 -0500

    adjust the alerting to reduce noise, and update to 4XX naming scheme

--- terraform/modules/cloud_watch/alarms.tf
+++ terraform/modules/cloud_watch/alarms.tf
@@ -1,10 +1,10 @@
-resource "aws_cloudwatch_metric_alarm" "lb_404_anomaly_detection" {
-    alarm_name = "${var.stack_description} CF Load Balancer High 404 Response Rate"
+resource "aws_cloudwatch_metric_alarm" "lb_4XX_anomaly_detection" {
+    alarm_name = "${var.stack_description} CF Load Balancer High 4XX Response Rate"
     comparison_operator = "GreaterThanUpperThreshold"
-    evaluation_periods = 2
-    datapoints_to_alarm = 2
+    evaluation_periods = 5
+    datapoints_to_alarm = 3
     threshold_metric_id = "ad1"
-    alarm_description = "An alert for when the targets are returning a higher than normal rate of 404s"
+    alarm_description = "Alerts when the targets are returning a higher than normal rate of 4XX responses"
     insufficient_data_actions = []
     actions_enabled = true
     ok_actions = []
@@ -13,7 +13,7 @@ resource "aws_cloudwatch_metric_alarm" "lb_404_anomaly_detection" {
 
     metric_query {
         id = "ad1"
-        expression = "ANOMALY_DETECTION_BAND(m1, 5)"
+        expression = "ANOMALY_DETECTION_BAND(m1, 20)"
         label = "HTTPCode_Target_4XX_Count (expected)"
         return_data = "true"
     }
@@ -24,7 +24,7 @@ resource "aws_cloudwatch_metric_alarm" "lb_404_anomaly_detection" {
         metric {
             metric_name = "HTTPCode_Target_4XX_Count"
             namespace = "AWS/ApplicationELB"
-            period = "300"
+            period = "60"
             stat = "Sum"
 
             dimensions = {
