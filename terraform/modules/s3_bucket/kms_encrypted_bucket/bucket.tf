8d8e81a940484770cf87c39179481d3083a9600c
Mark Boyd <mark.boyd@gsa.gov>
2023-01-09 16:36:22 -0500

    add resource and variables to support blocking public access to KMS encrypted buckets

--- terraform/modules/s3_bucket/kms_encrypted_bucket/bucket.tf
+++ terraform/modules/s3_bucket/kms_encrypted_bucket/bucket.tf
@@ -2,6 +2,15 @@ resource "aws_s3_bucket" "kms_encrypted_bucket" {
   bucket = var.bucket_name
 }
 
+resource "aws_s3_bucket_public_access_block" "kms_encrypted_bucket_block_public" {
+  bucket = aws_s3_bucket.kms_encrypted_bucket.id
+
+  block_public_acls       = var.block_public_acls
+  block_public_policy     = var.block_public_policy
+  ignore_public_acls      = var.ignore_public_acls
+  restrict_public_buckets = var.restrict_public_buckets
+}
+
 resource "aws_s3_bucket_versioning" "kms_encrypted_bucket_versioning" {
   bucket = aws_s3_bucket.kms_encrypted_bucket.id
   versioning_configuration {
