92d76b7c4764b29115c86e4d8b775fc9af110ac9
Mark Boyd <mark.boyd@gsa.gov>
2023-03-17 14:27:48 -0400

    try ignoring changes

--- terraform/modules/vpc_peering/network.tf
+++ terraform/modules/vpc_peering/network.tf
@@ -28,6 +28,10 @@ resource "aws_vpc_peering_connection" "peering" {
   tags = {
     Name = "${var.source_vpc_id} to ${var.target_vpc_id}"
   }
+
+  lifecycle {
+    ignore_changes = [tags_all]
+  }
 }
 
 resource "aws_vpc_peering_connection_accepter" "peer" {
