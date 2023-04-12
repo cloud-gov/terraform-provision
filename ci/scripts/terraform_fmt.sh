c93bc8ea83b30947c702d20b64d2c499e86e3812
James Hochadel <james.hochadel@gsa.gov>
2023-04-12 10:57:49 -0400

    Ensure tf is installed, matching validate.sh

--- ci/scripts/terraform_fmt.sh
+++ ci/scripts/terraform_fmt.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -e
 
+which terraform > /dev/null 2>&1 || {
+  echo "Aborted. Please install terraform by following https://www.terraform.io/intro/getting-started/install.html" 1>&2
+  exit 1
+}
+
 terraform fmt -recursive
 git diff-index --quiet HEAD -- # exit 1 if dirty, 0 otherwise
