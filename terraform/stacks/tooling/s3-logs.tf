716107a41b0bebcafdaa9832a47a4e9adfb3bf88
Mark Boyd <mark.boyd@gsa.gov>
2023-06-13 15:59:56 -0400

    re-added bucket policies and other resources

--- terraform/stacks/tooling/s3-logs.tf
+++ terraform/stacks/tooling/s3-logs.tf
@@ -29,8 +29,92 @@ resource "aws_s3_bucket_lifecycle_configuration" "s3_audit_logs_trail_bucket_lif
   }
 }
 
+resource "aws_s3_bucket_logging" "cg-s3-cloudtrail-bucket-logging" {
+  bucket = aws_s3_bucket.cg-s3-cloudtrail-bucket.id
+
+  target_bucket = aws_s3_bucket.cloudtrail-accesslog-bucket.id
+  target_prefix = "log/"
+}
+
 resource "aws_s3_bucket" "cloudtrail-accesslog-bucket" {
   bucket = var.cloudtrail_accesslog_bucket
+}
+
+resource "aws_s3_bucket_ownership_controls" "cloudtrail-accesslog-bucket-ownership" {
+  bucket = aws_s3_bucket.cloudtrail-accesslog-bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "cloudtrail-accesslog-bucket-acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.cloudtrail-accesslog-bucket-ownership]
+  bucket     = aws_s3_bucket.cloudtrail-accesslog-bucket.id
+  acl        = "log-delivery-write"
+}
+
+resource "aws_s3_bucket_policy" "cloudtrail_bucket_policy" {
+  bucket = aws_s3_bucket.cg-s3-cloudtrail-bucket.id
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AWSCloudTrailAclCheck",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:GetBucketAcl",
+            "Resource": "arn:${data.aws_partition.current.partition}:s3:::${var.cloudtrail_bucket}"
+        },
+        {
+            "Sid": "AWSCloudTrailWrite",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:PutObject",
+            "Resource": "arn:${data.aws_partition.current.partition}:s3:::${var.cloudtrail_bucket}/*",
+            "Condition": {
+                "StringEquals": {
+                    "s3:x-amz-acl": "bucket-owner-full-control"
+                }
+            }
+        }
+    ]
+}
+POLICY
+}
+
+resource "aws_s3_bucket_policy" "cloudtrail_accesslog_bucket_policy" {
+  bucket = aws_s3_bucket.cloudtrail-accesslog-bucket.id
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "S3ServerAccessLogsPolicy",
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "logging.s3.amazonaws.com"
+            },
+            "Action": [
+                "s3:PutObject"
+            ],
+            "Resource": "arn:${data.aws_partition.current.partition}:s3:::${var.cloudtrail_accesslog_bucket}/*",
+            "Condition": {
+                "ArnLike": {
+                    "aws:SourceArn": "arn:${data.aws_partition.current.partition}:s3:::${var.cloudtrail_bucket}"
+                },
+                "StringEquals": {
+                    "aws:SourceAccount": "${data.aws_caller_identity.current.account_id}"
+                }
+            }
+        }
+    ]
+}
+POLICY
 }
 
 resource "aws_s3_bucket_lifecycle_configuration" "s3_access_logs_trail_bucket_lifecycle" {
