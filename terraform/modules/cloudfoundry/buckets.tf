117eaa967ceb370c3e4f7f7d03f41a9719bf9714
Mark Boyd <mark.boyd@gsa.gov>
2023-06-14 15:12:31 -0400

    re-add private ACL for logsearch bucket

--- terraform/modules/cloudfoundry/buckets.tf
+++ terraform/modules/cloudfoundry/buckets.tf
@@ -27,5 +27,6 @@ module "logsearch-archive" {
   bucket          = "logsearch-${var.stack_prefix}"
   aws_partition   = var.aws_partition
   expiration_days = 930 # 31 days * 30 months = 930 days
+  acl = "private"
 }
 
