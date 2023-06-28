b8b1e9ab3c8f4d136671a837b48323bb65770fe3
ChrisMcGowan <chris.mcgowan@gsa.gov>
2023-06-28 09:40:32 -0400

    adding in ebs storage options

--- terraform/modules/opensearch_domain/opensearch.tf
+++ terraform/modules/opensearch_domain/opensearch.tf
@@ -11,6 +11,12 @@ resource "aws_opensearch_domain" "opensearch" {
     }
   }
 
+  ebs_options {
+    ebs_enabled               = true
+    volume_size               = 15
+    volume_type               = gp2
+  }
+
   domain_endpoint_options {
     enforce_https             = true
     tls_security_policy       = "Policy-Min-TLS-1-2-2019-07"
