70b5ad45b689d9071da237d842c78f8a3555df55
ChrisMcGowan <chris.mcgowan@gsa.gov>
2023-06-28 10:27:08 -0400

    final changes before PR/passes dev

--- terraform/modules/opensearch_domain/opensearch.tf
+++ terraform/modules/opensearch_domain/opensearch.tf
@@ -14,7 +14,6 @@ resource "aws_opensearch_domain" "opensearch" {
   ebs_options {
     ebs_enabled               = true
     volume_size               = 15
-   #volume_type               = gp2
   }
 
   domain_endpoint_options {
