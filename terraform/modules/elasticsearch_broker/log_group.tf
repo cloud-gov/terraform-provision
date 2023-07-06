3e4175bfd23f0ac6c76fa093454c17bc3fb3a851
Mark Boyd <mark.boyd@gsa.gov>
2023-07-06 13:54:31 -0400

    add retention period to cloudwatch log groups (#1523)

--- terraform/modules/elasticsearch_broker/log_group.tf
+++ terraform/modules/elasticsearch_broker/log_group.tf
@@ -1,3 +1,5 @@
 resource "aws_cloudwatch_log_group" "audit_log" {
   name = "${var.stack_description}-elasticsearch_broker_audit_log"
+  # Lowest allowed value that fulfills M-21-31 reqs of storing for 30 months
+  retention_in_days = 1827
 }
