cd95e6523c3bb03f014e53f6d5ba7531b6f13f42
David Anderson <david.n.anderson@gsa.gov>
2023-09-12 16:47:06 -0400

    add defect dojo module

---
+++ terraform/modules/defect_dojo/variables.tf
@@ -0,0 +1,61 @@
+variable "stack_description" {
+}
+
+variable "rds_db_name" {
+  default = "defectdojo"
+}
+
+variable "rds_parameter_group_name" {
+  default = ""
+}
+
+variable "rds_parameter_group_family" {
+  default = "postgres12"
+}
+
+variable "rds_db_size" {
+  default = 200
+}
+
+variable "rds_db_iops" {
+  default = 0
+}
+
+variable "rds_db_storage_type" {
+  default = "gp2"
+}
+
+variable "rds_instance_type" {
+  default = "db.m4.xlarge"
+}
+
+variable "rds_db_engine_version" {
+  default = "12.14"
+}
+
+variable "rds_username" {
+  default = "defectdj"
+}
+
+variable "rds_password" {
+  sensitive = true
+}
+
+variable "rds_subnet_group" {
+}
+
+variable "rds_security_groups" {
+  type = list(string)
+}
+
+variable "rds_multi_az" {
+}
+
+variable "rds_final_snapshot_identifier" {
+}
+
+variable "rds_allow_major_version_upgrade" {
+}
+
+variable "rds_apply_immediately" {
+}
