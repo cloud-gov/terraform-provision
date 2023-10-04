80004b9f19541743500aa8b2d6abf054eef977ef
Mark Boyd <mark.boyd@gsa.gov>
2023-10-04 16:35:07 -0400

    try to fix TF race condition

--- terraform/modules/s3_bucket/log_encrypted_bucket/log_encrypted_bucket.tf
+++ terraform/modules/s3_bucket/log_encrypted_bucket/log_encrypted_bucket.tf
@@ -40,6 +40,7 @@ resource "aws_s3_bucket_acl" "log_encrypted_bucket_acl" {
   count  = var.acl != "" ? 1 : 0
   bucket = aws_s3_bucket.log_encrypted_bucket.id
   acl    = var.acl
+  depends_on = [aws_s3_bucket_ownership_controls.log_encrypted_bucket_acl_ownership]
 }
 
 resource "aws_s3_bucket_lifecycle_configuration" "log_encrypted_bucket_lifecycle" {
