0f7168e45c0247f49c84350345c758b0183d12ce
Van Nguyen <vanh.nguyen@gsa.gov>
2023-10-25 14:48:19 -0400

    matching names

--- terraform/modules/iam_user/cvd_sync/outputs.tf
+++ terraform/modules/iam_user/cvd_sync/outputs.tf
@@ -11,10 +11,10 @@ output "secret_access_key_prev" {
 }
 
 output "access_key_id_curr" {
-  value = aws_iam_access_key.iam_access_key_new.id
+  value = aws_iam_access_key.iam_access_key_v2.id
 }
 
 output "secret_access_key_curr" {
-  value = aws_iam_access_key.iam_access_key_new.secret
+  value = aws_iam_access_key.iam_access_key_v2.secret
 }
 
