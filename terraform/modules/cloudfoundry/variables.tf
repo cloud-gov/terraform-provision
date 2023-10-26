e9ec0993c762fa3989957a399740af77da7153ac
Mark Boyd <mark.boyd@gsa.gov>
2023-10-26 17:46:08 -0400

    fix variable types

--- terraform/modules/cloudfoundry/variables.tf
+++ terraform/modules/cloudfoundry/variables.tf
@@ -175,19 +175,19 @@ variable "forwarded_ip_header_name" {
 }
 
 variable "non_cdn_traffic_rate_limit_challenge_by_source_ip" {
-  type = integer
+  type = number
   description = "Number of requests to allow per source IP per 5 minute interval before challenging requests"
   default = 2000
 }
 
 variable "non_cdn_traffic_rate_limit_challenge_by_forwarded_ip" {
-  type = integer
+  type = number
   description = "Number of requests to allow per forwarded IP per 5 minute interval before challenging requests"
   default = 2000
 }
 
 variable "non_cdn_traffic_rate_limit_block_by_forwarded_ip" {
-  type = integer
+  type = number
   description = "Number of requests to allow per forwarded IP per 5 minute interval before blocking requests"
   default = 5000
 }
