46e93610f4e09ae808b038d94b1c99245f5d2ddf
Mark Boyd <mark.boyd@gsa.gov>
2023-10-26 13:16:52 -0400

    reorganize WAF rules and stub out new rate limiting rules

--- terraform/modules/cloudfoundry/elb_uaa.tf
+++ terraform/modules/cloudfoundry/elb_uaa.tf
@@ -54,6 +54,14 @@ resource "aws_lb_listener" "cf_uaa_http" {
   }
 }
 
+# resource "aws_wafv2_ip_set" "cf_gateway_ips" {
+#   name        = "${var.stack_description}-gateway-ips"
+#   description        = "NAT Gateway IPs for traffic from the CF environment"
+#   scope              = "REGIONAL"
+#   ip_address_version = "IPV4"
+#   addresses          = var.gateway_ips
+# }
+
 // For webacl rules using AWS managed rule sets or custom rules, checkout the AWS Govcloud WAF V2 console
 // Use the console to craft a sample webacl but before you commit you can click the tab/option to show you
 // The rule in json format which will make it easier to translate to TF
@@ -364,38 +372,8 @@ resource "aws_wafv2_web_acl" "cf_uaa_waf_core" {
   }
 
   rule {
-    name     = "RateLimitByForwardedHeader"
-    priority = 5
-
-    action {
-      block {}
-    }
-
-    statement {
-      rate_based_statement {
-        limit              = 250000
-        aggregate_key_type = "FORWARDED_IP"
-
-        forwarded_ip_config {
-          # Match status to apply if the request doesn’t have a valid IP address in the specified position.
-          # Note that, if the specified header isn’t present at all in the request, AWS WAF doesn’t apply
-          # the rule to the request. (From AWS Console)
-          fallback_behavior = "MATCH"
-          header_name       = "X-Forwarded-For"
-        }
-      }
-    }
-
-    visibility_config {
-      cloudwatch_metrics_enabled = true
-      metric_name                = "${var.stack_description}-RateLimitNonCDN"
-      sampled_requests_enabled   = true
-    }
-  }
-
-  rule {
     name     = "CG-RegexPatternSets"
-    priority = 6
+    priority = 5
     action {
       block {}
     }
@@ -473,6 +451,51 @@ resource "aws_wafv2_web_acl" "cf_uaa_waf_core" {
       sampled_requests_enabled   = true
     }
   }
+
+  rule {
+    name     = "RateLimitBySourceIP-Challenge"
+    priority = 6
+
+    action {
+      challenge {}
+    }
+
+    statement {
+      rate_based_statement {
+        limit              = 2000
+        aggregate_key_type = "IP"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "${var.stack_description}-RateLimitSourceIPChallenge"
+      sampled_requests_enabled   = false
+    }
+  }
+
+  rule {
+    name     = "RateLimitBySourceIP-Block"
+    priority = 7
+
+    action {
+      block {}
+    }
+
+    statement {
+      rate_based_statement {
+        limit              = 4000
+        aggregate_key_type = "IP"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "${var.stack_description}-RateLimitSourceIPBlock"
+      sampled_requests_enabled   = false
+    }
+  }
+
 
   visibility_config {
     cloudwatch_metrics_enabled = true
