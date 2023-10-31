56c87f567a3bcf5d65b755210196c4f34987b7ec
Mark Boyd <mark.boyd@gsa.gov>
2023-10-31 15:43:09 -0400

    fix WAF rule

--- terraform/modules/cloudfoundry/elb_uaa.tf
+++ terraform/modules/cloudfoundry/elb_uaa.tf
@@ -699,23 +699,7 @@ resource "aws_wafv2_web_acl" "cf_uaa_waf_core" {
               not_statement {
                 statement {
                   ip_set_reference_statement {
-                    arn = var.nat_egress_ip_set_arn
-
-                    ip_set_forwarded_ip_config {
-                      header_name       = var.forwarded_ip_header_name
-                      fallback_behavior = "NO_MATCH"
-                      position          = "FIRST"
-                    }
-                  }
-                }
-              }
-            }
-
-            statement {
-              not_statement {
-                statement {
-                  ip_set_reference_statement {
-                    arn = var.tooling_nat_egress_ip_set_arn
+                    arn = var.cg_egress_ip_set_arn
 
                     ip_set_forwarded_ip_config {
                       header_name       = var.forwarded_ip_header_name
