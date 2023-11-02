649c61af23525e257c7882189440b501d468082c
James Hochadel <james.hochadel@gsa.gov>
2023-11-02 10:44:38 -0400

    Manage Production WAF dashboard with Terraform instead of the console

---
+++ terraform/modules/cloud_watch/dashboards.tf
@@ -0,0 +1,142 @@
+/*
+It is easier to iterate on CloudWatch dashboards directly in the console than in Terraform.
+If you do so, click "Actions > View/edit source", copy the entire source, and replace the
+object passed into `dashboard_body = jsonencode()` with the source.
+*/
+resource "aws_cloudwatch_dashboard" "production_waf" {
+  dashboard_name = "Production WAF"
+  dashboard_body = jsonencode({
+    "widgets" : [
+      {
+        "height" : 9,
+        "width" : 12,
+        "y" : 0,
+        "x" : 12,
+        "type" : "metric",
+        "properties" : {
+          "metrics" : [
+            ["AWS/WAFV2", "AllowedRequests", "Region", "us-gov-west-1", "Rule", "data-gov-logstash", "WebACL", "production-cf-uaa-waf-core", { "region" : "us-gov-west-1" }],
+            ["...", "production-AWS-AWSManagedRulesAnonymousIpList", ".", ".", { "region" : "us-gov-west-1" }],
+            [".", "BlockedRequests", ".", ".", ".", ".", ".", ".", { "region" : "us-gov-west-1" }],
+            [".", "CountedRequests", ".", ".", ".", ".", ".", ".", { "region" : "us-gov-west-1" }],
+            [".", "CaptchaRequests", ".", ".", ".", ".", ".", ".", { "region" : "us-gov-west-1" }],
+            [".", "ChallengeRequests", ".", ".", ".", ".", ".", ".", { "region" : "us-gov-west-1" }],
+            [".", "AllowedRequests", ".", ".", ".", "production-AWS-ManagedRulesAmazonIpReputationList", ".", ".", { "region" : "us-gov-west-1" }],
+            [".", "BlockedRequests", ".", ".", ".", ".", ".", ".", { "region" : "us-gov-west-1" }],
+            [".", "CountedRequests", ".", ".", ".", ".", ".", ".", { "region" : "us-gov-west-1" }],
+            [".", "CaptchaRequests", ".", ".", ".", ".", ".", ".", { "region" : "us-gov-west-1" }],
+            [".", "ChallengeRequests", ".", ".", ".", ".", ".", ".", { "region" : "us-gov-west-1" }],
+            [".", "AllowedRequests", ".", ".", ".", "production-AWS-KnownBadInputsRuleSet", ".", ".", { "region" : "us-gov-west-1" }],
+            [".", "BlockedRequests", ".", ".", ".", ".", ".", ".", { "region" : "us-gov-west-1" }],
+            [".", "CountedRequests", ".", ".", ".", ".", ".", ".", { "region" : "us-gov-west-1" }],
+            [".", "CaptchaRequests", ".", ".", ".", ".", ".", ".", { "region" : "us-gov-west-1" }],
+            [".", "ChallengeRequests", ".", ".", ".", ".", ".", ".", { "region" : "us-gov-west-1" }],
+            [".", "AllowedRequests", ".", ".", ".", "production-AWS-AWSManagedRulesCommonRuleSet", ".", ".", { "region" : "us-gov-west-1" }],
+            [".", "BlockedRequests", ".", ".", ".", ".", ".", ".", { "region" : "us-gov-west-1" }],
+            [".", "CountedRequests", ".", ".", ".", ".", ".", ".", { "region" : "us-gov-west-1" }],
+            [".", "CaptchaRequests", ".", ".", ".", ".", ".", ".", { "region" : "us-gov-west-1" }],
+            [".", "ChallengeRequests", ".", ".", ".", ".", ".", ".", { "region" : "us-gov-west-1" }],
+            [".", "BlockedRequests", ".", ".", ".", "BlockMaliciousFingerprints", ".", ".", { "region" : "us-gov-west-1" }],
+            [".", "CountedRequests", ".", ".", ".", "CountAPIDataGovRequests", ".", ".", { "region" : "us-gov-west-1" }],
+            [".", "ChallengeRequests", ".", ".", ".", "RateLimitBySourceIP-Mboyd-test", ".", ".", { "region" : "us-gov-west-1" }],
+            ["...", "RateLimitByForwardedIP-Mboyd-test", ".", ".", { "region" : "us-gov-west-1" }],
+            [".", "BlockedRequests", ".", ".", ".", "production-RateLimitNonCDN", ".", ".", { "region" : "us-gov-west-1" }],
+            ["...", "production-AWS-AWSManagedRulesCommonRuleSet", ".", ".", { "region" : "us-gov-west-1" }],
+            [".", "AllowedRequests", ".", ".", ".", "ALL", ".", ".", { "region" : "us-gov-west-1", "visible" : false }],
+            [".", "BlockedRequests", ".", ".", ".", ".", ".", ".", { "region" : "us-gov-west-1", "visible" : false }],
+            [".", "CountedRequests", ".", ".", ".", ".", ".", ".", { "region" : "us-gov-west-1", "visible" : false }],
+            [".", "CaptchaRequests", ".", ".", ".", ".", ".", ".", { "region" : "us-gov-west-1", "visible" : false }],
+            [".", "ChallengeRequests", ".", ".", ".", ".", ".", ".", { "region" : "us-gov-west-1", "visible" : false }],
+            [".", "AllowedRequests", ".", ".", ".", "production-cf-uaa-waf-core-metric", ".", ".", { "region" : "us-gov-west-1" }]
+          ],
+          "stat" : "Sum",
+          "view" : "timeSeries",
+          "stacked" : true,
+          "period" : 300,
+          "region" : "us-gov-west-1",
+          "title" : "WAF: Requests by Rule and Action",
+          "yAxis" : {
+            "left" : {
+              "showUnits" : false
+            },
+            "right" : {
+              "showUnits" : false
+            }
+          }
+        }
+      },
+      {
+        "height" : 6,
+        "width" : 6,
+        "y" : 9,
+        "x" : 0,
+        "type" : "log",
+        "properties" : {
+          "query" : "SOURCE 'aws-waf-logs-production' | stats count(*) as requestCount by httpRequest.clientIp\n| sort requestCount desc\n| limit 100",
+          "region" : "us-gov-west-1",
+          "stacked" : false,
+          "title" : "Top 100 Source IPs by Request Count",
+          "view" : "table"
+        }
+      },
+      {
+        "height" : 4,
+        "width" : 6,
+        "y" : 15,
+        "x" : 0,
+        "type" : "log",
+        "properties" : {
+          "query" : "SOURCE 'aws-waf-logs-production' | fields @timestamp, @message, @logStream, @log\n| filter @message like \"count-api-data-gov-requests\"\n| parse @message /\\{\"name\":\"(H|h)ost\",\"value\":\"(?<host>.*?)\"\\}/\n| stats count(*) as requestCount by host\n| sort requestCount desc\n| limit 100",
+          "region" : "us-gov-west-1",
+          "stacked" : false,
+          "title" : "Top 100 data.gov Target Hosts by Request Count",
+          "view" : "table"
+        }
+      },
+      {
+        "height" : 9,
+        "width" : 12,
+        "y" : 0,
+        "x" : 0,
+        "type" : "metric",
+        "properties" : {
+          "metrics" : [
+            ["AWS/WAFV2", "AllowedRequests", "WebACL", "production-cf-uaa-waf-core", "Region", "us-gov-west-1", "Rule", "ALL", { "region" : "us-gov-west-1" }],
+            [".", "CountedRequests", ".", ".", ".", ".", ".", ".", { "region" : "us-gov-west-1" }],
+            [".", "ChallengeRequests", ".", ".", ".", ".", ".", ".", { "region" : "us-gov-west-1" }],
+            [".", "RequestsWithValidChallengeToken", ".", ".", ".", ".", ".", ".", { "region" : "us-gov-west-1" }],
+            [".", "BlockedRequests", ".", ".", ".", ".", ".", ".", { "region" : "us-gov-west-1" }]
+          ],
+          "stat" : "Sum",
+          "view" : "timeSeries",
+          "stacked" : true,
+          "period" : 300,
+          "region" : "us-gov-west-1",
+          "title" : "WAF Summary: Count of Requests by Action",
+          "yAxis" : {
+            "left" : {
+              "showUnits" : false
+            },
+            "right" : {
+              "showUnits" : false
+            }
+          }
+        }
+      },
+      {
+        "height" : 10,
+        "width" : 8,
+        "y" : 9,
+        "x" : 6,
+        "type" : "log",
+        "properties" : {
+          "query" : "SOURCE 'aws-waf-logs-production' | fields @timestamp, @message, action\n| parse @message /(?i)\"name\":\"[Hh]ost\",\"value\":\"(?<host>[^\"]+)/\n| stats count() as count group by host, action\n| sort count desc\n| limit 20",
+          "region" : "us-gov-west-1",
+          "stacked" : false,
+          "view" : "table",
+          "title" : "Top 20 Target Hosts by Request Count and WAF Action"
+        }
+      }
+    ]
+  })
+}
