9ef2007999faa008d90dcd4e25022267d78ff824
David Anderson <david.n.anderson@gsa.gov>
2024-02-29 10:55:22 -0500

    remove vars

--- terraform/stacks/tooling/variables.tf
+++ terraform/stacks/tooling/variables.tf
@@ -189,12 +189,6 @@ variable "oidc_client" {
 variable "oidc_client_secret" {
 }
 
-variable "defectdojo_staging_oidc_client" {
-}
-
-variable "defectdojo_staging_oidc_client_secret" {
-}
-
 variable "opslogin_hostname" {
 }
 
