77e7077103c23159d284682b7d34c49213c62eb2
Robert Gottlieb <robert.gottlieb@gsa.gov>
2024-03-19 10:58:13 -0400

    rotating access key for external domain broker prod gov

--- terraform/modules/external_domain_broker_govcloud/resources.tf
+++ terraform/modules/external_domain_broker_govcloud/resources.tf
@@ -15,11 +15,11 @@ resource "aws_iam_user" "iam_user" {
   name = "external-domain-broker-gov-${var.stack_description}"
 }
 
-resource "aws_iam_access_key" "iam_access_key_v1" {
+resource "aws_iam_access_key" "iam_access_key_v2" {
   user = aws_iam_user.iam_user.name
 }
 
-resource "aws_iam_access_key" "iam_access_key_v2" {
+resource "aws_iam_access_key" "iam_access_key_v3" {
   user = aws_iam_user.iam_user.name
 }
 
