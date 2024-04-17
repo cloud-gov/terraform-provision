2b4f47b16996bdb02706433a46be5947b11bff46
Mark Boyd <mark.boyd@gsa.gov>
2024-04-17 14:57:33 -0400

    remove unused CloudFront and and web ACL resources for protecting load balancers

--- terraform/stacks/cloudfront/stack.tf
+++ terraform/stacks/cloudfront/stack.tf
@@ -1,17 +1,3 @@
-# Most stacks do not pass credentials this way. For an explanation of why
-# access and state are managed differently in the cloudfront stack compared
-# to the other stacks, see README.md, "DNS and CloudFront Stacks" in the root
-# of the repo.
-variable "aws_access_key" {
-}
-
-variable "aws_secret_key" {
-  sensitive = true
-}
-
-variable "aws_region" {
-}
-
 terraform {
   backend "s3" {
   }
@@ -45,33 +31,4 @@ data "terraform_remote_state" "govcloud" {
     region = var.remote_state_region
     key    = "${var.stack_description}/terraform.tfstate"
   }
-}
-
-# One ACL for all distributions in the environment.
-resource "aws_wafv2_web_acl" "commercial_acl" {
-  name        = "${var.stack_description}-commercial-acl"
-  description = "ACL for use with CloudFront and Shield Advanced."
-  scope       = "CLOUDFRONT"
-
-  default_action {
-    # no rules needed; they'll be added by Shield Advanced.
-    allow {
-    }
-  }
-
-  visibility_config {
-    cloudwatch_metrics_enabled = true
-    metric_name                = "${var.stack_description}-commercial-acl-metric"
-    sampled_requests_enabled   = true
-  }
-}
-
-module "star_admin" {
-  source = "../../modules/cloudfront"
-  origin = {
-    lb_dns_name   = data.terraform_remote_state.govcloud.outputs.cf_lb_dns_name
-    lb_http_port  = 80  # modules/cloudfoundry/elb_main.tf "aws_lb" "cf"
-    lb_https_port = 443 # modules/cloudfoundry/elb_main.tf "aws_lb" "cf"
-  }
-  acl_arn = aws_wafv2_web_acl.commercial_acl.arn
 }
--- terraform/stacks/cloudfront/variables.tf
+++ terraform/stacks/cloudfront/variables.tf
@@ -1,3 +1,17 @@
+# Most stacks do not pass credentials this way. For an explanation of why
+# access and state are managed differently in the cloudfront stack compared
+# to the other stacks, see README.md, "DNS and CloudFront Stacks" in the root
+# of the repo.
+variable "aws_access_key" {
+}
+
+variable "aws_secret_key" {
+  sensitive = true
+}
+
+variable "aws_region" {
+}
+
 variable "stack_description" {
   type = string
 }
