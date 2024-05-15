b2438508c8c921a790987cd3e0a0d9c04ff0699d
Mark Boyd <mark.boyd@gsa.gov>
2024-05-15 16:45:07 -0400

    fix formatting

--- terraform/modules/external_domain_broker/cloudwatch.tf
+++ terraform/modules/external_domain_broker/cloudwatch.tf
@@ -1,4 +1,4 @@
 resource "aws_cloudwatch_log_group" "external_domain_waf_logs" {
-  name = "aws-waf-logs-external-domain-broker-${var.stack_description}"
+  name              = "aws-waf-logs-external-domain-broker-${var.stack_description}"
   retention_in_days = 365
 }
