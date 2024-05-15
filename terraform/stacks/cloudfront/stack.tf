5d1d6a197ee068ca9720d01252c31b488b145f34
Mark Boyd <mark.boyd@gsa.gov>
2024-05-15 11:24:17 -0400

    Fix TF deployment error (#1667)
    * remove custom endpoint
    
    * try pinning the provider
    
    * point cloudfront deployments at dev branch
    
    * revert pipeline changes
    
    * fix TF formatting

--- terraform/stacks/cloudfront/stack.tf
+++ terraform/stacks/cloudfront/stack.tf
@@ -1,6 +1,15 @@
 terraform {
   backend "s3" {
   }
+
+  required_version = ">= 0.15"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "< 5.48.0"
+    }
+  }
 }
 
 provider "aws" {
