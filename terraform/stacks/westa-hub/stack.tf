372b8a83eb2d1131160e617908529687966dee09
Stephen Greenberg <stephen.greenberg@gsa.gov>
2024-06-06 12:08:42 -0600

    unused

--- terraform/stacks/westa-hub/stack.tf
+++ terraform/stacks/westa-hub/stack.tf
@@ -33,11 +33,6 @@ data "aws_iam_server_certificate" "wildcard_production" {
   latest      = true
 }
 
-data "aws_iam_server_certificate" "wildcard_staging" {
-  name_prefix = var.wildcard_staging_certificate_name_prefix
-  latest      = true
-}
-
 resource "aws_lb" "main" {
   name            = "${var.stack_description}-main"
   subnets         = module.stack.public_subnet_ids
@@ -69,11 +64,6 @@ resource "aws_lb_target_group" "dummy" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = module.stack.vpc_id
-}
-
-resource "aws_lb_listener_certificate" "main-staging" {
-  listener_arn    = aws_lb_listener.main.arn
-  certificate_arn = data.aws_iam_server_certificate.wildcard_staging.arn
 }
 
 module "stack" {
@@ -108,7 +98,7 @@ module "stack" {
 
   bosh_default_ssh_public_key            = tls_private_key.bosh_key.public_key_openssh
   target_concourse_security_group_cidrs  = [cidrsubnet(var.vpc_cidr, 8, 30), cidrsubnet(var.vpc_cidr, 8, 31), cidrsubnet(var.vpc_cidr, 8, 32), cidrsubnet(var.vpc_cidr, 8, 33), cidrsubnet(var.vpc_cidr, 8, 34), cidrsubnet(var.vpc_cidr, 8, 35), cidrsubnet(var.vpc_cidr, 8, 1)]
-  target_monitoring_security_group_cidrs = [cidrsubnet(var.vpc_cidr, 8, 42), cidrsubnet(var.vpc_cidr, 8, 43), cidrsubnet(var.vpc_cidr, 8, 44)] //Why only Prod?
+  #target_monitoring_security_group_cidrs = [cidrsubnet(var.vpc_cidr, 8, 42), cidrsubnet(var.vpc_cidr, 8, 43), cidrsubnet(var.vpc_cidr, 8, 44)] //Why only Prod?
   s3_gateway_policy_accounts             = var.s3_gateway_policy_accounts
 }
 
@@ -137,31 +127,6 @@ module "concourse_production" {
   hosts                           = var.concourse_production_hosts
 }
 
-module "concourse_staging" {
-  source                          = "../../modules/concourse_v2"
-  stack_description               = "${var.stack_description}-stage" # var.stack_description is too long, max 32 length total
-  vpc_id                          = module.stack.vpc_id
-  concourse_cidrs                 = [cidrsubnet(var.vpc_cidr, 8, 33), cidrsubnet(var.vpc_cidr, 8, 34), cidrsubnet(var.vpc_cidr, 8, 35)]
-  concourse_availability_zones    = [data.aws_availability_zones.available.names[0], data.aws_availability_zones.available.names[1], data.aws_availability_zones.available.names[2]]
-  route_table_ids                 = module.stack.private_route_table_ids
-  rds_password                    = random_string.concourse_staging_rds_password.result
-  rds_subnet_group                = module.stack.rds_subnet_group
-  rds_security_groups             = [module.stack.rds_postgres_security_group]
-  rds_parameter_group_name        = "tooling-concourse-staging"
-  rds_parameter_group_family      = var.rds_parameter_group_family
-  rds_db_engine_version           = var.rds_db_engine_version
-  rds_apply_immediately           = var.rds_apply_immediately
-  rds_allow_major_version_upgrade = var.rds_allow_major_version_upgrade
-  rds_instance_type               = var.concourse_staging_rds_instance_type
-  rds_db_size                     = 400
-  rds_db_storage_type             = "gp3"
-  rds_db_iops                     = 12000
-  rds_multi_az                    = var.rds_multi_az
-  rds_final_snapshot_identifier   = "final-snapshot-atc-tooling-staging"
-  listener_arn                    = aws_lb_listener.main.arn
-  hosts                           = var.concourse_staging_hosts
-}
-
 module "credhub_production" {
   source                          = "../../modules/credhub_v2"
   stack_description               = "${var.stack_description}-prod"
@@ -187,59 +152,6 @@ module "credhub_production" {
   hosts                           = var.credhub_production_hosts
 }
 
-module "credhub_staging" {
-  source                          = "../../modules/credhub_v2"
-  stack_description               = "${var.stack_description}-stage"
-  vpc_id                          = module.stack.vpc_id
-  credhub_cidrs                   = [cidrsubnet(var.vpc_cidr, 8, 39), cidrsubnet(var.vpc_cidr, 8, 40), cidrsubnet(var.vpc_cidr, 8, 41)]
-  credhub_availability_zones      = [data.aws_availability_zones.available.names[0], data.aws_availability_zones.available.names[1], data.aws_availability_zones.available.names[2]]
-  route_table_ids                 = module.stack.private_route_table_ids
-  rds_password                    = random_string.credhub_staging_rds_password.result
-  rds_subnet_group                = module.stack.rds_subnet_group
-  rds_security_groups             = [module.stack.rds_postgres_security_group]
-  rds_parameter_group_name        = "tooling-credhub-staging"
-  rds_parameter_group_family      = var.rds_parameter_group_family
-  rds_db_engine_version           = var.rds_db_engine_version
-  rds_apply_immediately           = var.rds_apply_immediately
-  rds_allow_major_version_upgrade = var.rds_allow_major_version_upgrade
-  rds_instance_type               = var.credhub_staging_rds_instance_type
-  rds_db_size                     = 400
-  rds_db_storage_type             = "gp3"
-  rds_db_iops                     = 12000
-  rds_multi_az                    = var.rds_multi_az
-  rds_final_snapshot_identifier   = "final-snapshot-credhub-tooling-staging"
-  listener_arn                    = aws_lb_listener.main.arn
-  hosts                           = var.credhub_staging_hosts
-}
-
-# TODO: need to rework route_table_id_az* to work with modified module.stack
-#module "defectdojo_staging" {
-#  source                          = "../../modules/defect_dojo"
-#  stack_description               = var.stack_description
-#  vpc_id                          = module.stack.vpc_id
-#  defectdojo_cidr_az1             = cidrsubnet(var.vpc_cidr, 8, 48)
-#  defectdojo_cidr_az2             = cidrsubnet(var.vpc_cidr, 8, 49)
-#  defectdojo_az1                  = data.aws_availability_zones.available.names[0]
-#  defectdojo_az2                  = data.aws_availability_zones.available.names[1]
-#  route_table_id_az1              = module.stack.private_route_table_az1
-#  route_table_id_az2              = module.stack.private_route_table_az2
-#  rds_password                    = var.defectdojo_staging_rds_password
-#  rds_subnet_group                = module.stack.rds_subnet_group
-#  rds_security_groups             = [module.stack.rds_postgres_security_group]
-#  rds_parameter_group_name        = "tooling-defectdojo-staging"
-#  rds_parameter_group_family      = var.rds_parameter_group_family
-#  rds_db_engine_version           = var.rds_db_engine_version
-#  rds_apply_immediately           = var.rds_apply_immediately
-#  rds_allow_major_version_upgrade = var.rds_allow_major_version_upgrade
-#  rds_instance_type               = "db.m5.large"
-#  rds_db_size                     = 400
-#  rds_db_storage_type             = "gp3"
-#  rds_db_iops                     = 12000
-#  rds_multi_az                    = var.rds_multi_az
-#  rds_final_snapshot_identifier   = "final-snapshot-defectdojo-tooling-staging"
-#  listener_arn                    = aws_lb_listener.main.arn
-#  hosts                           = var.defectdojo_staging_hosts
-#}
 
 module "monitoring_production" {
   source                        = "../../modules/monitoring_v2"
@@ -256,35 +168,11 @@ module "monitoring_production" {
   opslogin_hostname             = var.opslogin_hostname
 }
 
-module "monitoring_staging" {
-  source                        = "../../modules/monitoring_v2"
-  stack_description             = "${var.stack_description}-stage"
-  vpc_id                        = module.stack.vpc_id
-  vpc_cidr                      = var.vpc_cidr
-  monitoring_cidrs              = [cidrsubnet(var.vpc_cidr, 8, 45), cidrsubnet(var.vpc_cidr, 8, 46), cidrsubnet(var.vpc_cidr, 8, 47)]
-  monitoring_availability_zones = [data.aws_availability_zones.available.names[0], data.aws_availability_zones.available.names[1], data.aws_availability_zones.available.names[2]]
-  route_table_ids               = module.stack.private_route_table_ids
-  listener_arn                  = aws_lb_listener.main.arn
-  hosts                         = var.monitoring_staging_hosts
-  oidc_client                   = var.oidc_client
-  oidc_client_secret            = random_string.oidc_client_secret.result
-  opslogin_hostname             = var.opslogin_hostname
-}
 
 resource "aws_eip" "production_dns_eip" {
   vpc = true
 
   count = var.dns_eip_count_production
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
-resource "aws_eip" "staging_dns_eip" {
-  vpc = true
-
-  count = var.dns_eip_count_staging
 
   lifecycle {
     prevent_destroy = true
@@ -304,8 +192,6 @@ module "smtp" {
   ingress_cidr_blocks = var.smtp_ingress_cidr_blocks
 }
 
-
-
 module "jumpbox" {
   ec2_name               = "jumpbox" # This needs to be changed to protobosh-jumpbox or something like that
   count                  = var.create_jumpbox ? 1 : 0
@@ -318,15 +204,3 @@ module "jumpbox" {
   iam_instance_profile   = "bootstrap" #"westa-hub-protobosh" # "bootstrap" 
 }
 
-
-# module "tooling_jumpbox" {
-#  ec2_name               = "tooling-jumpbox"
-#  count                  = var.create_jumpbox ? 1 : 0
-#  source                 = "../../modules/jumpbox"
-#  subnet_id              = module.stack.private_subnet_ids[0]
-#  vpc_security_group_ids = [module.stack.bosh_security_group]
-#  stack_description      = var.stack_description
-#  private_ip             = cidrhost(cidrsubnet(var.vpc_cidr, 8, 1), 221) # This is from the private subnet, the ip offset is just higher in the range so bosh never tries to use it.  Change if there is a conflict later on
-#  instance_type          = "m5.large"
-#  iam_instance_profile   = "bootstrap" #"westa-hub-protobosh" # "bootstrap" 
-# }
