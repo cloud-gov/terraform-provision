4d81641ce8a29424a130d96cbd2abcfa2c895c55
Stephen Greenberg <stephen.greenberg@gsa.gov>
2024-06-25 08:27:57 -0600

    provision cert from let's encrypt

---
+++ terraform/modules/acme/certificate.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    acme = {
+      source = "vancluever/acme"
+      version = "~>2.23"
+    }
+  }
+}
+
+locals {
+  config  = yamldecode(file("${path.module}/${var.config_file_name}"))
+}
+
+provider "acme" {
+  server_url = locals.config.acme.use_test_endpoint ? "https://acme-staging-v02.api.letsencrypt.org/directory" : "https://acme-v02.api.letsencrypt.org/directory"
+}
+
+resource "tls_private_key" "private_key" {
+  algorithm = "RSA"
+}
+
+resource "acme_registration" "reg" {
+  account_key_pem = tls_private_key.private_key.private_key_pem
+  email_address   = locals.config.acme.registration_email
+}
+
+resource "acme_certificate" "certificate" {
+  account_key_pem           = acme_registration.reg.account_key_pem
+  common_name               = locals.config.acme.common_name
+  subject_alternative_names = locals.config.acme.subject_alternative_names
+
+  dns_challenge {
+    provider = "route53"
+  }
+}
+
---
+++ terraform/modules/acme/variables.tf
@@ -0,0 +1,5 @@
+variable "config_file_name" {
+  type = string
+  description = "The name of the configuration file. Example: tooling.yml"
+  nullable = false
+}
