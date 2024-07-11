7495c11f4714b9c67934c6693a83a387c871e259
= <ben.berry@gsa.gov>
2024-07-11 11:02:23 -0700

    feat: move certificate deletion to its own task

--- ci/delete-old-certificates.sh
+++ ci/delete-old-certificates.sh
@@ -12,12 +12,11 @@ if [[ -n "${ASSUME_ROLE_ARN}" ]]; then
   export AWS_SESSION_TOKEN="${session_token}"
 fi
 
-# Upload new certificate if present
-if [[ -s acme/cert.pem ]]; then
-  aws iam upload-server-certificate \
-    --path "${CERT_PATH}" \
-    --server-certificate-name "${CERT_PREFIX}-$(date +%Y-%m-%d-%H-%M)" \
-    --certificate-body file://acme/cert.pem \
-    --certificate-chain file://acme/chain.pem \
-    --private-key file://acme/privkey.pem
-fi
+# Delete expired certificates
+cutoff=$(date --date '-1 month' --iso-8601=seconds --utc | sed 's/+0000$/Z/')
+certificates=$(jq -r --arg cutoff "${cutoff}" \
+  '.ServerCertificateMetadataList[] | select(.Expiration < $cutoff) | .ServerCertificateName' \
+  < certificates/metadata)
+for certificate in ${certificates}; do
+  aws iam delete-server-certificate --server-certificate-name "${certificate}"
+done
