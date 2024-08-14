abf0f33afa229b98b7574f587555133ffb216aea
Robert Gottlieb <robert.gottlieb@gsa.gov>
2024-08-14 10:38:59 -0400

    fixed container name and removed ./ for spruce

--- ci/rotate-keys.sh
+++ ci/rotate-keys.sh
@@ -2,8 +2,8 @@
 
 set -eu
 
-AWS_ACCESS_KEY_ID=$(./spruce json terraform-yaml-external/state.yml | jq -r ".terraform_outputs.iam_dev_user_access_key_id_curr")
-AWS_SECRET_ACCESS_KEY=$(./spruce json terraform-yaml-external/state.yml | jq -r ".terraform_outputs.iam_dev_user_secret_access_key_curr")
+AWS_ACCESS_KEY_ID=$(spruce json terraform-yaml-external/state.yml | jq -r ".terraform_outputs.iam_dev_user_access_key_id_curr")
+AWS_SECRET_ACCESS_KEY=$(spruce json terraform-yaml-external/state.yml | jq -r ".terraform_outputs.iam_dev_user_secret_access_key_curr")
 
 credhub set -t value -n /roberts/awesome/adventure/dev_access_key -v $AWS_SECRET_ACCESS_KEY
 credhub set -t value -n /roberts/awesome/adventure/dev_access_key_id -v $AWS_ACCESS_KEY_ID
