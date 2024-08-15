e065c68fd1f95338d2c4417397e90f0f44f5849c
Robert Gottlieb <robert.gottlieb@gsa.gov>
2024-08-15 15:05:00 -0400

    Forgot to run fmt

--- terraform/stacks/external/stack.tf
+++ terraform/stacks/external/stack.tf
@@ -37,9 +37,9 @@ data "aws_caller_identity" "current" {
 module "external_domain_broker" {
   source = "../../modules/external_domain_broker"
 
-  account_id               = data.aws_caller_identity.current.account_id
-  stack_description        = var.stack_description
-  aws_partition            = data.aws_partition.current.partition
+  account_id        = data.aws_caller_identity.current.account_id
+  stack_description = var.stack_description
+  aws_partition     = data.aws_partition.current.partition
 
   providers = {
     aws          = aws.fips
