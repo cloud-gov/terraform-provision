a21ae1332a6a9d2c48f2a7fbca3c2bbe4c058d16
James Hochadel <james.hochadel@gsa.gov>
2024-09-11 10:55:04 -0400

    Add missing types to CSB variables

--- terraform/modules/csb/broker/variables.tf
+++ terraform/modules/csb/broker/variables.tf
@@ -1,38 +1,47 @@
 variable "stack_description" {
+  type        = string
   description = "Like development, staging, or production."
 }
 
 # RDS variables
 
 variable "rds_instance_type" {
+  type    = string
   default = "db.t3.small"
 }
 
 variable "rds_db_size" {
+  type    = string
   default = 20
 }
 
 variable "rds_db_name" {
+  type    = string
   default = "csb"
 }
 
 variable "rds_db_engine" {
+  type    = string
   default = "mysql"
 }
 
 variable "rds_db_engine_version" {
+  type    = string
   default = "8.0"
 }
 
 variable "rds_parameter_group_family" {
+  type    = string
   default = "mysql8.0"
 }
 
 variable "rds_username" {
+  type    = string
   default = "csb"
 }
 
 variable "rds_password" {
+  type      = string
   sensitive = true
 }
 
