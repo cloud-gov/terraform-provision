dec3c8cab544e764599f51e862303a1a7c3786db
Mark Boyd <mark.boyd@gsa.gov>
2024-09-26 10:34:45 -0400

    Fix ECR tag detection (#1786)
    * remove tags_all and tags from aws_ecr_repository
    
    * add provider with default tags
    
    * add provider definition with default tags for ECR repositories

--- terraform/stacks/ecr/stack.tf
+++ terraform/stacks/ecr/stack.tf
@@ -1,60 +1,17 @@
-variable "remote_state_bucket" {
-}
-
-variable "tooling_stack_name" {
-}
-
-variable "repositories" {
-  type = set(string)
-  default = [
-    "bosh-deployment-resource",
-    "bosh-io-release-resource",
-    "bosh-io-stemcell-resource",
-    "cf-cli-resource",
-    "cf-resource",
-    "cloud-service-broker",
-    "concourse-http-jq-resource",
-    "concourse-task",
-    "cron-resource",
-    "csb",
-    "email-resource",
-    "external-domain-broker-testing",
-    "external-domain-broker-migrator-testing",
-    "general-task",
-    "git-resource",
-    "github-pr-resource",
-    "github-release-resource",
-    "legacy-domain-certificate-renewer-testing",
-    "oci-build-task",
-    "openresty",
-    "pages-dind",
-    "pages-dind-v25",
-    "pages-nginx-v1",
-    "pages-node-v20",
-    "pages-postgres-v15",
-    "pages-python-v3.11",
-    "pages-redis-v7.2",
-    "pages-zap",
-    "playwright-python",
-    "pool-resource",
-    "pulledpork",
-    "registry-image-resource",
-    "s3-resource",
-    "s3-simple-resource",
-    "semver-resource",
-    "slack-notification-resource",
-    "opensearch-testing",
-    "opensearch-dashboards-testing",
-    "time-resource",
-    "ubuntu-hardened",
-  ]
-}
-
 terraform {
   backend "s3" {
   }
 }
 
+provider "aws" {
+  use_fips_endpoint = true
+
+  default_tags {
+    tags = {
+      deployment = "ecr"
+    }
+  }
+}
 
 data "terraform_remote_state" "tooling" {
   backend = "s3"
@@ -70,8 +27,6 @@ resource "aws_ecr_repository" "repository" {
 
   name                 = each.key
   image_tag_mutability = "MUTABLE"
-  tags                 = {}
-  tags_all             = {}
 }
 
 resource "aws_ecr_lifecycle_policy" "ecr_repository_lifecycle_policy" {
--- terraform/stacks/tooling/variables.tf
+++ terraform/stacks/tooling/variables.tf
@@ -30,7 +30,7 @@ variable "rds_apply_immediately" {
 }
 
 variable "rds_db_engine_version" {
-  default = "12.17"
+  default = "12.19"
 }
 
 variable "rds_parameter_group_family" {
