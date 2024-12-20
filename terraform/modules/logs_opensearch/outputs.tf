6507015082436803ce1a30169ec45fa2ae8e0c5e
Jason A. Gambino <jason.gambino@gsa.gov>
2024-12-20 13:57:29 -0500

    removing elb for opensearch

--- terraform/modules/logs_opensearch/outputs.tf
+++
@@ -1,8 +0,0 @@
-/* logs_opensearch ELB */
-output "logs_opensearch_elb_name" {
-  value = aws_elb.logs_opensearch_elb.name
-}
-
-output "logs_opensearch_elb_dns_name" {
-  value = aws_elb.logs_opensearch_elb.dns_name
-}
