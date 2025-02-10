889fb82eaf5799fb6a50a75578b4b427577e2f50
Robert Gottlieb <robert.gottlieb@gsa.gov>
2025-02-10 13:52:46 -0500

    create new key for rotation for iam cert provision

--- terraform/modules/iam_user/iam_cert_provision/outputs.tf
+++ terraform/modules/iam_user/iam_cert_provision/outputs.tf
@@ -3,17 +3,17 @@ output "username" {
 }
 
 output "access_key_id_prev" {
-  value = ""
-}
-
-output "secret_access_key_prev" {
-  value = ""
-}
-
-output "access_key_id_curr" {
   value = aws_iam_access_key.iam_access_key_v3.id
 }
 
-output "secret_access_key_curr" {
+output "secret_access_key_prev" {
   value = aws_iam_access_key.iam_access_key_v3.secret
+}
+
+output "access_key_id_curr" {
+  value = aws_iam_access_key.iam_access_key_v4.id
+}
+
+output "secret_access_key_curr" {
+  value = aws_iam_access_key.iam_access_key_v4.secret
 }
