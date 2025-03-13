3890bc899c6bd3b8a22b4d8def2f67908bed0439
James Hochadel <james.hochadel@gsa.gov>
2025-03-13 08:53:20 -0400

    Use correct external apex domains

--- terraform/modules/environment_dns/dns.tf
+++ terraform/modules/environment_dns/dns.tf
@@ -224,12 +224,16 @@ resource "aws_route53_record" "brokered_mail_ns" {
 }
 
 locals {
+  external_domains_map = {
+    "development" = "external-domains-dev.cloud.gov."
+    "staging"     = "external-domains-staging.cloud.gov."
+    "production"  = "external-domains-production.cloud.gov."
+  }
   # Work around the staging apex domain being inconsistent with the rest.
-  external_domain_suffix        = trimprefix(var.domain, "fr-stage.")
   csb_helper_domain_name        = "services.${var.domain}."
-  csb_helper_domain_record      = "services.${var.domain}.external-domains-${var.stack_name}.${local.external_domain_suffix}."
+  csb_helper_domain_record      = "services.${var.domain}.${external_domains_map[var.stack_name]}"
   csb_helper_acme_domain_name   = "_acme-challenge.services.${var.domain}."
-  csb_helper_acme_domain_record = "_acme-challenge.services.${var.domain}.external-domains-${var.stack_name}.${local.external_domain_suffix}."
+  csb_helper_acme_domain_record = "_acme-challenge.services.${var.domain}.${external_domains_map[var.stack_name]}"
 }
 
 // DNS records corresponding to the External Domain Service Instance
