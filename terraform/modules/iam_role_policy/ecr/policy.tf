b42f5d68c4f867ab7201055b7dc9609cc417cd16
Mark Boyd <mark.boyd@gsa.gov>
2025-03-18 11:51:06 -0400

    replace usage of template_file with aws_iam_policy_document in iam_role_policy/ecr module

--- terraform/modules/iam_role_policy/ecr/policy.tf
+++ terraform/modules/iam_role_policy/ecr/policy.tf
@@ -1,14 +1,39 @@
-data "template_file" "policy" {
-  template = file("${path.module}/policy.json")
+data "aws_iam_policy_document" "ecr_policy" {
+  statement {
+    actions = [
+      "ecr:BatchGetImage",
+      "ecr:DescribeImages",
+      "ecr:DescribeRepositories",
+      "ecr:BatchDeleteImage",
+      "ecr:ListImages",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetRepositoryPolicy",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:PutImageTagMutability"
+    ]
 
-  vars = {
-    account_id         = var.account_id
-    aws_partition      = var.aws_partition
-    aws_default_region = var.aws_default_region
+    resources = [
+      "arn:${var.aws_partition}:ecr:${var.aws_default_region}:${var.account_id}:repository/*"
+    ]
   }
+
+  statement {
+    actions = [
+      "ecr:CreateRepository",
+      "ecr:DeleteRepository",
+      "ecr:GetAuthorizationToken",
+      "ecr:DeleteLifecyclePolicy",
+      "ecr:GetLifecyclePolicy",
+      "ecr:PutLifecyclePolicy"
+    ]
+
+    resources = [
+      "*"
+    ]
+  }  
 }
 
 resource "aws_iam_policy" "iam_policy" {
   name   = var.policy_name
-  policy = data.template_file.policy.rendered
+  policy = data.aws_iam_policy_document.ecr_policy.json
 }
