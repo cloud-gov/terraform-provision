4ffcd22fe061b24836a3b28f8a27cc511d8bd53f
Mark Boyd <mark.boyd@gsa.gov>
2025-03-18 13:15:12 -0400

    replace usage of template_file with aws_iam_policy_document in iam_user/iam_cert_provision module

--- terraform/modules/iam_user/iam_cert_provision/user.tf
+++ terraform/modules/iam_user/iam_cert_provision/user.tf
@@ -1,9 +1,25 @@
-data "template_file" "policy" {
-  template = file("${path.module}/policy.json")
-  vars = {
-    account_id    = var.account_id
-    aws_partition = var.aws_partition
+data "aws_iam_policy_document" "iam_cert_provision_policy" {
+  statement {
+    actions = [
+      "iam:ListServerCertificates",
+      "iam:UploadServerCertificate",
+      "iam:DeleteServerCertificate"
+    ]
+
+    resources = [
+      "arn:${var.aws_partition}:iam::${var.account_id}:server-certificate/lets-encrypt/*"
+    ]
   }
+
+  statement {
+    actions = [
+      "sts:AssumeRole"
+    ]
+
+    resources = [
+      "arn:${var.aws_partition}:iam::*:role/terraform/*-tooling-cert-uploader"
+    ]
+  }  
 }
 
 resource "aws_iam_user" "iam_user" {
@@ -17,5 +33,5 @@ resource "aws_iam_access_key" "iam_access_key_v4" {
 resource "aws_iam_user_policy" "iam_policy" {
   name   = "${aws_iam_user.iam_user.name}-policy"
   user   = aws_iam_user.iam_user.name
-  policy = data.template_file.policy.rendered
+  policy = data.aws_iam_policy_document.iam_cert_provision_policy.json
 }
