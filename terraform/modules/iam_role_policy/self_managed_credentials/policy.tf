0543e1ab04780b79b2139f0305d188dcd16700c6
Mark Boyd <mark.boyd@gsa.gov>
2025-03-18 12:11:23 -0400

    replace usage of template_file with aws_iam_policy_document in iam_role_policy/self_managed_credentials module

--- terraform/modules/iam_role_policy/self_managed_credentials/policy.tf
+++ terraform/modules/iam_role_policy/self_managed_credentials/policy.tf
@@ -1,14 +1,109 @@
-data "template_file" "policy" {
-  # from https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_examples_aws_my-sec-creds-self-manage-pass-accesskeys-ssh.html
-  # and  https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_examples_aws_my-sec-creds-self-manage-mfa-only.html
-  # and  https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_examples_iam_credentials_console.html
-  template = file("${path.module}/policy.json")
-  vars = {
-    aws_partition = var.aws_partition
+# from https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_examples_aws_my-sec-creds-self-manage-pass-accesskeys-ssh.html
+# and  https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_examples_aws_my-sec-creds-self-manage-mfa-only.html
+# and  https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_examples_iam_credentials_console.html
+data "aws_iam_policy_document" "self_managed_credentials_policy" {
+  statement {
+    sid = "AllowViewAccountInfo"
+    actions = [
+      "iam:GetAccountPasswordPolicy",
+      "iam:ListVirtualMFADevices",
+      "iam:ListUsers",
+      "iam:GetAccountSummary"
+    ]
+
+    resources = [
+      "*"
+    ]
   }
+
+  statement {
+    sid = "AllowManageOwnPasswords"
+    actions = [
+      "iam:ChangePassword",
+      "iam:GetUser"
+    ]
+
+    resources = [
+      "arn:${var.aws_partition}:iam::*:user/$${aws:username}"
+    ]
+  }  
+
+  statement {
+    sid = "AllowManageOwnKeys"
+    actions = [
+      "iam:CreateAccessKey",
+      "iam:DeleteAccessKey",
+      "iam:ListAccessKeys",
+      "iam:UpdateAccessKey",
+      "iam:*ServiceSpecificCredential*",
+      "iam:*SigningCertificate*",
+      "iam:DeleteSSHPublicKey",
+      "iam:GetSSHPublicKey",
+      "iam:ListSSHPublicKeys",
+      "iam:UpdateSSHPublicKey",
+      "iam:UploadSSHPublicKey"
+    ]
+
+    resources = [
+      "arn:${var.aws_partition}:iam::*:user/$${aws:username}"
+    ]
+  }    
+
+ statement {
+    sid = "AllowManageOwnVirtualMFADevice"
+    actions = [
+      "iam:CreateVirtualMFADevice",
+      "iam:DeleteVirtualMFADevice"
+    ]
+
+    resources = [
+      "arn:${var.aws_partition}:iam::*:mfa/$${aws:username}"
+    ]
+  }   
+
+ statement {
+    sid = "AllowManageOwnUserMFA"
+    actions = [
+      "iam:DeactivateMFADevice",
+      "iam:EnableMFADevice",
+      "iam:GetUser",
+      "iam:ListMFADevices",
+      "iam:ResyncMFADevice"
+    ]
+
+    resources = [
+      "arn:${var.aws_partition}:iam::*:user/$${aws:username}"
+    ]
+  }    
+
+ statement {
+    effect = "Deny"
+    sid = "DenyAllExceptListedIfNoMFA"
+    not_actions = [
+      "iam:ChangePassword",
+      "iam:CreateVirtualMFADevice",
+      "iam:EnableMFADevice",
+      "iam:GetUser",
+      "iam:ListMFADevices",
+      "iam:ListVirtualMFADevices",
+      "iam:ResyncMFADevice",
+      "sts:GetSessionToken",
+      "sts:GetFederationToken"
+    ]
+
+    resources = [
+      "*"
+    ]
+
+    condition {
+      test = "BoolIfExists"
+      variable = "aws:MultiFactorAuthPresent"
+      values = ["false"]
+    }
+  }          
 }
 
 resource "aws_iam_policy" "iam_policy" {
   name   = var.policy_name
-  policy = data.template_file.policy.rendered
+  policy = data.aws_iam_policy_document.self_managed_credentials_policy.json
 }
