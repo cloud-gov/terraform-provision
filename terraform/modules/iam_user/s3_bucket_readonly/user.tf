160a47c6ba045e8e10d435457fe9d8dd3ad68c22
Mark Boyd <mark.boyd@gsa.gov>
2025-03-18 13:20:22 -0400

    replace usage of template_file with aws_iam_policy_document in iam_user/s3_bucket_readonly module

--- terraform/modules/iam_user/s3_bucket_readonly/user.tf
+++ terraform/modules/iam_user/s3_bucket_readonly/user.tf
@@ -1,7 +1,14 @@
-data "template_file" "policy" {
-  template = file("${path.module}/policy.json")
-  vars = {
-    "bucket_name" = var.bucket_name
+data "aws_iam_policy_document" "s3_bucket_readonly_policy" {
+  statement {
+    actions = [
+      "s3:GetObject", 
+      "s3:ListBucket"
+    ]
+
+    resources = [
+      "arn:aws:s3:::${var.bucket_name}",
+      "arn:aws:s3:::${var.bucket_name}/*"
+    ]
   }
 }
 
@@ -16,5 +23,5 @@ resource "aws_iam_access_key" "iam_access_key_v3" {
 resource "aws_iam_user_policy" "iam_policy" {
   name   = "${aws_iam_user.iam_user.name}-policy"
   user   = aws_iam_user.iam_user.name
-  policy = data.template_file.policy.rendered
+  policy = data.aws_iam_policy_document.s3_bucket_readonly_policy.json
 }
