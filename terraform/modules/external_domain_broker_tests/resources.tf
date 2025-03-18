286d6478c38e3680eeb07ecfdaa51741f249370c
Mark Boyd <mark.boyd@gsa.gov>
2025-03-18 10:31:59 -0400

    fix resource reference

--- terraform/modules/external_domain_broker_tests/resources.tf
+++ terraform/modules/external_domain_broker_tests/resources.tf
@@ -148,5 +148,5 @@ resource "aws_iam_access_key" "iam_access_key_v1" {
 resource "aws_iam_user_policy" "iam_policy" {
   name   = "${aws_iam_user.iam_user.name}-policy"
   user   = aws_iam_user.iam_user.name
-  policy = data.external_domain_broker_tests_policy.policy.json
+  policy = data.aws_iam_policy_document.external_domain_broker_tests_policy.json
 }
