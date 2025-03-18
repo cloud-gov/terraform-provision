105bd7639e8018c1ab962bb9ced1a24bdf890a12
Mark Boyd <mark.boyd@gsa.gov>
2025-03-18 10:46:12 -0400

    replace usage of template_file with aws_iam_policy_document in iam_role_policy/bosh module

--- terraform/modules/iam_role_policy/bosh/policy.tf
+++ terraform/modules/iam_role_policy/bosh/policy.tf
@@ -1,14 +1,40 @@
-data "template_file" "policy" {
-  template = file("${path.module}/policy.json")
+data "aws_iam_policy_document" "bosh_policy" {
+  statement {
+    actions = [
+      "iam:PassRole"
+    ]
 
-  vars = {
-    aws_partition = var.aws_partition
-    account_id    = var.account_id
-    bucket_name   = var.bucket_name
+    resources = [
+      "arn:${var.aws_partition}:iam::${var.account_id}:role/bosh-passed/*"
+    ]
   }
+
+  statement {
+    actions = [
+      "ec2:*",
+      "elasticloadbalancing:*"
+    ]
+
+    resources = [
+      "*"
+    ]
+  }  
+
+  statement {
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject"
+    ]
+
+    resources = [
+      "arn:${var.aws_partition}:s3:::${var.bucket_name}",
+      "arn:${var.aws_partition}:s3:::${var.bucket_name}/*"
+    ]
+  }    
 }
 
 resource "aws_iam_policy" "iam_policy" {
   name   = var.policy_name
-  policy = data.template_file.policy.rendered
+  policy = data.aws_iam_policy_document.bosh_policy.json
 }
