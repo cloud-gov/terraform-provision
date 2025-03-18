e3a8199bdc72ac79af19fc29eefa016c53b09596
Mark Boyd <mark.boyd@gsa.gov>
2025-03-18 13:22:20 -0400

    replace usage of template_file with aws_iam_policy_document in iam_user/s3_logstash module

--- terraform/modules/iam_user/s3_logstash/user.tf
+++ terraform/modules/iam_user/s3_logstash/user.tf
@@ -1,9 +1,45 @@
-data "template_file" "policy" {
-  template = file("${path.module}/policy.json")
-  vars = {
-    aws_partition = var.aws_partition
-    log_bucket    = var.log_bucket
+data "aws_iam_policy_document" "s3_logstash_user_policy" {
+  statement {
+    actions = [
+      "s3:ListAllMyBuckets"
+    ]
+
+    resources = [
+      "arn:${var.aws_partition}:s3:::*"
+    ]
   }
+
+  statement {
+    actions = [
+      "s3:ListBucket",
+      "s3:GetBucketLocation"
+    ]
+
+    resources = [
+      "arn:${var.aws_partition}:s3:::${var.log_bucket}"
+    ]
+  } 
+
+  statement {
+    actions = [
+      "s3:DeleteObject",
+      "s3:DeleteObjectVersion",
+      "s3:GetBucketVersioning",
+      "s3:GetObject",
+      "s3:GetObjectAcl",
+      "s3:GetObjectVersion",
+      "s3:GetObjectVersionAcl",
+      "s3:ListBucketMultipartUploads",
+      "s3:ListBucketVersions",
+      "s3:ListMultipartUploadParts",
+      "s3:PutObject",
+      "s3:PutObjectAcl"
+    ]
+
+    resources = [
+      "arn:${var.aws_partition}:s3:::${var.log_bucket}/*"
+    ]
+  }    
 }
 
 resource "aws_iam_user" "iam_user" {
@@ -17,5 +53,5 @@ resource "aws_iam_access_key" "iam_access_key_v1" {
 resource "aws_iam_user_policy" "iam_policy" {
   name   = "${aws_iam_user.iam_user.name}-policy"
   user   = aws_iam_user.iam_user.name
-  policy = data.template_file.policy.rendered
+  policy = data.aws_iam_policy_document.s3_logstash_user_policy.json
 }
