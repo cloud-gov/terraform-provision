dc1acdc810f715d24a46e36b989b54a3af97f9ef
Christopher Weibel <christopher.weibel@gsa.gov>
2025-03-20 08:56:05 -0400

    terraform fmt ftw

--- terraform/modules/concourse/rds.tf
+++ terraform/modules/concourse/rds.tf
@@ -19,5 +19,5 @@ module "rds_96" {
   rds_multi_az                    = var.rds_multi_az
   rds_final_snapshot_identifier   = var.rds_final_snapshot_identifier
   performance_insights_enabled    = var.performance_insights_enabled
-  rds_force_ssl                   = var.rds_force_ssl 
+  rds_force_ssl                   = var.rds_force_ssl
 }
