f767cb32418650b51aa0026cb6fa5e9d3646506f
Robert Gottlieb <robert.gottlieb@gsa.gov>
2025-06-16 15:48:31 -0400

    revert: back out westa-hub

--- terraform/stacks/westa-hub/outputs.tf
+++ terraform/stacks/westa-hub/outputs.tf
@@ -934,21 +934,12 @@ output "rds_storage_alert_username" {
   value = module.rds_storage_alert.username
 }
 
-output "rds_storage_alert_access_key_id_prev" {
-  value = module.rds_storage_alert.access_key_id_prev
+output "rds_storage_alert_access_key_id" {
+  value = module.rds_storage_alert.access_key_id
 }
 
-output "rds_storage_alert_secret_access_key_prev" {
-  value     = module.rds_storage_alert.secret_access_key_prev
-  sensitive = true
-}
-
-output "rds_storage_alert_access_key_id_curr" {
-  value = module.rds_storage_alert.access_key_id_curr
-}
-
-output "rds_storage_alert_secret_access_key_curr" {
-  value     = module.rds_storage_alert.secret_access_key_curr
+output "rds_storage_alert_secret_access_key" {
+  value     = module.rds_storage_alert.secret_access_key
   sensitive = true
 }
 
