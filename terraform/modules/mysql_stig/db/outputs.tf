6e901aa462aea7274acb156a6fd180f93a493340
Peter Burkholder <peter.burkholder@gsa.gov>
2025-07-17 14:58:59 -0400

    Properly name outputs

--- terraform/modules/mysql_stig/db/outputs.tf
+++ terraform/modules/mysql_stig/db/outputs.tf
@@ -1,24 +1,24 @@
-output "rds_host" {
+output "mysqlstig_rds_host" {
   value = module.db.rds_host
 }
 
-output "rds_port" {
+output "mysqlstig_rds_port" {
   value = module.db.rds_port
 }
 
-output "rds_url" {
+output "mysqlstig_rds_url" {
   value = module.db.rds_url
 }
 
-output "rds_name" {
+output "mysqlstig_rds_name" {
   value = module.db.rds_name
 }
 
-output "rds_username" {
+output "mysqlstig_rds_username" {
   value = module.db.rds_username
 }
 
-output "rds_password" {
+output "mysqlstig_rds_password" {
   value     = module.db.rds_password
   sensitive = true
 }
