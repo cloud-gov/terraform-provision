60d6549a8949ff1d6ee9fdf75b4448fc4a11e43a
Peter Burkholder <peter.burkholder@gsa.gov>
2025-07-23 14:41:47 -0400

    Properly declare outputs needed by downstream shell

--- terraform/stacks/main/outputs.tf
+++ terraform/stacks/main/outputs.tf
@@ -805,8 +805,6 @@ output "opensearch_proxy_redis_cluster" {
   sensitive = true
 }
 
-
-
 output "mysql_stig" {
   description = "Values required for MySQL DB used for STIG hardening"
   sensitive   = true
@@ -820,4 +818,14 @@ output "mysql_stig" {
       password = one(module.mysql_stig[*].rds_password)
     }
   }
+}
+
+output "mysql_stig_rds_host" {
+  value = one(module.mysql_stig[*].rds_host)
+}
+output "mysql_stig_rds_password" {
+  value = one(module.mysql_stig[*].rds_password)
+}
+output "mysql_stig_rds_username" {
+  value = one(module.mysql_stig[*].rds_username)
 }
