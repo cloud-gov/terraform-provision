51a10138422dadabb3237102c5c2caacc5cb6612
ChrisMcGowan <chris.mcgowan@gsa.gov>
2025-07-24 13:35:25 -0400

    2nd pass

--- terraform/modules/rds_network/sg_mysql.tf
+++ terraform/modules/rds_network/sg_mysql.tf
@@ -8,25 +8,33 @@ resource "aws_security_group" "rds_mysql" {
   description = "Allow access to incoming mysql traffic"
   vpc_id      = var.vpc_id
 
-  ingress {
-    from_port       = 3306
-    to_port         = 3306
-    protocol        = "tcp"
-    security_groups = var.security_groups
-  }
-
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    security_groups = var.security_groups
-  }
-
   tags = {
     Name = "${var.stack_description} - Incoming MySQL Traffic"
   }
-
 }
+
+resource "aws_security_group_rule" "ingress_default" {
+  count = var.security_groups_count
+
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  source_security_group_id = element(var.security_groups, count.index)
+  security_group_id        = aws_security_group.rds_postgres.id
+}
+
+resource "aws_security_group_rule" "egress_default" {
+  count = var.security_groups_count
+
+  type                     = "egress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  source_security_group_id = element(var.security_groups, count.index)
+  security_group_id        = aws_security_group.rds_postgres.id
+}
+
 resource "aws_security_group_rule" "ingress_tooling" {
 
   type              = "ingress"
