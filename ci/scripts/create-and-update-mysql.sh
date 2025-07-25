c21374218d333d988a3c710eb49c68b8d3141498
Peter Burkholder <peter.burkholder@gsa.gov>
2025-07-25 16:32:05 -0400

    Remove -e and use SQL on stdin

--- ci/scripts/create-and-update-mysql.sh
+++ ci/scripts/create-and-update-mysql.sh
@@ -13,14 +13,16 @@ db_pass=$(${TERRAFORM} output -raw -state="${STATE_FILE_PATH}" "${TERRAFORM_DB_P
 # There's no way to use an env variable for mysql password, so either
 # we specify on command line or we write an options file and read it
 # with --defaults-extra-file=file_name
-mysql_adm() { mysql -h "${db_address}" -u "${db_user}" -p"${db_pass}" -e "$@"; }
+mysql_adm() {
+	mysql -h "${db_address}" -u "${db_user}" -p"${db_pass}"
+}
 
 # SV-235137 etc... for password complexity
 # See https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/MySQL.Concepts.PasswordValidationPlugin.html
 # Requires restart to take effect, then uses parameter group for tuning
 set -v
 # SQL suggested by Claude, verified by humans
-mysql_adm <<- EOT
+mysql_adm <<-EOT
 	SET @plugin_exists = (SELECT COUNT(*) FROM INFORMATION_SCHEMA.PLUGINS WHERE PLUGIN_NAME = 'validate_password');
 
 	SET @sql = IF(@plugin_exists = 0, 
@@ -32,3 +34,5 @@ mysql_adm <<- EOT
 	DEALLOCATE PREPARE stmt;
 EOT
 set +v
+## Testing DB is cg-aws-broker-dev4xabqe7mm3d0gi1.
+
