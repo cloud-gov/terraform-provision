ef3ad6aefeb1ea7e75f00d6d6a17ed64654fcd66
Jason A. Gambino <jason.gambino@gsa.gov>
2025-09-11 09:14:24 -0400

    bucket naming difference

--- terraform/modules/metric_proccessing_lambda/s3.tf
+++ terraform/modules/metric_proccessing_lambda/s3.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "opensearch_metric_buckets" {
   for_each = toset(var.environments)
-  bucket   = "${var.name_prefix}-${each.key}-metrics"
+  bucket   = "logs-${var.name_prefix}-${each.key}-metrics"
   tags = merge(local.common_tags, {
     Environment = each.key
   })
