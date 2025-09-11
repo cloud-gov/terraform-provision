74f756f1841e0468226eb6c2de9183068a69cb46
Jason A. Gambino <jason.gambino@gsa.gov>
2025-09-11 12:44:39 -0400

    function name changes

--- terraform/modules/metric_proccessing_lambda/lambda.tf
+++ terraform/modules/metric_proccessing_lambda/lambda.tf
@@ -4,7 +4,7 @@ resource "aws_lambda_function" "transform" {
   filename         = data.archive_file.lambda_zip.output_path
   function_name    = "${var.name_prefix}-${each.key}-transform"
   role             = aws_iam_role.lambda_role[each.key].arn
-  handler          = "lambda_function_${each.key}.lambda_handler"
+  handler          = "lambda_function.lambda_handler"
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
   runtime          = "python3.13"
   architectures    = ["arm64"]
