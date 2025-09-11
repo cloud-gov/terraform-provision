e45575410f08903cafa8cb490047d3a043c8a2a7
Jason A. Gambino <jason.gambino@gsa.gov>
2025-09-11 11:42:02 -0400

    trying again for right path

--- terraform/modules/metric_proccessing_lambda/lambda.tf
+++ terraform/modules/metric_proccessing_lambda/lambda.tf
@@ -5,7 +5,7 @@ resource "aws_lambda_function" "transform" {
   function_name    = "${var.name_prefix}-${each.key}-transform"
   role             = aws_iam_role.lambda_role[each.key].arn
   handler          = "lambda_function_${each.key}.lambda_handler"
-  source_code_hash = data.archive_file.lambda_zip[each.key].output_base64sha256
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
   runtime          = "python3.13"
   architectures    = ["arm64"]
 
