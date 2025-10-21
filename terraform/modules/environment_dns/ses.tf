9046424cac10587070fb4423b0229172e34cd628
Mark Boyd <mark.boyd@gsa.gov>
2025-10-21 13:19:46 -0400

    fix definition of DKIM route53 records for log alerts SES verification

--- terraform/modules/environment_dns/ses.tf
+++ terraform/modules/environment_dns/ses.tf
@@ -30,13 +30,13 @@ resource "aws_route53_record" "brokered_log_alerts_zone_ns" {
 }
 
 resource "aws_route53_record" "log_alerts_dkim_records" {
-  for_each = toset(aws_sesv2_email_identity.logs_alerts_ses_domain.dkim_signing_attributes)
+  for_each = toset(data.aws_sesv2_email_identity.logs_alerts_ses_domain.dkim_signing_attributes[0].tokens)
 
   zone_id = aws_route53_zone.log_alerts_mail_zone.zone_id
-  name    = "${each.value.token}._domainkey.${local.log_alerts_domain}"
+  name    = "${each.value}._domainkey.${local.log_alerts_domain}"
   type    = "CNAME"
   ttl     = "600"
-  records = ["${each.value.token}.dkim.amazonses.com"]
+  records = ["${each.value}.dkim.amazonses.com"]
 }
 
 resource "aws_route53_record" "log_alerts_dmarc" {
