5e6ce91f289f10bf1fa73b4f562e5929537fa28b
Jason <jason.gambino@gsa.gov>
2025-10-21 15:28:43 -0400

    updating iam

--- terraform/modules/cloudwatch_s3_ingestor/iam.tf
+++ terraform/modules/cloudwatch_s3_ingestor/iam.tf
@@ -112,3 +112,49 @@ resource "aws_iam_role_policy" "firehose_policy" {
   })
 }
 
+resource "aws_iam_role" "cloudwatch_role" {
+  for_each = toset(var.environments)
+
+  name = "${var.name_prefix}-${each.key}-cloudwatch-to-firehose-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "logs.${var.aws_region}.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = merge(local.common_tags, {
+    Environment = each.key
+  })
+}
+
+resource "aws_iam_role_policy" "cloudwatch_policy" {
+  for_each = toset(var.environments)
+
+  name = "${var.name_prefix}-${each.key}-cloudwatch-to-firehose-policy"
+  role = aws_iam_role.cloudwatch_role[each.key].id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "Action": [
+				"firehose:PutRecord",
+				"firehose:PutRecordBatch"
+		]
+        Resource = [
+          ["arn:${var.aws_partition}:firhose:${var.aws_region}:${var.account_id}:deliverystream/${aws_kinesis_firehose_delivery_stream.cloudwatch_stream.name}"]
+        ]
+      }
+    ]
+  })
+}
+
