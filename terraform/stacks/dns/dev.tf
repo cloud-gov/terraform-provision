c0ba4f6dc82a04191360a0211b041e8348637d9f
Mark Boyd <mark.boyd@gsa.gov>
2025-10-21 15:58:19 -0400

    fix variable names

--- terraform/stacks/dns/dev.tf
+++ terraform/stacks/dns/dev.tf
@@ -13,19 +13,19 @@ resource "aws_route53_record" "dev_ns" {
 
 
 module "dev_dns" {
-  source                         = "../../modules/environment_dns"
-  stack_name                     = "development"
-  zone_id                        = aws_route53_zone.dev_zone.zone_id
-  domain                         = "dev.us-gov-west-1.aws-us-gov.cloud.gov"
-  app_subdomain                  = "app.dev.us-gov-west-1.aws-us-gov.cloud.gov"
-  admin_subdomain                = "dev.us-gov-west-1.aws-us-gov.cloud.gov"
-  cf_lb_dns_name                 = data.terraform_remote_state.development.outputs.cf_lb_dns_name
-  cf_apps_lb_dns_name            = data.terraform_remote_state.development.outputs.cf_apps_lb_dns_name
-  cf_uaa_lb_dns_name             = data.terraform_remote_state.development.outputs.cf_uaa_lb_dns_name
-  main_lb_dns_name               = data.terraform_remote_state.development.outputs.main_lb_dns_name
-  diego_elb_dns_name             = data.terraform_remote_state.development.outputs.diego_elb_dns_name
-  tcp_lb_dns_names               = data.terraform_remote_state.development.outputs.tcp_lb_dns_names
-  log_alerts_ses_dkim_attributes = lookup(data.terraform_remote_state.development.outputs, "log_alerts_ses_dkim_attribute_tokens", [])
-  log_alerts_dmarc_email         = var.log_alerts_dmarc_email
-  log_alerts_ses_aws_region      = var.log_alerts_ses_aws_region
+  source                               = "../../modules/environment_dns"
+  stack_name                           = "development"
+  zone_id                              = aws_route53_zone.dev_zone.zone_id
+  domain                               = "dev.us-gov-west-1.aws-us-gov.cloud.gov"
+  app_subdomain                        = "app.dev.us-gov-west-1.aws-us-gov.cloud.gov"
+  admin_subdomain                      = "dev.us-gov-west-1.aws-us-gov.cloud.gov"
+  cf_lb_dns_name                       = data.terraform_remote_state.development.outputs.cf_lb_dns_name
+  cf_apps_lb_dns_name                  = data.terraform_remote_state.development.outputs.cf_apps_lb_dns_name
+  cf_uaa_lb_dns_name                   = data.terraform_remote_state.development.outputs.cf_uaa_lb_dns_name
+  main_lb_dns_name                     = data.terraform_remote_state.development.outputs.main_lb_dns_name
+  diego_elb_dns_name                   = data.terraform_remote_state.development.outputs.diego_elb_dns_name
+  tcp_lb_dns_names                     = data.terraform_remote_state.development.outputs.tcp_lb_dns_names
+  log_alerts_ses_dkim_attribute_tokens = lookup(data.terraform_remote_state.development.outputs, "log_alerts_ses_dkim_attribute_tokens", [])
+  log_alerts_dmarc_email               = var.log_alerts_dmarc_email
+  log_alerts_ses_aws_region            = var.log_alerts_ses_aws_region
 }
