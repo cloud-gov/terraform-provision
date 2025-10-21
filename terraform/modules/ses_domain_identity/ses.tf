2529d71325ba41343090305eb3c2f1b28b13074d
Mark Boyd <mark.boyd@gsa.gov>
2025-10-21 12:35:45 -0400

    add module for creating an SES domain identity

---
+++ terraform/modules/ses_domain_identity/ses.tf
@@ -0,0 +1,38 @@
+locals {
+  email_domain = "${var.email_identity_subdomain}.${var.environment_domain}"
+
+  dmarc_rua = join(
+    ",",
+    [
+      "mailto:reports@dmarc.cyber.dhs.gov",
+      "mailto:${var.dmarc_email}"
+    ]
+  )
+  dmarc_ruf = "mailto:${var.dmarc_email}"
+}
+
+resource "aws_sesv2_email_identity" "email_domain_identity" {
+  email_identity         = local.email_domain
+  configuration_set_name = aws_sesv2_configuration_set.email_domain_identity_config.configuration_set_name
+}
+
+resource "aws_sesv2_email_identity_mail_from_attributes" "email_mail_from" {
+  email_identity = aws_sesv2_email_identity.email_domain_identity.email_identity
+
+  behavior_on_mx_failure = "REJECT_MESSAGE"
+  mail_from_domain       = "${var.mail_from_subdomain}.${aws_sesv2_email_identity.email_domain_identity.email_identity}"
+}
+
+resource "aws_sesv2_configuration_set" "email_domain_identity_config" {
+  configuration_set_name = "${var.email_identity_subdomain}-${var.stack_description}"
+
+  delivery_options {
+    tls_policy = "REQUIRE"
+  }
+  reputation_options {
+    reputation_metrics_enabled = true
+  }
+  suppression_options {
+    suppressed_reasons = ["BOUNCE", "COMPLAINT"]
+  }
+}
