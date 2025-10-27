8fdbb4e3b05e912e5a528f1a79a2a34a85b507bc
Jason <jason.gambino@gsa.gov>
2025-10-27 14:54:23 -0400

    need iam

--- terraform/modules/cloudwatch_s3_ingestor/iam.tf
+++ terraform/modules/cloudwatch_s3_ingestor/iam.tf
@@ -186,3 +186,41 @@ resource "aws_iam_role_policy" "cloudwatch_policy" {
   role     = aws_iam_role.cloudwatch_role[each.key].id
   policy   = data.aws_iam_policy_document.cloudwatch_policy[each.key].json
 }
+
+resource "aws_iam_role" "eventbridge_lambda_role" {
+  for_each = toset(var.environments)
+  name = "${var.name_prefix}-${each.key}-eventbridge-lambda-role" 
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect   = "Allow"
+        Principal = {
+          Service = "events.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "eventbridge_lambda_policy" {
+  for_each = toset(var.environments)
+  name        = "${var.name_prefix}-${each.key}-eventbridge-lambda-policy"
+  description = "Policy for EventBridge to invoke Lambda"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "lambda:InvokeFunction"
+        Effect   = "Allow"
+        Resource = aws_lambda_function.cloudwatch_filter.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eventbridge_lambda_attachment" {
+  role       = aws_iam_role.eventbridge_lambda_role.name
+  policy_arn = aws_iam_policy.eventbridge_lambda_policy.arn
+}
