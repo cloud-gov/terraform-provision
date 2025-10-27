eeac36723f0c03ca4156a3283a4c7b706943bbb7
Mark Boyd <mark.boyd@gsa.gov>
2025-10-27 17:16:57 -0400

    refactor invocation of environment_dns module in dns stack

--- terraform/stacks/dns/staging.tf
+++ terraform/stacks/dns/staging.tf
@@ -12,14 +12,21 @@ resource "aws_route53_record" "staging_ns" {
 
 
 module "staging_dns" {
-  source              = "../../modules/environment_dns"
-  stack_name          = "staging"
-  zone_id             = aws_route53_zone.staging_zone.zone_id
-  domain              = "fr-stage.cloud.gov"
-  app_subdomain       = "app.fr-stage.cloud.gov"
-  admin_subdomain     = "fr-stage.cloud.gov"
-  remote_state_bucket = var.remote_state_bucket
-  remote_state_region = var.remote_state_region
+  source                               = "../../modules/environment_dns"
+  stack_name                           = "staging"
+  zone_id                              = aws_route53_zone.staging_zone.zone_id
+  domain                               = "fr-stage.cloud.gov"
+  app_subdomain                        = "app.fr-stage.cloud.gov"
+  admin_subdomain                      = "fr-stage.cloud.gov"
+  cf_lb_dns_name                       = data.terraform_remote_state.staging.outputs.cf_lb_dns_name
+  cf_apps_lb_dns_name                  = data.terraform_remote_state.staging.outputs.cf_apps_lb_dns_name
+  cf_uaa_lb_dns_name                   = data.terraform_remote_state.staging.outputs.cf_uaa_lb_dns_name
+  main_lb_dns_name                     = data.terraform_remote_state.staging.outputs.main_lb_dns_name
+  diego_elb_dns_name                   = data.terraform_remote_state.staging.outputs.diego_elb_dns_name
+  tcp_lb_dns_names                     = data.terraform_remote_state.staging.outputs.tcp_lb_dns_names
+  log_alerts_ses_dkim_attribute_tokens = lookup(data.terraform_remote_state.staging.outputs, "log_alerts_ses_dkim_attribute_tokens", [])
+  log_alerts_dmarc_email               = var.log_alerts_dmarc_email
+  log_alerts_ses_aws_region            = var.log_alerts_ses_aws_region
 }
 
 module "test_cdn_dns_staging" {
