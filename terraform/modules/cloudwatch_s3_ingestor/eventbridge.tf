ddfc56b19cde1ebb5dcaecd468a2255081fb1da9
Jason <jason.gambino@gsa.gov>
2025-10-27 15:02:22 -0400

    fixing both

--- terraform/modules/cloudwatch_s3_ingestor/eventbridge.tf
+++ terraform/modules/cloudwatch_s3_ingestor/eventbridge.tf
@@ -2,7 +2,6 @@ resource "aws_cloudwatch_event_rule" "rds_log_group_creation_broker" {
   for_each       = toset(var.environments)
   name        = "rds-log-group-creation-broker"
   description = "Triggers when a CloudWatch Log Group is created with an /aws/rds/instance/cg-aws-broker-dev prefix in the name."
-  role = aws_iam_role.eventbridge_lambda_role[each.key].arn
   event_pattern = jsonencode({
     "source"      = ["aws.logs"],
     "detail-type" = ["AWS API Call via CloudTrail"],
@@ -23,6 +22,7 @@ resource "aws_cloudwatch_event_rule" "rds_log_group_creation_broker" {
 resource "aws_cloudwatch_event_target" "rds_log_group_target" {
   for_each       = toset(var.environments)
   rule      = aws_cloudwatch_event_rule.rds_log_group_creation_broker[each.key].name
+  role_arn= aws_iam_role.eventbridge_lambda_role[each.key].arn
   arn       = resource.aws_lambda_function.cloudwatch_filter[each.key].arn
   target_id = resource.aws_lambda_function.cloudwatch_filter[each.key].id
 }
--- terraform/modules/cloudwatch_s3_ingestor/iam.tf
+++ terraform/modules/cloudwatch_s3_ingestor/iam.tf
@@ -221,6 +221,7 @@ resource "aws_iam_policy" "eventbridge_lambda_policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "eventbridge_lambda_attachment" {
-  role       = aws_iam_role.eventbridge_lambda_role.name
+  for_each = toset(var.environments)
+  role       = aws_iam_role.eventbridge_lambda_role[each.key].name
   policy_arn = aws_iam_policy.eventbridge_lambda_policy.arn
 }
