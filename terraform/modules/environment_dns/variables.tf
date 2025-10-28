6aa3dbcaf8683101fe897b923670cb4634324226
Jason <jason.gambino@gsa.gov>
2025-10-28 12:49:37 -0400

    formatting

--- terraform/modules/environment_dns/variables.tf
+++ terraform/modules/environment_dns/variables.tf
@@ -24,12 +24,9 @@ variable "alb_zone_id" {
 variable "nlb_zone_id" {
   default = "ZMG1MZ2THAWF1" # this is for us-gov-west-1. See others here: https://docs.aws.amazon.com/general/latest/gr/elb.html
 }
-
-
 variable "remote_state_bucket" {
 
 }
-
 variable "remote_state_region" {
 
 }
