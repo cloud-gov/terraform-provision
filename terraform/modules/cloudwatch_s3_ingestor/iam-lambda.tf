a2e1b6b8b83dfdb09ea1d5863ecacae3e3ef7482
Jason <jason.gambino@gsa.gov>
2025-10-28 11:01:15 -0400

    typo

--- terraform/modules/cloudwatch_s3_ingestor/iam-lambda.tf
+++ terraform/modules/cloudwatch_s3_ingestor/iam-lambda.tf
@@ -39,7 +39,7 @@ data "aws_iam_policy_document" "lambda_logs_policy" {
     ]
     effect = "Allow"
     resources = [
-      "arn:${var.aws_partition}:logs:${var.aws_region}:${var.account_id}:log-group:/aws/rds/instance/cg-aws-broker--${each.key == "production" ? "prd" : (each.key == "staging" ? "stg" : "dev")}-*"
+      "arn:${var.aws_partition}:logs:${var.aws_region}:${var.account_id}:log-group:/aws/rds/instance/cg-aws-broker${each.key == "production" ? "prd" : (each.key == "staging" ? "stg" : "dev")}-*"
     ]
   }
 }
