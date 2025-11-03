638791e935e28af64e69f33baa0b6296e242dfd2
Jason <jason.gambino@gsa.gov>
2025-11-03 11:53:46 -0500

    adding in different lambda versions

--- terraform/modules/cloudwatch_s3_ingestor/iam-firehose.tf
+++ terraform/modules/cloudwatch_s3_ingestor/iam-firehose.tf
@@ -37,7 +37,8 @@ data "aws_iam_policy_document" "firehose_policy" {
       "lambda:InvokeFunction",
       "lambda:GetFunctionConfiguration"
     ]
-    resources = ["${aws_lambda_function.transform[each.key].arn}/*"]
+    resources = [aws_lambda_function.transform[each.key].arn,   # Base function ($LATEST)
+    "${aws_lambda_function.transform[each.key].arn}:*"  # All versions]
   }
   statement {
     effect = "Allow"
