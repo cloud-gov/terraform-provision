532841b47d0ee40a6ff6a0df2768016706684159
Jason <jason.gambino@gsa.gov>
2025-12-19 11:14:34 -0500

    putting in s3 policy for cloudwatch bucket

--- terraform/modules/cloudwatch_s3_ingestor/s3.tf
+++ terraform/modules/cloudwatch_s3_ingestor/s3.tf
@@ -37,22 +37,36 @@ data "aws_iam_policy_document" "opensearch_buckets_deny_unencrypted_policy" {
       "${aws_s3_bucket.opensearch_cloudwatch_buckets[each.key].arn}/*"
     ]
     condition {
-      test     = "StringNotLike"
-      variable = "aws:PrincipalArn"
-      values   = ["arn:${var.aws_partition}:iam::${var.account_id}:role/${var.ingestor_arn}"]
+      test     = "StringNotEquals"
+      variable = "s3:x-amz-server-side-encryption"
+      values   = ["AES256"]
     }
-    condition {
-      test     = "StringNotLike"
-      variable = "aws:PrincipalArn"
-      values   = ["arn:${var.aws_partition}:sts::${var.account_id}:assumed-role/${var.ingestor_arn}"]
-    }
-    condition {
-      test     = "StringNotLike"
-      variable = "aws:PrincipalArn"
-      values   = [aws_iam_role.lambda_role[each.key].arn]
-    }
+
   }
+
+  statement {
+    sid    = "DenyUnencryptedObjectUploads"
+    effect = "Deny"
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    actions = [
+      "s3:PutObject",
+    ]
+    resources = [
+      "${aws_s3_bucket.opensearch_cloudwatch_buckets[each.key].arn}/*"
+    ]
+    condition {
+      test     = "Null"
+      variable = "s3:x-amz-server-side-encryption"
+      values   = ["true"]
+    }
+
+    }
 }
+
+
 
 resource "aws_s3_bucket_policy" "opensearch_buckets_policy" {
   for_each = aws_s3_bucket.opensearch_cloudwatch_buckets
