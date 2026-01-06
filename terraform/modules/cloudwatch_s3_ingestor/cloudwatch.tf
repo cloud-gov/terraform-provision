acd8200e7704764adc3865cd0b4541b36dbea2b0
Jason <jason.gambino@gsa.gov>
2026-01-06 11:30:10 -0500

    fixing

--- terraform/modules/cloudwatch_s3_ingestor/cloudwatch.tf
+++ terraform/modules/cloudwatch_s3_ingestor/cloudwatch.tf
@@ -9,8 +9,10 @@ for_each = toset(var.environments)
 }
 
 resource "aws_cloudwatch_log_subscription_filter" "test_subscription" {
-  name            = "${local.prefixes[each.key]}-test-subscription"
+  for_each = toset(var.environments)
+  name = "/aws/rds/instance/cg-aws-broker-${local.prefixes[each.key]}-smoketestenvironment"
   log_group_name  = aws_cloudwatch_log_group.test_log[each.key].name
   filter_pattern  = "" 
-  destination_arn = resource.aws_kinesis_firehose_delivery_stream.cloudwatch_stream[each.key].arn
+  destination_arn = aws_kinesis_firehose_delivery_stream.cloudwatch_stream[each.key].arn
+  role_arn        = resource.aws_iam_role.cloudwatch_role[each.key].arn
 }
