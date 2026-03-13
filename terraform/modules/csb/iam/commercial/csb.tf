c7d4d2be3ee46625c8593550b1d3c74e7c1c4189
Mark Boyd <mark.boyd@gsa.gov>
2026-03-13 09:39:54 -0400

    update route53 permissions

--- terraform/modules/csb/iam/commercial/csb.tf
+++ terraform/modules/csb/iam/commercial/csb.tf
@@ -1,8 +1,42 @@
+// Values required for policy attachment
+data "aws_caller_identity" "current" {}
+data "aws_partition" "current" {}
+locals {
+  this_aws_account_id = data.aws_caller_identity.current.account_id
+  // Attribute aws_iam_policy.brokerpak_aws_ses.arn is not determined until apply, so it cannot be
+  // referenced in for_each below. Build the ARN here instead.
+  brokerpak_aws_ses_arn = "arn:${data.aws_partition.current.partition}:iam::${local.this_aws_account_id}:policy/${aws_iam_policy.brokerpak_aws_ses.name}"
+}
+
 // See also the companion user in GovCloud (../govcloud/csb.tf)
 data "aws_iam_policy_document" "brokerpak_aws_ses_commercial" {
   statement {
-    effect    = "Allow"
-    actions   = ["route53:ListHostedZones"]
+    effect = "Allow"
+    actions = [
+      "route53:CreateHostedZone",
+      "route53:ListHostedZones"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "route53:GetHostedZone",
+      "route53:ListResourceRecordSets",
+      "route53:GetDNSSEC",
+      "route53:DeleteHostedZone",
+      "route53:ChangeResourceRecordSets"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "route53:GetChange",
+      "route53:ListResourceRecordSets",
+    ]
     resources = ["*"]
   }
 }
@@ -21,21 +55,10 @@ resource "aws_iam_access_key" "csb_v2" {
   user = aws_iam_user.csb.name
 }
 
-// Values required for policy attachment
-data "aws_caller_identity" "current" {}
-data "aws_partition" "current" {}
-locals {
-  this_aws_account_id = data.aws_caller_identity.current.account_id
-  // Attribute aws_iam_policy.brokerpak_aws_ses.arn is not determined until apply, so it cannot be
-  // referenced in for_each below. Build the ARN here instead.
-  brokerpak_aws_ses_arn = "arn:${data.aws_partition.current.partition}:iam::${local.this_aws_account_id}:policy/${aws_iam_policy.brokerpak_aws_ses.name}"
-}
-
 resource "aws_iam_user_policy_attachment" "csb_policies" {
   for_each = toset([
     # Each brokerpak will use a separate policy so it is clear which permissions they individually require.
     local.brokerpak_aws_ses_arn,
-    "arn:aws:iam::aws:policy/AmazonRoute53FullAccess", # Also the aws-ses brokerpak
   ])
 
   user       = aws_iam_user.csb.name
