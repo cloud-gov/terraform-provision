52ffa4175842b8cb97464914a1d02a6108c1de3c
Jason <jason.gambino@gsa.gov>
2026-03-26 09:30:29 -0400

    name too long

--- terraform/modules/platform_opensearch/elb_platform_dashboard.tf
+++ terraform/modules/platform_opensearch/elb_platform_dashboard.tf
@@ -1,5 +1,5 @@
 resource "aws_lb_target_group" "platform_dashboard" {
-  name     = "${var.stack_description}-platform_opensearch-dashboard"
+  name     = "${var.stack_description}-platform-opensearch-dashboard"
   port     = 5600
   protocol = "HTTP"
   vpc_id   = var.vpc_id
