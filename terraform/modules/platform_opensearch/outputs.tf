9c06c3889b92f08707890ebbe23b3d0b1e338dac
Jason <jason.gambino@gsa.gov>
2026-03-26 09:57:07 -0400

    dont need bucket yet

--- terraform/modules/platform_opensearch/outputs.tf
+++ terraform/modules/platform_opensearch/outputs.tf
@@ -11,20 +11,3 @@ output "platform_opensearch_syslog_elb_dns_name" {
 output "platform_opensearch_lb_target_group" {
   value = aws_lb_target_group.platform_dashboard.name
 }
-
-output "platform_opensearch_logs_bucket_access_key_id_prev" {
-  value = ""
-}
-
-output "platform_opensearch_logs_bucket_secret_access_key_prev" {
-  value = ""
-}
-
-output "platform_opensearch_logs_bucket_access_key_id_curr" {
-  value = aws_iam_access_key.iam_access_key_v2.id
-}
-
-output "platform_opensearch_logs_bucket_secret_access_key_curr" {
-  value     = aws_iam_access_key.iam_access_key_v2.secret
-  sensitive = true
-}
