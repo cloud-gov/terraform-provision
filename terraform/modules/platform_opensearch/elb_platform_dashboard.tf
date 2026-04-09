892fa6ec56379c97e3be3ddff29e16f437ea4e7f
Jason <jason.gambino@gsa.gov>
2026-04-09 09:48:23 -0400

    load balancer was not https

--- terraform/modules/platform_opensearch/elb_platform_dashboard.tf
+++ terraform/modules/platform_opensearch/elb_platform_dashboard.tf
@@ -1,15 +1,16 @@
 resource "aws_lb_target_group" "platform_dashboard" {
   name     = "${var.stack_description}-platform-dashboard"
   port     = 5605
-  protocol = "HTTP"
+  protocol = "HTTPS"
   vpc_id   = var.vpc_id
 
   health_check {
     healthy_threshold   = 2
     unhealthy_threshold = 10
+    protocol            = "HTTPS"
     timeout             = 5
     interval            = 30
-    matcher             = 403
+    matcher            = "200,403"
   }
 }
 
