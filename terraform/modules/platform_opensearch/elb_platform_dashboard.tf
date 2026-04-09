6f27db50dfe0d2a0c2c9712e28152ab0d6c681c4
Jason <jason.gambino@gsa.gov>
2026-04-09 12:15:37 -0400

    adding in healthcheck

--- terraform/modules/platform_opensearch/elb_platform_dashboard.tf
+++ terraform/modules/platform_opensearch/elb_platform_dashboard.tf
@@ -3,4 +3,13 @@ resource "aws_lb_target_group" "platform_dashboard" {
   port     = 5605
   protocol = "TLS"
   vpc_id   = var.vpc_id
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 10
+    protocol            = "TCP"
+    timeout             = 5
+    interval            = 30
+  }
 }
+
