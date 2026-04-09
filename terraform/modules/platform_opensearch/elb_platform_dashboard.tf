a73fec855ff27334eb0dac2913d3ed7f912ade23
Jason <jason.gambino@gsa.gov>
2026-04-09 12:08:18 -0400

    tls passthrouhg

--- terraform/modules/platform_opensearch/elb_platform_dashboard.tf
+++ terraform/modules/platform_opensearch/elb_platform_dashboard.tf
@@ -10,24 +10,5 @@ resource "aws_lb_target_group" "platform_dashboard" {
     protocol            = "TCP"
     timeout             = 5
     interval            = 30
-    matcher            = "200,403"
-  }
-}
-
-resource "aws_lb_listener_rule" "platform_dashboard" {
-  count = length(var.hosts)
-
-  listener_arn = var.listener_arn
-  priority     = 203 + count.index
-
-  action {
-    target_group_arn = aws_lb_target_group.platform_dashboard.arn
-    type             = "forward"
-  }
-
-  condition {
-    host_header {
-      values = [element(var.hosts, count.index)]
-    }
   }
 }
