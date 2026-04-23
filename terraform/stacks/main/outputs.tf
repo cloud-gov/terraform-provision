187bf9f169c50dcad91d7a79dfe31f6602a21c24
Jason <jason.gambino@gsa.gov>
2026-04-23 13:28:51 -0400

    adding in new profile

--- terraform/stacks/main/outputs.tf
+++ terraform/stacks/main/outputs.tf
@@ -577,6 +577,10 @@ output "logs_opensearch_ingestor_profile" {
   value = module.logs_opensearch_ingestor_role.profile_name
 }
 
+output "platform_opensearch_ingestor_profile" {
+  value = module.platform_opensearch_ingestor_role.profile_name
+}
+
 output "cf_blobstore_profile" {
   value = module.cf_blobstore_role.profile_name
 }
