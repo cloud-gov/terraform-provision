9f165c86a2493aef1c8287322b79b80abecaa4e6
Jason <jason.gambino@gsa.gov>
2026-04-23 11:32:26 -0400

    catching up

--- terraform/modules/platform_opensearch/outputs.tf
+++ terraform/modules/platform_opensearch/outputs.tf
@@ -1,13 +1,26 @@
-/* Logsearch ELB */
-
-output "platform_opensearch_syslog_elb_name" {
-  value = aws_elb.platform_opensearch_syslog_elb.name
-}
-
-output "platform_opensearch_syslog_elb_dns_name" {
-  value = aws_elb.platform_opensearch_syslog_elb.dns_name
-}
+/* Opensearchsearch ELB */
 
 output "platform_opensearch_lb_target_group" {
   value = aws_lb_target_group.platform_dashboard.name
+}
+
+output "platform-opensearch_bucket_access_key_id_prev" {
+  value = ""
+}
+
+output "platform-opensearch_bucket_secret_access_key_prev" {
+  value = ""
+}
+
+output "platform-opensearch_bucket_access_key_id_curr" {
+  value = aws_iam_access_key.iam_access_key_v2.id
+}
+
+output "platform-opensearch_bucket_secret_access_key_curr" {
+  value     = aws_iam_access_key.iam_access_key_v2.secret
+  sensitive = true
+}
+
+output "platform-opensearch_bucket_name" {
+  value = aws_s3_bucket.log_bucket.id
 }
