a92d31b65e5f9f9934e7a38bd0b3a0cdad5d748d
Van Nguyen <vanh.nguyen@gsa.gov>
2026-05-28 12:31:00 -0400

    Updates via linters (#2328)

--- ci/provision-certificate.sh
+++ ci/provision-certificate.sh
@@ -2,8 +2,8 @@
 
 set -eu
 
-pip install certbot==3.3.0
-pip install certbot-dns-route53==3.3.0
+pip install certbot==4.2.0
+pip install certbot-dns-route53==4.2.0
 
 #spruce_url=$(curl https://api.github.com/repos/geofffranks/spruce/releases/latest \
 #  | jq -r '.assets[] | select(.name == "spruce-linux-amd64") | .browser_download_url')
@@ -13,11 +13,12 @@ chmod +x ./spruce
 
 # Quit if current certificate isn't close to expiration date
 expiration=$(
-  jq -r --arg prefix "${CERT_PREFIX}-" \
-    '.ServerCertificateMetadataList | map(select(.ServerCertificateName | startswith($prefix))) | sort_by(.Expiration) | .[-1].Expiration' \
-    < certificates/metadata)
+	jq -r --arg prefix "${CERT_PREFIX}-" \
+		'.ServerCertificateMetadataList | map(select(.ServerCertificateName | startswith($prefix))) | sort_by(.Expiration) | .[-1].Expiration' \
+		<certificates/metadata
+)
 if [[ $(date --date "+30 days" +%s) -lt $(date --date "${expiration}" +%s) ]]; then
-  exit 0
+	exit 0
 fi
 
 config_path=$(pwd)
@@ -28,14 +29,14 @@ export AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY
 
 certbot certonly \
-  -n --agree-tos \
-  --server "${ACME_SERVER:-https://acme-staging-v02.api.letsencrypt.org/directory}" \
-  --dns-route53 \
-  --config-dir "${config_path}" \
-  --email "${EMAIL}" \
-  --domain "${DOMAIN}" \
-  --rsa-key-size 2048 \
-  --key-type rsa
+	-n --agree-tos \
+	--server "${ACME_SERVER:-https://acme-staging-v02.api.letsencrypt.org/directory}" \
+	--dns-route53 \
+	--config-dir "${config_path}" \
+	--email "${EMAIL}" \
+	--domain "${DOMAIN}" \
+	--rsa-key-size 2048 \
+	--key-type rsa
 
 out_path=$(ls -d -1 "${config_path}"/live/*/)
 cp "${out_path}"/*.pem acme
@@ -46,15 +47,15 @@ CERT_CHECK=$(openssl x509 -in acme/cert.pem -text -noout | grep "Public-Key")
 KEY_CHECK=$(openssl rsa -in acme/privkey.pem -check -noout | grep "RSA key")
 
 if [[ "$CERT_CHECK" == *"2048 bit"* ]]; then
-    echo  "Certificate is 2048 bit and good"
-    else
-    echo "Certificate failed 2048 bit check and is bad/corrupt"
-    exit 1
+	echo "Certificate is 2048 bit and good"
+else
+	echo "Certificate failed 2048 bit check and is bad/corrupt"
+	exit 1
 fi
 
 if [[ "$KEY_CHECK" == *"RSA key ok"* ]]; then
-    echo  "Key is RSA based and good"
-    else
-    echo "Key is NOT RSA based and is bad/corrupt"
-    exit 1
+	echo "Key is RSA based and good"
+else
+	echo "Key is NOT RSA based and is bad/corrupt"
+	exit 1
 fi
