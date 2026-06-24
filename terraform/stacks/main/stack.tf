159e3c00b4276f9ab7f8ba1e63350f6565ab153d
Jason <jason.gambino@gsa.gov>
2026-06-24 16:30:58 -0400

    bucket lifecycle duplicates

--- terraform/stacks/main/stack.tf
+++ terraform/stacks/main/stack.tf
@@ -607,12 +607,12 @@ module "logs_opensearch_snapshot" {
   source = "../../modules/snapshot_credentials"
 
   resource_prefix = "log-opensearch-snapshot-${var.stack_description}"
-  bucket          = module.logs_opensearch_snapshot_bucket.bucket
+  bucket          = module.logs_opensearch_snapshot_bucket.arn
 }
 
 module "platform_snapshot" {
   source = "../../modules/snapshot_credentials"
 
   resource_prefix = "platform-snapshot-${var.stack_description}"
-  bucket          = module.platform_opensearch.aws_s3_bucket.snapshot_bucket.bucket
+  bucket          = module.platform_opensearch.snapshot_bucket.arn
 }
