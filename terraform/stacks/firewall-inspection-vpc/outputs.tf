5c67fb95a9957bcfdff2e5330724c099655b7e6e
Steve Greenberg <stephen.greenberg@gsa.gov>
2026-08-25 09:46:21 -0600

    reference module outputs

--- terraform/stacks/firewall-inspection-vpc/outputs.tf
+++ terraform/stacks/firewall-inspection-vpc/outputs.tf
@@ -1,14 +1,14 @@
 output "transit_gateway_id" {
   description = "ID of the Transit Gateway."
-  value       = aws_ec2_transit_gateway.tgw.id
+  value       = module.nfw_inspection_vpc.transit_gateway_id
 }
 
 output "inspection_vpc_id" {
   description = "ID of the inspection VPC."
-  value       = aws_vpc.inspection.id
+  value       = module.nfw_inspection_vpc.inspection_vpc_id
 }
 
 output "tgw_route_table_inspection_id" {
   description = "TGW route table ID for the inspection VPC."
-  value       = aws_ec2_transit_gateway_route_table.tgw.id
+  value       = module.nfw_inspection_vpc.tgw_route_table_inspection_id
 }
